5813613400af7866beee98c7e966e8487ed3d18f
Rafiq <aazztech6@gmail.com>
2018-10-07 08:27:47 +0600

    fiexed the category texonomy issue

--- includes/custom-fields/field-settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-$scporder_options = get_option('scporder_options');
-$scporder_objects = isset($scporder_options['objects']) ? $scporder_options['objects'] : array();
-$scporder_tags = isset($scporder_options['tags']) ? $scporder_options['tags'] : array();
-?>

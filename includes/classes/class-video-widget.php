1d410f5de591537ec47aeee35a8aafca56dba3b3
Shahadat Hossain <hridomheart@gmail.com>
2018-11-17 11:35:20 +0600

    available widget compatibility done

--- includes/classes/class-video-widget.php
+++ includes/classes/class-video-widget.php
@@ -45,9 +45,10 @@ if( !class_exists('BD_VIDEO_WIDGET')) {
 
                 echo $args['before_title'] . esc_html(apply_filters('widget_video_title', $title)) . $args['after_title'];
                 ?>
-
-                <iframe class="embed-responsive-item" src="<?php echo $videourl; ?>"
-                        allowfullscreen></iframe>
+                <div class="atbdp">
+                    <iframe class="embed-responsive-item" src="<?php echo $videourl; ?>"
+                            allowfullscreen></iframe>
+                </div>
 
                 <?php
                 echo $args['after_widget'];

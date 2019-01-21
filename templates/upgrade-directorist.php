262b23c2293fafb9e06d77b04bf3c65e27157f60
armanul <aazztech4@gmail.com>
2019-01-21 15:27:22 +0600

    capitalize settings name

--- templates/upgrade-directorist.php
+++ templates/upgrade-directorist.php
@@ -3,6 +3,7 @@
 <div class="container directorist-upgrade-wrap" style="width: 50%; margin-top: 30px;">
     <h2><?php _e('Upgrade Pages', ATBDP_TEXTDOMAIN)?></h2>
     <p style="background:#ffffff; padding: 20px; line-height: 24px;"><?php _e('If you are an existing user of the directorist, you have to upgrade your Directorist pages shortcode.', ATBDP_TEXTDOMAIN)?></p>
+    <p><span style="color: red; font-size: 14px"><?php _e('*It won\'t work unless you have selected Pages for the Shortcodes from Directory Settings >> Pages, Links & Views.', ATBDP_TEXTDOMAIN)?></span></p>
             <button type="button" id="shortcode-updated" class="button button-primary"><?php _e('Upgrade/Regenerate Pages', ATBDP_TEXTDOMAIN)?></button>
 
 

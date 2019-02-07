7cd89cca216dce3011d5661a1c29f346338437a2
Shamim-Ahmed <shamimeforb@gmail.com>
2019-02-07 16:07:03 +0600

    commit style changes

--- templates/front-end/all-listings/all-grid-listings.php
+++ templates/front-end/all-listings/all-grid-listings.php
@@ -313,7 +313,7 @@ $pagenation = get_directorist_option('paginate_all_listings',1);
                                                 <div class="atbd_content_left">
                                                     <div class="atbd_listting_category">
                                                         <a href="<?php echo esc_url(ATBDP_Permalink::get_category_archive($cats[0]));?>"><?php if ('none' != get_cat_icon($cats[0]->term_id)){ ?>
-                                                                <span class="fa fa-folder-open"></span> <?php }?><?php  echo $cats[0]->name;?></a>
+                                                                <span class="fa fa-folder-open"></span><?php }?><?php  echo $cats[0]->name;?></a>
                                                         <?php
                                                         if ($totalTerm>1){
                                                             ?>
@@ -371,8 +371,8 @@ $pagenation = get_directorist_option('paginate_all_listings',1);
         </div>
     <?php endif; ?>
 
-
-
+    </div>
+    <div class="row atbd_listing_pagination">
         <?php
         if (1 == $pagenation){
             ?>

fa4bcfdbdc17b047aea2208e9029932141b0811a
Shamim-Ahmed <shamimeforb@gmail.com>
2019-04-20 20:31:46 +0600

    fixed style of themes page

--- templates/themes.php
+++ templates/themes.php
@@ -4,13 +4,13 @@
             <div class="row">
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/wp-content/uploads/edd/2019/04/directoria_theme_prev.jpg"
                              alt="Directoria">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/directoria" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' Directoria ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' Directoria ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/directoria" target="_blank"
@@ -23,13 +23,13 @@
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/demos/plugins/directorist/demo-one/wp-content/uploads/2019/04/Direo.jpg"
                              alt="Direo">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/direo" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' Direo ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' Direo ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/direo" target="_blank"
@@ -42,13 +42,13 @@
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/demos/plugins/directorist/demo-one/wp-content/uploads/2019/04/Dplace.jpg"
                              alt="dPlace">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/dplace" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' dPlace ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' dPlace ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/dplace" target="_blank"
@@ -61,13 +61,13 @@
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/demos/plugins/directorist/demo-one/wp-content/uploads/2019/04/DRestaurant.jpg"
                              alt="dRestaurant">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/drestaurant" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' dRestaurant ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' dRestaurant ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/drestaurant" target="_blank"
@@ -80,13 +80,13 @@
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/demos/plugins/directorist/demo-one/wp-content/uploads/2019/04/DService.jpg"
                              alt="dService">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/dservice" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' dService ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' dService ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/dservice" target="_blank"
@@ -99,13 +99,13 @@
                 </div>
                 <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                     <!--Google reCAPTCHA-->
-                    <div class="single_extension">
+                    <div class="single_extension single_thm">
                         <img src="http://aazztech.com/demos/plugins/directorist/demo-one/wp-content/uploads/2019/04/DEvent.jpg"
                              alt="dEvent">
 
                         <div class="extension_detail">
                             <a href="http://directorist.com/theme/devent" target="_blank" class="ext_title">
-                                <h4 class="text-center"><?php esc_html_e(' dEvent ', ATBDP_TEXTDOMAIN); ?></h4></a>
+                                <h4><?php esc_html_e(' dEvent ', ATBDP_TEXTDOMAIN); ?></h4></a>
 
                             <div class="btn_wrapper">
                                 <a href="http://directorist.com/theme/demo/devent" target="_blank"

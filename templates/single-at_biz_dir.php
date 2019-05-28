5b5afe7c1b996d221b3912c8bbd3000c0e78b884
Shamim-Ahmed <shamimeforb@gmail.com>
2019-05-28 12:28:23 +0600

    fixed input height issue

--- templates/single-at_biz_dir.php
+++ templates/single-at_biz_dir.php
@@ -567,17 +567,6 @@ $main_col_size = is_active_sidebar('right-sidebar-listing') ? 'col-lg-8' : 'col-
             }
             ?>
 
-
-
-
-
-
-
-
-
-
-
-
             <?php
 
             if ($enable_video_url && !empty($videourl) && 'none' != $display_video_for) { ?>
@@ -600,12 +589,6 @@ $main_col_size = is_active_sidebar('right-sidebar-listing') ? 'col-lg-8' : 'col-
             <?php do_action('atbdp_after_video_gallery'); ?>
             <!--Google map section-->
 
-
-
-
-
-
-
             <?php
             if (!$disable_map && (empty($hide_map)) && !empty($manual_lng || $manual_lat) && !empty($display_map_field)) { ?>
                 <div class="atbd_content_module">
@@ -622,11 +605,6 @@ $main_col_size = is_active_sidebar('right-sidebar-listing') ? 'col-lg-8' : 'col-
                     </div>
                 </div><!-- end .atbd_custom_fields_contents -->
             <?php } ?>
-
-
-
-
-
 
             <?php if ((!$hide_contact_info) && !empty($address || $phone || $email || $website || $zip || $social) && empty($disable_contact_info)) { ?>
                 <div class="atbd_content_module atbd_contact_information_module">

e58c53cc307447d0d5c815dcc6d8f007591aeba7
Shamim-Ahmed <shamimeforb@gmail.com>
2019-05-29 15:52:04 +0600

    added RTL script support

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
-    sourcemap = require('gulp-sourcemaps');
+    sourcemap = require('gulp-sourcemaps'),
+        rtlcss = require("gulp-rtlcss"),
+        rename = require("gulp-rename"),
+        gulpfilter = require('gulp-filter');
 
 
 /* sass compiler */
@@ -26,4 +29,51 @@ gulp.task('default',['bs', 'style', 'admin'], function(){
     gulp.watch('style/**/*.scss', ['style']);
     gulp.watch('style/admin/**/*.scss', ['admin']);
     gulp.watch('style/business-hour/**/*.scss', ['bsHour']);
+});
+
+//rtl css generator
+gulp.task('rtl', function () {
+    var bootstrap = gulpfilter('**/bootstrap.css', {restore: true}),
+        style = gulpfilter('**/style.css', {restore: true}),
+        search_style = gulpfilter('**/search-style.css', {restore: true});
+
+    gulp.src(['bootstrap/bootstrap.css', 'public/assets/css/style.css', 'public/assets/css/search-style.css'])
+        .pipe(rtlcss({
+            'stringMap': [
+                {
+                    'name': 'left-right',
+                    'priority': 100,
+                    'search': ['left', 'Left', 'LEFT'],
+                    'replace': ['right', 'Right', 'RIGHT'],
+                    'options': {
+                        'scope': '*',
+                        'ignoreCase': false
+                    }
+                },
+                {
+                    'name': 'ltr-rtl',
+                    'priority': 100,
+                    'search': ['ltr', 'Ltr', 'LTR'],
+                    'replace': ['rtl', 'Rtl', 'RTL'],
+                    'options': {
+                        'scope': '*',
+                        'ignoreCase': false
+                    }
+                }
+            ]
+        }))
+        .pipe(bootstrap)
+        .pipe(rename({suffix: '-rtl', extname: '.css'}))
+        .pipe(gulp.dest('public/assets/css/'))
+        .pipe(bootstrap.restore)
+
+        .pipe(style)
+        .pipe(rename({suffix: '-rtl', extname: '.css'}))
+        .pipe(gulp.dest('public/assets/css/'))
+        .pipe(style.restore)
+
+        .pipe(search_style)
+        .pipe(rename({suffix: '-rtl', extname: '.css'}))
+        .pipe(gulp.dest('public/assets/css/'))
+        .pipe(search_style.restore)
 });

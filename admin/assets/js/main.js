cf8b249a73cd6b7441bd7f82c4055ad4e1c99cae
Shamim-Ahmed <shamimeforb@gmail.com>
2019-07-18 12:42:45 +0600

    fixed css / responsive issues

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -1376,6 +1376,28 @@ jQuery(function ($) {
         lf_opt55.show();
     }
 
+    $(".vp-field.vp-select .label .description strong").on("click", function () {
+        var sel, range;
+        var el = $(this)[0];
+        if (window.getSelection && document.createRange) { //Browser compatibility
+            sel = document.getSelection();
+            if(sel.toString() === ''){ //no text selection
+                window.setTimeout(function(){
+                    range = document.createRange(); //range object
+                    range.selectNodeContents(el); //sets Range
+                    sel.removeAllRanges(); //remove all ranges from selection
+                    sel.addRange(range);//add Range to a Selection.
+                },1);
+            }
+        }else if (document.selection) { //older ie
+            sel = document.selection.createRange();
+            if(sel.text === ''){ //no text selection
+                range = document.body.createTextRange();//Creates TextRange object
+                range.moveToElementText(el);//sets Range
+                range.select(); //make selection.
+            }
+        }
+    })
 
 });
 

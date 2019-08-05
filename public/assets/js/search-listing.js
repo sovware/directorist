cbb74a0ab55bc2210ad5015e5feb0b9608fc7ffb
Shamim-Ahmed <shamimeforb@gmail.com>
2019-08-05 17:08:19 +0600

    undo the style of previous category location child elements style in dropdown

--- public/assets/js/search-listing.js
+++ public/assets/js/search-listing.js
@@ -1,7 +1,7 @@
 (function ($) {
     $('#at_biz_dir-location').select2({
         placeholder: atbdp_search_listing.i18n_text.location_selection,
-        allowClear: true,
+        allowClear: true/*,
         templateResult: function (data) {
             // We only really care if there is an element to pull classes from
             if (!data.element) {
@@ -17,12 +17,12 @@
 
             return $wrapper;
         }
-    });
+    });*/
 
     // Category
     $('#at_biz_dir-category').select2({
         placeholder: atbdp_search_listing.i18n_text.category_selection,
-        allowClear: true,
+        allowClear: true/*,
         templateResult: function (data) {
             // We only really care if there is an element to pull classes from
             if (!data.element) {
@@ -37,7 +37,7 @@
             $wrapper.text(data.text);
 
             return $wrapper;
-        }
+        }*/
     });
 
     //ad search js

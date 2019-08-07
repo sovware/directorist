ac85396e30d6699533e06d7912f8891af4df68f5
Rafiq <aazztech6@gmail.com>
2019-08-07 19:26:12 +0600

    pot file update

--- templates/front-end/public-profile.php
+++ templates/front-end/public-profile.php
@@ -1,12 +1,12 @@
 <?php
 !empty($args['data']) ? extract($args['data']) : array(); // data array contains all required var.
 $all_listings = !empty($all_listings) ? $all_listings : new WP_Query;
-$paginate     = !empty($paginate) ? $paginate : '';
+$paginate = !empty($paginate) ? $paginate : '';
 $is_disable_price = get_directorist_option('disable_list_price');
-$container_fluid  = 'container-fluid';
+$container_fluid = 'container-fluid';
 ?>
 <div id="directorist" class="atbd_wrapper atbd_author_profile">
-    <div class="<?php echo apply_filters('atbdp_public_profile_container_fluid',$container_fluid) ?>">
+    <div class="<?php echo apply_filters('atbdp_public_profile_container_fluid', $container_fluid) ?>">
         <div class="row">
             <div class="col-md-12">
                 <?php
@@ -48,21 +48,21 @@ $container_fluid  = 'container-fluid';
                         $args = array(
                             'post_type' => ATBDP_POST_TYPE,
                             'post_status' => 'publish',
-                            'author'        =>  $author_id,
-                            'orderby'       =>  'post_date',
-                            'order'         =>  'ASC',
+                            'author' => $author_id,
+                            'orderby' => 'post_date',
+                            'order' => 'ASC',
                             'posts_per_page' => -1 // no limit
                         );
-                        $current_user_posts = get_posts( $args );
+                        $current_user_posts = get_posts($args);
                         $total_listing = count($current_user_posts);
                         $review_in_post = 0;
-                        $all_reviews =0;
-                        foreach ($current_user_posts as $post){
+                        $all_reviews = 0;
+                        foreach ($current_user_posts as $post) {
                             $average = ATBDP()->review->get_average($post->ID);
-                            if (!empty($average)){
+                            if (!empty($average)) {
                                 $averagee = array($average);
-                                foreach ($averagee as $key){
-                                    $all_reviews+= $key;
+                                foreach ($averagee as $key) {
+                                    $all_reviews += $key;
                                 }
                                 $review_in_post++;
                             }
@@ -72,16 +72,16 @@ $container_fluid  = 'container-fluid';
                         ?>
                         <div class="atbd_listing_meta">
                             <span class="atbd_meta atbd_listing_rating">
-                                <?php echo $author_rating;?><i class="<?php atbdp_icon_type(true);?>-star"></i>
+                                <?php echo $author_rating; ?><i class="<?php atbdp_icon_type(true); ?>-star"></i>
                             </span>
                         </div>
                         <p class="meta-info">
-                            <span><?php echo !empty($review_in_post)?$review_in_post:'0'?></span>
-                            <?php echo ($review_in_post>1)?'Reviews':'Review'?>
+                            <span><?php echo !empty($review_in_post) ? $review_in_post : '0' ?></span>
+                            <?php echo (($review_in_post > 1) || ($review_in_post === 0)) ? __('Reviews', ATBDP_TEXTDOMAIN) : __('Review', ATBDP_TEXTDOMAIN) ?>
                         </p>
                         <p class="meta-info">
-                            <span><?php echo !empty($total_listing)?$total_listing:'0'?></span>
-                            <?php echo ($total_listing>1)?'Listings':'Listing'?>
+                            <span><?php echo !empty($total_listing) ? $total_listing : '0' ?></span>
+                            <?php echo (($total_listing > 1) || ($total_listing === 0)) ? __('Listings', ATBDP_TEXTDOMAIN) : __('Listing', ATBDP_TEXTDOMAIN) ?>
                         </p>
                     </div>
                 </div>
@@ -94,7 +94,9 @@ $container_fluid  = 'container-fluid';
                     <div class="atbd_content_module">
                         <div class="atbd_content_module__tittle_area">
                             <div class="atbd_area_title">
-                                <h4><span class="<?php atbdp_icon_type(true);?>-user"></span><?php _e('About', ATBDP_TEXTDOMAIN); ?></h4>
+                                <h4>
+                                    <span class="<?php atbdp_icon_type(true); ?>-user"></span><?php _e('About', ATBDP_TEXTDOMAIN); ?>
+                                </h4>
                             </div>
                         </div>
 
@@ -117,51 +119,52 @@ $container_fluid  = 'container-fluid';
                         <div class="atbd_widget_contact_info">
                             <ul>
                                 <?php
-                                if (!empty($address)){
+                                if (!empty($address)) {
                                     ?>
                                     <li>
-                                        <span class="<?php atbdp_icon_type(true);?>-map-marker"></span>
-                                        <span class="atbd_info"><?= !empty($address)?esc_html($address):''; ?></span>
+                                        <span class="<?php atbdp_icon_type(true); ?>-map-marker"></span>
+                                        <span class="atbd_info"><?= !empty($address) ? esc_html($address) : ''; ?></span>
                                     </li>
                                     <?php
                                 }
-                                if (!empty($phone)){
+                                if (!empty($phone)) {
                                     ?>
                                     <!-- In Future, We will have to use a loop to print more than 1 number-->
                                     <li>
-                                        <span class="<?php atbdp_icon_type(true);?>-phone"></span>
-                                        <span class="atbd_info"><a href="tel:<?php echo esc_html(stripslashes($phone)); ?>"><?php echo esc_html(stripslashes($phone)); ?></a></span>
+                                        <span class="<?php atbdp_icon_type(true); ?>-phone"></span>
+                                        <span class="atbd_info"><a
+                                                    href="tel:<?php echo esc_html(stripslashes($phone)); ?>"><?php echo esc_html(stripslashes($phone)); ?></a></span>
                                     </li>
                                     <?php
                                 }
                                 $email_show = get_directorist_option('display_author_email', 'public');
-                                if ('public' === $email_show){
-                                    if (!empty($email)){
+                                if ('public' === $email_show) {
+                                    if (!empty($email)) {
                                         ?>
                                         <li>
-                                            <span class="<?php atbdp_icon_type(true);?>-envelope"></span>
-                                            <span class="atbd_info"><?= !empty($email)?esc_html($email):''; ?></span>
+                                            <span class="<?php atbdp_icon_type(true); ?>-envelope"></span>
+                                            <span class="atbd_info"><?= !empty($email) ? esc_html($email) : ''; ?></span>
                                         </li>
                                         <?php
                                     }
-                                }elseif ('logged_in' === $email_show){
-                                    if (is_user_logged_in()){
-                                        if (!empty($email)){
+                                } elseif ('logged_in' === $email_show) {
+                                    if (is_user_logged_in()) {
+                                        if (!empty($email)) {
                                             ?>
                                             <li>
-                                                <span class="<?php atbdp_icon_type(true);?>-envelope"></span>
-                                                <span class="atbd_info"><?= !empty($email)?esc_html($email):''; ?></span>
+                                                <span class="<?php atbdp_icon_type(true); ?>-envelope"></span>
+                                                <span class="atbd_info"><?= !empty($email) ? esc_html($email) : ''; ?></span>
                                             </li>
                                             <?php
                                         }
                                     }
                                 }
 
-                                if (!empty($website)){
+                                if (!empty($website)) {
                                     ?>
                                     <li>
-                                        <span class="<?php atbdp_icon_type(true);?>-globe"></span>
-                                        <span class="atbd_info"><?= !empty($website)?esc_html($website):''; ?></span>
+                                        <span class="<?php atbdp_icon_type(true); ?>-globe"></span>
+                                        <span class="atbd_info"><?= !empty($website) ? esc_html($website) : ''; ?></span>
                                     </li>
                                     <?php
                                 }
@@ -169,21 +172,21 @@ $container_fluid  = 'container-fluid';
                             </ul>
                         </div>
                         <?php
-                        if (!empty($facebook || $twitter || $linkedIn || $youtube)){
+                        if (!empty($facebook || $twitter || $linkedIn || $youtube)) {
                             ?>
                             <div class="atbd_social_wrap">
                                 <?php
                                 if ($facebook) {
-                                    printf('<p><a target="_blank" href="%s"><span class="'.atbdp_icon_type().'-facebook"></span></a></p>', $facebook);
+                                    printf('<p><a target="_blank" href="%s"><span class="' . atbdp_icon_type() . '-facebook"></span></a></p>', $facebook);
                                 }
                                 if ($twitter) {
-                                    printf('<p><a target="_blank" href="%s"><span class="'.atbdp_icon_type().'-twitter"></span></a></p>', $twitter);
+                                    printf('<p><a target="_blank" href="%s"><span class="' . atbdp_icon_type() . '-twitter"></span></a></p>', $twitter);
                                 }
                                 if ($linkedIn) {
-                                    printf('<p><a target="_blank" href="%s"><span class="'.atbdp_icon_type().'-linkedin"></span></a></p>', $linkedIn);
+                                    printf('<p><a target="_blank" href="%s"><span class="' . atbdp_icon_type() . '-linkedin"></span></a></p>', $linkedIn);
                                 }
                                 if ($youtube) {
-                                    printf('<p><a target="_blank" href="%s"><span class="'.atbdp_icon_type().'-youtube"></span></a></p>', $youtube);
+                                    printf('<p><a target="_blank" href="%s"><span class="' . atbdp_icon_type() . '-youtube"></span></a></p>', $youtube);
                                 }
                                 ?>
                             </div>
@@ -198,17 +201,18 @@ $container_fluid  = 'container-fluid';
         <div class="row">
             <div class="col-md-12">
                 <div class="atbd_author_listings_area">
-                    <h1><?php _e( "Author Listings", ATBDP_TEXTDOMAIN ); ?></h1>
+                    <h1><?php _e("Author Listings", ATBDP_TEXTDOMAIN); ?></h1>
                     <div class="atbd_author_filter_area">
                         <div class="dropdown">
-                            <a class="btn btn-outline-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <?php _e( "Filter by category", ATBDP_TEXTDOMAIN ); ?> <span class="caret"></span>
+                            <a class="btn btn-outline-primary dropdown-toggle" href="#" role="button"
+                               id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <?php _e("Filter by category", ATBDP_TEXTDOMAIN); ?> <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                 <?php
-                                foreach( $categories as $category ) {
-                                    printf( '<a class="dropdown-item" href="%s">%s</a>', add_query_arg( 'category', $category->slug ), $category->name );
+                                foreach ($categories as $category) {
+                                    printf('<a class="dropdown-item" href="%s">%s</a>', add_query_arg('category', $category->slug), $category->name);
                                 }
                                 ?>
                             </div>
@@ -219,9 +223,9 @@ $container_fluid  = 'container-fluid';
         </div>
         <div class="row atbd_authors_listing">
 
-                <?php
-                listing_view_by_grid($all_listings, $paginate, $is_disable_price);
-                ?>
+            <?php
+            listing_view_by_grid($all_listings, $paginate, $is_disable_price);
+            ?>
 
         </div>
     </div>

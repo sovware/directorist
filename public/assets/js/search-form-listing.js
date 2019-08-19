d5b6e754fa1cd2d53266db0c414c60c6ed9a865b
Rafiq <aazztech6@gmail.com>
2019-08-19 13:53:06 +0600

    fixed custom fields issue assigned with category in serch home

--- public/assets/js/search-form-listing.js
+++ public/assets/js/search-form-listing.js
@@ -35,8 +35,8 @@
     });
 
     // load custom fields of the selected category in the search form
-    $( 'body' ).on( 'change', '.bdas-category-search', function() {
-
+    //$( 'body' ).on( 'change', '.bdas-category-search', function() {
+        $('#at_biz_dir-category').on('change', function () {
         var $search_elem = $( this ).closest ( 'form' ).find( ".atbdp-custom-fields-search" );
 
         if( $search_elem.length ) {

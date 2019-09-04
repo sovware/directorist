132b8526391c492e5d0106c3c2d33620d6ef0f96
Rafiq <aazztech6@gmail.com>
2019-09-04 12:20:24 +0600

    fix price formetting issue

--- includes/payments/functions.php
+++ includes/payments/functions.php
@@ -99,51 +99,43 @@ function atbdp_get_payment_bulk_actions()
  *
  * @return   string     $amount               Newly formatted amount or Price Not Available
  */
-function atbdp_format_amount($amount, $decimals = true, $currency_settings = array())
-{
-    $decimals = get_directorist_option('allow_decimal', 1);
+function atbdp_format_amount( $amount, $decimals = true, $currency_settings = array() ) {
     !is_array($currency_settings) || extract($currency_settings); // if it is array then extract it. Using the magic of OR CONDITION's FLOW
 
-    $currency = !empty($currency) ? $currency : get_directorist_option('g_currency', 'USD');
-    $thousands_sep = !empty($thousands_separator) ? $thousands_separator : get_directorist_option('g_thousand_separator', ',');
-    $decimal_sep = !empty($decimal_separator) ? $decimal_separator : get_directorist_option('g_decimal_separator', '.');
+    $currency       = ! empty( $currency )              ? $currency : get_directorist_option('g_currency', 'USD') ;
+    $thousands_sep  = ! empty( $thousands_separator )   ? $thousands_separator:  get_directorist_option('g_thousand_separator', ',');
+    $decimal_sep    = ! empty( $decimal_separator )     ? $decimal_separator:  get_directorist_option('g_decimal_separator', '.');
 
     // Format the amount
-    if ($decimal_sep == ',' && false !== ($sep_found = strpos($amount, $decimal_sep))) {
-        $whole = substr($amount, 0, $sep_found);
-        $part = substr($amount, $sep_found + 1, (strlen($amount) - 1));
+    if( $decimal_sep == ',' && false !== ( $sep_found = strpos( $amount, $decimal_sep ) ) ) {
+        $whole = substr( $amount, 0, $sep_found );
+        $part = substr( $amount, $sep_found + 1, ( strlen( $amount ) - 1 ) );
         $amount = $whole . '.' . $part;
     }
 
     // Strip , from the amount (if set as the thousands separator)
-    if ($thousands_sep == ',' && false !== ($found = strpos($amount, $thousands_sep))) {
-        $amount = str_replace(',', '', $amount);
-    }
-    // Strip . from the amount (if set as the thousands separator)
-    if ($thousands_sep == '.' && false !== ($found = strpos($amount, $thousands_sep))) {
-        $amount = str_replace('.', '.', $amount);
-    }
-    // Strip ' ' from the amount (if set as the thousands separator)
-    if ($thousands_sep == ' ' && false !== ($found = strpos($amount, $thousands_sep))) {
-        $amount = str_replace(' ', '', $amount);
+    if( $thousands_sep == ',' && false !== ( $found = strpos( $amount, $thousands_sep ) ) ) {
+        $amount = str_replace( ',', '', $amount );
     }
 
-    if (empty($amount)) {
+    // Strip ' ' from the amount (if set as the thousands separator)
+    if( $thousands_sep == ' ' && false !== ( $found = strpos( $amount, $thousands_sep ) ) ) {
+        $amount = str_replace( ' ', '', $amount );
+    }
+
+    if( empty( $amount ) ) {
         $amount = 0;
     }
 
-    if ($decimals) {
-        $decimals = atbdp_currency_decimal_count(2, $currency);
+    if( $decimals ) {
+        $decimals  = atbdp_currency_decimal_count( 2, $currency );
     } else {
         $decimals = 0;
     }
 
-    /**
-     * @since 5.7.1 skip formatted data in order to deliver the actual amount for featured listing
-     */
-    // $formatted = number_format( $amount, $decimals, $decimal_sep, '' );
+    $formatted = number_format( $amount, $decimals, $decimal_sep, $thousands_sep );
 
-    return apply_filters('atbdp_format_amount', $amount, $amount, $decimals, $decimal_sep, $thousands_sep);
+    return apply_filters( 'atbdp_format_amount', $formatted, $amount, $decimals, $decimal_sep, $thousands_sep );
 
 }
 

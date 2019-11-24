84bb5ec3d094e13575b7b6e720af259e457a0d67
armanul46 <aazztech4@gmail.com>
2019-11-24 11:44:30 +0600

    fix radius search issue on search home

--- public/assets/js/geolocation.js
+++ public/assets/js/geolocation.js
@@ -19,8 +19,22 @@
     $(".atbdpr_amount").text(slider_range.slider("value") + miles);
     $("#atbd_rs_value").val(slider_range.slider("value"));
 
-
-
+    $("#atbdp_reset").on("click", function (){
+        $("#atbd_rs_value").val(0);
+        $(".atbdpr_amount").text(0 + miles);
+        slider_range.each(function () {
+            $(this).slider({
+                range: "min",
+                min: 0,
+                max: 1000,
+                value: 0,
+                slide: function (event, ui) {
+                    $(".atbdpr_amount").text(ui.value + miles);
+                    $("#atbd_rs_value").val(ui.value);
+                }
+            });
+        });
+    });
     /*
     get current location
 */

9bb14eb4030cf3c393bc533ef017a0f06f8622f1
aazztech <aazztechcontact@gmail.com>
2020-04-04 18:11:18 +0600

    remove repeated js stuff

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,8 +4,9 @@
     Author: Aazztech
     Author URI: www.aazztech.com
 */
+// Custom Image uploader for user dashboard page
+jQuery(function ($) {
 
-(function ($) {
 
     //sorting toggle
     $('.sorting span').on('click', function () {
@@ -579,10 +580,7 @@
     })();
     pureScriptTab('.atbd_tab');
 
-})(jQuery);
 
-// Custom Image uploader for user dashboard page
-jQuery(function ($) {
     // Set all variables to be used in scope
     var frame,
         imgContainer = $('#profile_pic_container'), // profile picture container id here
@@ -638,144 +636,133 @@ jQuery(function ($) {
     });
 
 
-});
+    // Validate forms
+    if ($.fn.validator) {
 
-(function ($) {
-    /**
-     * Called when the page has loaded.
-     *
-     * @since    1.0.0
-     */
-    $(function () {
+        // Validate report abuse form
+        var atbdp_report_abuse_submitted = false;
 
-        // Validate forms
-        if ($.fn.validator) {
+        $('#atbdp-report-abuse-form').validator({
+            disable: false
+        }).on('submit', function (e) {
 
-            // Validate report abuse form
-            var atbdp_report_abuse_submitted = false;
+            if (atbdp_report_abuse_submitted) return false;
+            atbdp_report_abuse_submitted = true;
+            // Check for errors
+            if (!e.isDefaultPrevented()) {
+                e.preventDefault();
 
-            $('#atbdp-report-abuse-form').validator({
-                disable: false
-            }).on('submit', function (e) {
+                // Post via AJAX
+                var data = {
+                    'action': 'atbdp_public_report_abuse',
+                    'post_id': $('#atbdp-post-id').val(),
+                    'message': $('#atbdp-report-abuse-message').val()
+                };
 
-                if (atbdp_report_abuse_submitted) return false;
-                atbdp_report_abuse_submitted = true;
-                // Check for errors
-                if (!e.isDefaultPrevented()) {
-                    e.preventDefault();
-
-                    // Post via AJAX
-                    var data = {
-                        'action': 'atbdp_public_report_abuse',
-                        'post_id': $('#atbdp-post-id').val(),
-                        'message': $('#atbdp-report-abuse-message').val()
-                    };
-
-                    $.post(atbdp_public_data.ajaxurl, data, function (response) {
-                        if (1 == response.error) {
-                            $('#atbdp-report-abuse-message-display').addClass('text-danger').html(response.message);
-                        } else {
-                            $('#atbdp-report-abuse-message').val('');
-                            $('#atbdp-report-abuse-message-display').addClass('text-success').html(response.message);
-                        }
+                $.post(atbdp_public_data.ajaxurl, data, function (response) {
+                    if (1 == response.error) {
+                        $('#atbdp-report-abuse-message-display').addClass('text-danger').html(response.message);
+                    } else {
+                        $('#atbdp-report-abuse-message').val('');
+                        $('#atbdp-report-abuse-message-display').addClass('text-success').html(response.message);
+                    }
 
 
-                        atbdp_report_abuse_submitted = false; // Re-enable the submit event
-                    }, 'json');
+                    atbdp_report_abuse_submitted = false; // Re-enable the submit event
+                }, 'json');
 
-                }
-            });
-
-            // Validate contact form
-            var atbdp_contact_submitted = false;
-
-            $('#atbdp-contact-form,#atbdp-contact-form-widget').validator({
-                disable: false
-            }).on('submit', function (e) {
-
-                if (atbdp_contact_submitted) return false;
-
-                // Check for errors
-                if (!e.isDefaultPrevented()) {
-
-                    e.preventDefault();
-
-                    atbdp_contact_submitted = true;
-
-
-                    $('#atbdp-contact-message-display').append('<div class="atbdp-spinner"></div>');
-
-                    // Post via AJAX
-                    var data = {
-                        'action': 'atbdp_public_send_contact_email',
-                        'post_id': $('#atbdp-post-id').val(),
-                        'name': $('#atbdp-contact-name').val(),
-                        'email': $('#atbdp-contact-email').val(),
-                        'listing_email': $('#atbdp-listing-email').val(),
-                        'message': $('#atbdp-contact-message').val(),
-                    };
-
-                    $.post(atbdp_public_data.ajaxurl, data, function (response) {
-                        if (1 == response.error) {
-                            $('#atbdp-contact-message-display').addClass('text-danger').html(response.message);
-                        } else {
-                            $('#atbdp-contact-message').val('');
-                            $('#atbdp-contact-message-display').addClass('text-success').html(response.message);
-                        }
-
-                    }, 'json');
-
-                } else {
-                    atbdp_contact_submitted = false;
-                }
-
-            });
-        }
-
-        // Report abuse [on modal closed]
-        $('#atbdp-report-abuse-modal').on('hidden.bs.modal', function (e) {
-
-            $('#atbdp-report-abuse-message').val('');
-            $('#atbdp-report-abuse-message-display').html('');
-
+            }
         });
 
-        // Contact form [on modal closed]
-        $('#atbdp-contact-modal').on('hidden.bs.modal', function (e) {
+        // Validate contact form
+        var atbdp_contact_submitted = false;
 
-            $('#atbdp-contact-message').val('');
-            $('#atbdp-contact-message-display').html('');
+        $('#atbdp-contact-form,#atbdp-contact-form-widget').validator({
+            disable: false
+        }).on('submit', function (e) {
+
+            if (atbdp_contact_submitted) return false;
+
+            // Check for errors
+            if (!e.isDefaultPrevented()) {
+
+                e.preventDefault();
+
+                atbdp_contact_submitted = true;
+
+
+                $('#atbdp-contact-message-display').append('<div class="atbdp-spinner"></div>');
+
+                // Post via AJAX
+                var data = {
+                    'action': 'atbdp_public_send_contact_email',
+                    'post_id': $('#atbdp-post-id').val(),
+                    'name': $('#atbdp-contact-name').val(),
+                    'email': $('#atbdp-contact-email').val(),
+                    'listing_email': $('#atbdp-listing-email').val(),
+                    'message': $('#atbdp-contact-message').val(),
+                };
+
+                $.post(atbdp_public_data.ajaxurl, data, function (response) {
+                    if (1 == response.error) {
+                        $('#atbdp-contact-message-display').addClass('text-danger').html(response.message);
+                    } else {
+                        $('#atbdp-contact-message').val('');
+                        $('#atbdp-contact-message-display').addClass('text-success').html(response.message);
+                    }
+
+                }, 'json');
+
+            } else {
+                atbdp_contact_submitted = false;
+            }
 
         });
+    }
 
+    // Report abuse [on modal closed]
+    $('#atbdp-report-abuse-modal').on('hidden.bs.modal', function (e) {
 
-        // Alert users to login (only if applicable)
-        $('.atbdp-require-login').on('click', function (e) {
+        $('#atbdp-report-abuse-message').val('');
+        $('#atbdp-report-abuse-message-display').html('');
 
-            e.preventDefault();
-            alert(atbdp_public_data.login_alert_message);
-
-        });
-
-        // Add or Remove from favourites
-        $('#atbdp-favourites').on('click', function (e) {
-
-            //e.preventDefault();
-            var data = {
-                'action': 'atbdp_public_add_remove_favorites',
-                'post_id': $("a.atbdp-favourites").data('post_id')
-            };
-            $.post(atbdp_public_data.ajaxurl, data, function (response) {
-                $('#atbdp-favourites').html(response);
-            });
-
-        });
-
-        /*$('#atbdp_tabs a').click(function(e) {
-            e.preventDefault();
-            $(this).tab('show');
-        });*/
     });
+
+    // Contact form [on modal closed]
+    $('#atbdp-contact-modal').on('hidden.bs.modal', function (e) {
+
+        $('#atbdp-contact-message').val('');
+        $('#atbdp-contact-message-display').html('');
+
+    });
+
+
+    // Alert users to login (only if applicable)
+    $('.atbdp-require-login').on('click', function (e) {
+
+        e.preventDefault();
+        alert(atbdp_public_data.login_alert_message);
+
+    });
+
+    // Add or Remove from favourites
+    $('#atbdp-favourites').on('click', function (e) {
+
+        //e.preventDefault();
+        var data = {
+            'action': 'atbdp_public_add_remove_favorites',
+            'post_id': $("a.atbdp-favourites").data('post_id')
+        };
+        $.post(atbdp_public_data.ajaxurl, data, function (response) {
+            $('#atbdp-favourites').html(response);
+        });
+
+    });
+
+    /*$('#atbdp_tabs a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });*/
 
     $("#recover-pass-modal").hide();
     $(".atbdp_recovery_pass").on("click", function (e) {
@@ -900,7 +887,9 @@ jQuery(function ($) {
             $(".atbd_dropdown").removeClass("atbd_drop--active");
         }
     })
-})(jQuery);
+
+
+});
 
 // on load of the page: switch to the currently selected tab
 var tab_url = window.location.href.split("/").pop();

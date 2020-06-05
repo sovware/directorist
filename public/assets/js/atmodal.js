29190d804ebf387300ceabd3d8464c147a7e8b57
Syed Galib <syedgalib.dev@outlook.com>
2020-06-05 15:00:51 +0600

    fixed event delegation issue

--- public/assets/js/atmodal.js
+++ public/assets/js/atmodal.js
@@ -4,55 +4,59 @@
 	Author: AazzTech
 	Author URI: http://aazztech.com
 */
+
+const aazztechModal1 = (selector) => {
+	const element = document.querySelectorAll(selector);
+
+	element.forEach(function (el, index) {
+		el.style.display = 'none';
+		const item = document.querySelectorAll(`*[data-target="${el.getAttribute('id')}"]`);
+		
+		document.addEventListener('click', function (event) {
+			const item = document.querySelectorAll(`*[data-target="${el.getAttribute('id')}"]`);
+			if ( ! item.length ) { return; }
+			
+			const first_item = item[0];
+			const class_name = first_item.className;
+			
+			if (event.target.matches('.' + class_name )) {
+				event.preventDefault();
+				el.style.display = 'block';
+				document.body.classList.add("atm-open");
+				setTimeout(function () {
+					el.classList.add("atm-show");
+				}, 100);
+				document.querySelector("html").style.overflow = "hidden";
+			}
+		}, false);
+		el.querySelector('a.at-modal-close').addEventListener('click', (e) => {
+			e.preventDefault();
+			el.classList.remove("atm-show");
+			document.body.classList.remove("atm-open");
+			setTimeout(function () {
+				el.style.display = 'none';
+			}, 100);
+			document.querySelector("html").removeAttribute("style");
+		});
+		el.addEventListener('click', function (e) {
+			if (e.target.closest('.atm-contents-inner'))
+				return;
+			el.classList.remove("atm-show");
+			document.body.classList.remove("atm-open");
+			setTimeout(function () {
+				el.style.display = 'none';
+			}, 100);
+			document.querySelector("html").removeAttribute("style");
+		})
+	});
+};
+
+function initModal() {
+	aazztechModal1('#dcl-claim-modal, #atbdp-report-abuse-modal, #atpp-plan-change-modal, #pyn-plan-change-modal');
+}
+
 window.addEventListener("load", function () {
     setTimeout(function () {
-        const aazztechModal1 = (selector) => {
-            const element = document.querySelectorAll(selector);
-            element.forEach(function (el, index) {
-
-                el.style.display = 'none';
-
-                const item = document.querySelectorAll(`*[data-target="${el.getAttribute('id')}"]`);
-				if ( ! item.length ) { return; }
-				
-				const first_item = item[0];
-				const class_name = first_item.className;
-				
-				document.addEventListener('click', function (event) {
-					if (event.target.matches('.' + class_name )) {
-						event.preventDefault();
-						el.style.display = 'block';
-                        document.body.classList.add("atm-open");
-                        setTimeout(function () {
-                            el.classList.add("atm-show");
-                        }, 100);
-                        document.querySelector("html").style.overflow = "hidden";
-						
-					}
-				}, false);
-
-                el.querySelector('a.at-modal-close').addEventListener('click', (e) => {
-                    e.preventDefault();
-                    el.classList.remove("atm-show");
-                    document.body.classList.remove("atm-open");
-                    setTimeout(function () {
-                        el.style.display = 'none';
-                    }, 100);
-                    document.querySelector("html").removeAttribute("style");
-                });
-                el.addEventListener('click', function (e) {
-                    if (e.target.closest('.atm-contents-inner'))
-                        return;
-                    el.classList.remove("atm-show");
-                    document.body.classList.remove("atm-open");
-                    setTimeout(function () {
-                        el.style.display = 'none';
-                    }, 100);
-                    document.querySelector("html").removeAttribute("style");
-                })
-
-            });
-        };
-        aazztechModal1('#dcl-claim-modal, #atbdp-report-abuse-modal, #atpp-plan-change-modal, #pyn-plan-change-modal');
+        initModal();
     },500);
 });

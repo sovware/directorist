0b0de18e2e621129d7d6bdde70aacd528a19556e
shamimsaj <shamimeforb@gmail.com>
2020-06-09 14:19:06 +0600

    updated atmodal js

--- public/assets/js/atmodal.js
+++ public/assets/js/atmodal.js
@@ -5,46 +5,60 @@
 	Author URI: http://aazztech.com
 */
 
-const aazztechModal = (selector) => {
-	const element = document.querySelectorAll(selector);
-	element.forEach(function (el, index) {
-		el.style.display = 'none';
+const aazztechModal1 = selector => {
+        const element = document.querySelectorAll(selector);
 
-		const item = document.querySelectorAll(`*[data-target="${el.getAttribute('id')}"]`);
-		item.forEach(function (clickitem, index) {
-			clickitem.style.cursor = 'pointer';
-			clickitem.addEventListener('click', (e) => {
-				e.preventDefault();
-				el.style.display = 'block';
-				document.body.classList.add("atm-open");
-				setTimeout(function () {
-					el.classList.add("atm-show");
-				}, 100);
-				document.querySelector("html").style.overflow = "hidden";
-			});
-		});
+        element.forEach(function(el, index) {
+                el.style.display = 'none';
 
-		el.querySelector('a.at-modal-close').addEventListener('click', (e) => {
-			e.preventDefault();
-			el.classList.remove("atm-show");
-        	document.body.classList.remove("atm-open");
-			setTimeout(function () {
-				el.style.display = 'none';
-			}, 100);
-        	document.querySelector("html").removeAttribute("style");
-		});
-		el.addEventListener('click', function (e) {
-			if (e.target.closest('.atm-contents-inner'))
-				return;
-			el.classList.remove("atm-show");
-            document.body.classList.remove("atm-open");
-			setTimeout(function () {
-				el.style.display = 'none';
-			}, 100);
-            document.querySelector("html").removeAttribute("style");
-		})
+                document.addEventListener(
+                        'click',
+                        function(event) {
+                                const current_elm = event.target;
+                                const target_id = current_elm.getAttribute('data-target');
+                                const el_id = el.getAttribute('id');
 
-	});
+                                if (target_id === el_id) {
+                                        event.preventDefault();
+                                        el.style.display = 'block';
+                                        document.body.classList.add('atm-open');
+                                        setTimeout(function() {
+                                                el.classList.add('atm-show');
+                                        }, 100);
+                                        document.querySelector('html').style.overflow = 'hidden';
+                                }
+                        },
+                        false
+                );
+
+                el.querySelector('a.at-modal-close').addEventListener('click', e => {
+                        e.preventDefault();
+                        el.classList.remove('atm-show');
+                        document.body.classList.remove('atm-open');
+                        setTimeout(function() {
+                                el.style.display = 'none';
+                        }, 100);
+                        document.querySelector('html').removeAttribute('style');
+                });
+
+                el.addEventListener('click', function(e) {
+                        if (e.target.closest('.atm-contents-inner')) return;
+                        el.classList.remove('atm-show');
+                        document.body.classList.remove('atm-open');
+                        setTimeout(function() {
+                                el.style.display = 'none';
+                        }, 100);
+                        document.querySelector('html').removeAttribute('style');
+                });
+        });
 };
-aazztechModal('#dcl-claim-modal, #atbdp-report-abuse-modal, #atpp-plan-change-modal, #pyn-plan-change-modal, #atbdp-single-popup, #atbdp-reg-terms-modal');
 
+function initModal() {
+        aazztechModal1('#dcl-claim-modal, #atbdp-report-abuse-modal, #atpp-plan-change-modal, #pyn-plan-change-modal');
+}
+
+window.addEventListener('load', function() {
+        setTimeout(function() {
+                initModal();
+        }, 500);
+});

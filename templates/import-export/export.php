eaca8eb1d788f14203547be1b2859d40c8d7b309
vairafiq <vairafiq@gmail.com>
2020-07-03 16:13:03 +0600

    refacuted csv

--- templates/import-export/export.php
+++ templates/import-export/export.php
@@ -5,5 +5,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+$download_link = 'https://directorist.com/wp-content/uploads/2020/07/dummy.zip';
 ?>
-<button type="submit" class="button button-primary button-next" value="<?php esc_attr_e( 'Download a simple CSV', 'directorist' ); ?>" name="save_step"><?php esc_html_e( 'Download a simple CSV', 'directorist' ); ?></button>
+<button type="submit" class="button button-primary button-next" value="<?php esc_attr_e( 'Download a simple CSV', 'directorist' ); ?>" name="save_step">
+<a href="<?php echo esc_url($download_link); ?>"><?php esc_html_e( 'Download a simple CSV', 'directorist' ); ?></a>
+</button>

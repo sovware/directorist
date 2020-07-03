e647486ad08444a499dfeb5ec32ae1a92c3d4d7a
shamimsaj <shamimeforb@gmail.com>
2020-07-03 20:27:29 +0600

    csv design updated

--- templates/tools.php
+++ templates/tools.php
@@ -18,10 +18,31 @@ if (!empty($_GET['tab'])) {
 }
 $url = admin_url() . 'edit.php?post_type=at_biz_dir&page=tools&tab=csv_import';
 ?>
-<h2 class="nav-tab-wrapper">
-	<a class="nav-tab <?php echo 'csv_import' == $current_tab ? 'nav-tab-active' : ''; ?>" href="<?php echo esc_url($url); ?>"><span class="dashicons dashicons-download"></span> <?php _ex('Import', 'admin csv', 'directorist'); ?></a>
-	<a class="nav-tab <?php echo 'csv_export' == $current_tab ? 'nav-tab-active' : ''; ?>" href="<?php echo esc_url(add_query_arg('tab', 'csv_export')); ?>"><span class="dashicons dashicons-upload"></span> <?php _ex('Export', 'admin csv', 'directorist'); ?></a>
-</h2>
+<div class="csv-action-btns">
+	<a class="<?php echo 'csv_import' == $current_tab ? 'btn-active' : ''; ?>" href="<?php echo esc_url($url); ?>"><span class="dashicons dashicons-download"></span> <?php _ex('Import', 'admin csv', 'directorist'); ?></a>
+	<a class="<?php echo 'csv_export' == $current_tab ? 'btn-active' : ''; ?>" href="<?php echo esc_url(add_query_arg('tab', 'csv_export')); ?>"><span class="dashicons dashicons-upload"></span> <?php _ex('Export', 'admin csv', 'directorist'); ?></a>
+</div>
+
+<div class="csv-action-steps">
+	<ul>
+		<li class="active">
+			<span class="step">1</span>
+			<span>Upload CSV File</span>
+		</li>
+		<li>
+			<span class="step">2</span>
+			<span>Column Mapping</span>
+		</li>
+		<li>
+			<span class="step">3</span>
+			<span>Import</span>
+		</li>
+		<li>
+			<span class="step">4</span>
+			<span>Done</span>
+		</li>
+	</ul>
+</div>
 
 <?php if ('csv_import' == $current_tab) { ?>
 

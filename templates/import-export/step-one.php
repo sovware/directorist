d3e10f585270ccd4bcad9d746d8fed1e253b4e64
shamimsaj <shamimeforb@gmail.com>
2020-07-06 08:04:57 +0600

    csv style updated

--- templates/import-export/step-one.php
+++ templates/import-export/step-one.php
@@ -10,68 +10,67 @@ $size       = size_format($bytes);
 $upload_dir = wp_upload_dir();
 ?>
 <div class="csv-wrapper">
-<form class="atbdp-progress-form-content directorist-importer" id="atbdp_csv_step_one" enctype="multipart/form-data" method="post">
-	<header>
-		<h2><?php esc_html_e( 'Select CSV File', 'directorist' ); ?></h2>
-		<p><?php esc_html_e( 'This tool allows you to import (or merge) listing data to your store from a CSV file.', 'directorist' ); ?></p>
-	</header>
-	<div class="form-content">
-		<section>
-			<div class="form-table directorist-importer-options">
-				<h4 for="upload">
-					<?php esc_html_e( 'Choose a CSV file from your computer:', 'directorist' ); ?>
-				</h4>
-				<div>
-					<?php
-					if ( ! empty( $upload_dir['error'] ) ) {
-						?>
-						<div class="inline error">
-							<p><?php esc_html_e( 'Before you can upload your import file, you will need to fix the following error:', 'directorist' ); ?></p>
-							<p><strong><?php echo esc_html( $upload_dir['error'] ); ?></strong></p>
-						</div>
-						<?php
-					} else {
-						?>
-						<div class="csv-upload">
-							<input type="file" id="upload" name="import" required size="25" />
-							<label for="upload"><span class="upload-btn">Upload CSV</span> <span class="file-name">No file chosen</span></label>
-							<small>
-								<?php
-								printf(
-									/* translators: %s: maximum upload size */
-									esc_html__( 'Maximum size: %s', 'directorist' ),
-									esc_html( $size )
-								);
-								?>
-							</small>
-						</div>
-						<input type="hidden" name="action" value="save" />
-						<input type="hidden" name="max_file_size" value="<?php echo esc_attr( $bytes ); ?>" />
-						<?php
-					}
-					?>
-				</div>
-				<div class="update-existing">
+	<form class="atbdp-progress-form-content directorist-importer" id="atbdp_csv_step_one" enctype="multipart/form-data" method="post">
+		<header>
+			<h2><?php esc_html_e( 'Select CSV File', 'directorist' ); ?></h2>
+			<p><?php esc_html_e( 'This tool allows you to import (or merge) listing data to your store from a CSV file.', 'directorist' ); ?></p>
+		</header>
+		<div class="form-content">
+			<section>
+				<div class="form-table directorist-importer-options">
+					<h4 for="upload">
+						<?php esc_html_e( 'Choose a CSV file from your computer:', 'directorist' ); ?>
+					</h4>
 					<div>
-						<label for="directorist-importer-update-existing" class="ue">
-							<?php esc_html_e( 'Update existing listings', 'directorist' ); ?>
-						</label>
-						<input type="hidden" name="update_existing" value="0" />
-						<input type="checkbox" id="directorist-importer-update-existing" name="update_existing" value="1" />
-						<label for="directorist-importer-update-existing"><?php esc_html_e( 'Existing listings that match by ID will be updated. listings that do not exist will be skipped.', 'directorist' ); ?></label>
+						<?php
+						if ( ! empty( $upload_dir['error'] ) ) {
+							?>
+							<div class="inline error">
+								<p><?php esc_html_e( 'Before you can upload your import file, you will need to fix the following error:', 'directorist' ); ?></p>
+								<p><strong><?php echo esc_html( $upload_dir['error'] ); ?></strong></p>
+							</div>
+							<?php
+						} else {
+							?>
+							<div class="csv-upload">
+								<input type="file" id="upload" name="import" required size="25" />
+								<label for="upload"><span class="upload-btn">Upload CSV</span> <span class="file-name">No file chosen</span></label>
+								<small>
+									<?php
+									printf(
+										/* translators: %s: maximum upload size */
+										esc_html__( 'Maximum size: %s', 'directorist' ),
+										esc_html( $size )
+									);
+									?>
+								</small>
+							</div>
+							<input type="hidden" name="action" value="save" />
+							<input type="hidden" name="max_file_size" value="<?php echo esc_attr( $bytes ); ?>" />
+							<?php
+						}
+						?>
+					</div>
+					<div class="update-existing">
+						<div>
+							<label for="directorist-importer-update-existing" class="ue">
+								<?php esc_html_e( 'Update existing listings', 'directorist' ); ?>
+							</label>
+							<input type="hidden" name="update_existing" value="0" />
+							<input type="checkbox" id="directorist-importer-update-existing" name="update_existing" value="1" />
+							<label for="directorist-importer-update-existing"><?php esc_html_e( 'Existing listings that match by ID will be updated. listings that do not exist will be skipped.', 'directorist' ); ?></label>
+						</div>
+					</div>
+					<div class="csv-delimiter">
+						<label><?php esc_html_e( 'CSV Delimiter', 'directorist' ); ?></label>
+						<input type="text" name="delimiter" placeholder="," size="2" />
 					</div>
 				</div>
-				<div class="csv-delimiter">
-					<label><?php esc_html_e( 'CSV Delimiter', 'directorist' ); ?></label>
-					<input type="text" name="delimiter" placeholder="," size="2" />
-				</div>
-			</div>
-		</section>
-	</div>
-	<div class="atbdp-actions">
-		<button type="submit" class="button" value="<?php esc_attr_e( 'Continue', 'directorist' ); ?>" name="atbdp_save_csv_step"><?php esc_html_e( 'Continue', 'directorist' ); ?></button>
-		<?php wp_nonce_field( 'directorist-csv-importer' ); ?>
-	</div>
-</form>
-
+			</section>
+		</div>
+		<div class="atbdp-actions">
+			<button type="submit" class="button" value="<?php esc_attr_e( 'Continue', 'directorist' ); ?>" name="atbdp_save_csv_step"><?php esc_html_e( 'Continue', 'directorist' ); ?></button>
+			<?php wp_nonce_field( 'directorist-csv-importer' ); ?>
+		</div>
+	</form>
 </div>

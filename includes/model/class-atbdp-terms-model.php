63e75d5311d698b89feb2e11e37a190bc4122f30
Syed Galib Ahmed <aazztech13@gmail.com>
2020-07-19 18:09:51 +0600

    improved

--- includes/model/class-atbdp-terms-model.php
+++ includes/model/class-atbdp-terms-model.php
@@ -3,7 +3,9 @@ if ( ! class_exists( 'ATBDP_Terms_Model' ) ) :
     class ATBDP_Terms_Model {
         // get_categories_term
         public static function get_categories_term( array $args = [] ) {
-            return ATBDP_Cache_Helper::get_the_transient([
+            return get_terms( ATBDP_CATEGORY, $args );
+
+            /* return ATBDP_Cache_Helper::get_the_transient([
                 'group' => 'atbdp_category_terms',
                 'name'  => 'atbdp_term_categories',
                 'args'  => $args,
@@ -11,12 +13,14 @@ if ( ! class_exists( 'ATBDP_Terms_Model' ) ) :
                 'value' => function( $data ) {
                     return get_terms( ATBDP_CATEGORY, $data['args'] );
                 }
-            ]);
+            ]); */
         }
 
         // get_categories_taxanomy
         public static function get_categories_taxanomy( array $args = [] ) {
-            return ATBDP_Cache_Helper::get_the_transient([
+            return get_categories( $args );
+
+            /* return ATBDP_Cache_Helper::get_the_transient([
                 'group'  => 'atbdp_category_terms',
                 'name'   => 'atbdp_taxonomy_categories',
                 'args'   => $args,
@@ -24,12 +28,14 @@ if ( ! class_exists( 'ATBDP_Terms_Model' ) ) :
                 'value'  => function( $data ) {
                     return get_categories( $data['args'] );
                 }
-            ]);
+            ]); */
         }
 
         // get_locations_term
         public static function get_locations_term( array $args = [] ) {
-            return ATBDP_Cache_Helper::get_the_transient([
+            return get_terms( ATBDP_LOCATION, $args );
+
+          /*   return ATBDP_Cache_Helper::get_the_transient([
                 'group' => 'atbdp_location_terms',
                 'name'  => 'atbdp_locations',
                 'args'  => $args,
@@ -37,12 +43,14 @@ if ( ! class_exists( 'ATBDP_Terms_Model' ) ) :
                 'value' => function( $data ) {
                     return get_terms( ATBDP_LOCATION, $data['args'] );
                 }
-            ]);
+            ]); */
         }
         
         // get_tags_term
         public static function get_tags_term( array $args = [] ) {
-            return ATBDP_Cache_Helper::get_the_transient([
+            return get_terms( ATBDP_TAGS, $args );
+
+            /* return ATBDP_Cache_Helper::get_the_transient([
                 'group' => 'atbdp_tag_terms',
                 'name'  => 'atbdp_listings_tags',
                 'args'  => $args,
@@ -50,7 +58,7 @@ if ( ! class_exists( 'ATBDP_Terms_Model' ) ) :
                 'value' => function( $data ) {
                     return get_terms( ATBDP_TAGS, $data['args'] );
                 }
-            ]);
+            ]); */
         }
     }
 endif;

6a4925e5782fcee37bc13d7456457dba590ed81b
Syed Galib <aazztech13@gmail.com>
2020-07-28 08:53:49 +0600

    improved

--- includes/classes/class-enqueue-assets.php
+++ includes/classes/class-enqueue-assets.php
@@ -2,6 +2,16 @@
 
 if ( ! class_exists( 'ATBDP_Enqueue_Assets' ) ):
     class ATBDP_Enqueue_Assets {
-        
+
+        public function __construct()
+        {
+            $this->load_assets();
+        }
+
+        // load_assets
+        protected function load_assets()
+        {
+            # code...
+        }
     }
 endif;

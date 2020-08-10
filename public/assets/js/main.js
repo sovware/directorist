8e82dbf6dd8f8f0fb6e87a265dae7a1bf5235d77
shamimsaj <shamimeforb@gmail.com>
2020-08-10 07:42:10 +0600

    commit changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -860,18 +860,21 @@
     });
 
     /* atbd tooltip */
-    var atbd_tooltip = document.querySelectorAll('.atbd_tooltip');
-    atbd_tooltip.forEach(function(el){
-        if(el.getAttribute('aria-label') !== " "){
-            document.body.addEventListener('mouseover', function(e) {
-                for (var target = e.target; target && target != this; target = target.parentNode) {
-                    if (target.matches('.atbd_tooltip')) {
-                        el.classList.add('atbd_tooltip_active');
+    function atbdp_tooltip(){
+        var atbd_tooltip = document.querySelectorAll('.atbd_tooltip');
+        atbd_tooltip.forEach(function(el){
+            if(el.getAttribute('aria-label') !== " "){
+                document.body.addEventListener('mouseover', function(e) {
+                    for (var target = e.target; target && target != this; target = target.parentNode) {
+                        if (target.matches('.atbd_tooltip')) {
+                            el.classList.add('atbd_tooltip_active');
+                        }
                     }
-                }
-            }, false);
-        }
-    });
+                }, false);
+            }
+        });
+    }
+    atbdp_tooltip();
 
     /* User Dashboard tab */
     $(function () {

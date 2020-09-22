2ca3f532ac8a1a7d7b2ec7dc48304e514001e92f
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-09-22 07:31:56 +0600

    card

--- templates/public-templates/shortcodes/listings-archive/loop/card.php
+++ templates/public-templates/shortcodes/listings-archive/loop/card.php
@@ -7,24 +7,30 @@
 
 $card_data = $listings->loop['directorist_options']['card_fields'];
 ?>
-<!-- listings-archive > loop > card -->
 <div class="atbd_single_listing atbd_listing_card <?php echo esc_attr( $listings->info_display_in_single_line ); ?>">
 	<article class="atbd_single_listing_wrapper <?php echo esc_attr( $listings->loop_wrapper_class() ); ?>">
 		
 		<?php if ( $listings->display_preview_image ): ?>
 			<figure class="atbd_listing_thumbnail_area">
-				<div class="atbd-cover-top-left"></div>
-				<div class="atbd-cover-top-right"></div>
-				<div class="atbd-cover-bottom-left"></div>
-				<div class="atbd-cover-bottom-right"></div>
+				<div class="directorist-cover-top-left"></div>
+				<div class="directorist-cover-top-right"></div>
+				<div class="directorist-cover-bottom-left"></div>
+				<div class="directorist-cover-bottom-right"></div>
 			</figure>
 		<?php endif; ?>
 
 		<div class="atbd_listing_info">
-			<?php
-			$listings->loop_top_content_template();
-			$listings->loop_grid_bottom_content_template();
-			?>
+			<div class="directorist-card-content-top"></div>
+			<div class="directorist-card-content-after-top"></div>
+			<div class="directorist-card-content-meta">
+				<ul>
+					<li></li>
+				</ul>
+			</div>
+			<div class="directorist-card-footer">
+				<div class="directorist-card-footer-left"></div>
+				<div class="directorist-card-footer-right"></div>
+			</div>
 		</div>
 		
 	</article>

0d0adb35b10537f72fa1796eefe053da3c46ab96
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-09-22 10:01:36 +0600

    fixed error

---
+++ templates/public-templates/shortcodes/listings-archive-new/loop/grid-thumbnail.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author  AazzTech
+ * @since   6.6
+ * @version 6.6
+ */
+?>
+<!-- listings-archive > loop > card -->
+<div class="atbd_listing_image">
+	
+	<?php
+	$listings->loop_thumb_card_template();
+
+	if ($listings->display_author_image) { ?>
+		<div class="atbd_author"><?php $listings->loop_author_template(); ?></div>
+		<?php
+	}
+	?>
+</div>
+
+<?php
+/**
+ * @since 5.0
+ */
+?>
+<span class="atbd_upper_badge bh_only"><?php echo apply_filters('atbdp_upper_badges', '');?></span>
+
+<?php
+/**
+ * @since 5.0
+ * @hooked Directorist_Listings::featured_badge - 10
+ * @hooked Directorist_Listings::popular_badge - 15
+ * @hooked Directorist_Listings::new_listing_badge - 20
+ */
+?>
+<span class="atbd_lower_badge"><?php echo apply_filters('atbdp_grid_lower_badges', '');?></span>
+
+<?php
+/**
+ * @since 6.6
+ * @hooked Directorist_Listings::mark_as_favourite_button - 15
+ * @hooked CompareButton() || display_compare_btn_on_grid_listings || from extension- 10
+ */
+do_action('atbdp_listing_thumbnail_area');
--- templates/public-templates/shortcodes/listings-archive/loop/card.php
+++ templates/public-templates/shortcodes/listings-archive/loop/card.php
@@ -4,33 +4,22 @@
  * @since   6.6
  * @version 6.6
  */
-
-$card_data = $listings->loop['directorist_options']['card_fields'];
 ?>
+<!-- listings-archive > loop > card -->
 <div class="atbd_single_listing atbd_listing_card <?php echo esc_attr( $listings->info_display_in_single_line ); ?>">
 	<article class="atbd_single_listing_wrapper <?php echo esc_attr( $listings->loop_wrapper_class() ); ?>">
 		
 		<?php if ( $listings->display_preview_image ): ?>
 			<figure class="atbd_listing_thumbnail_area">
-				<div class="directorist-cover-top-left"></div>
-				<div class="directorist-cover-top-right"></div>
-				<div class="directorist-cover-bottom-left"></div>
-				<div class="directorist-cover-bottom-right"></div>
+				<?php $listings->loop_grid_thumbnail_template();?>
 			</figure>
 		<?php endif; ?>
 
 		<div class="atbd_listing_info">
-			<div class="directorist-card-content-top"></div>
-			<div class="directorist-card-content-after-top"></div>
-			<div class="directorist-card-content-meta">
-				<ul>
-					<li></li>
-				</ul>
-			</div>
-			<div class="directorist-card-footer">
-				<div class="directorist-card-footer-left"></div>
-				<div class="directorist-card-footer-right"></div>
-			</div>
+			<?php
+			$listings->loop_top_content_template();
+			$listings->loop_grid_bottom_content_template();
+			?>
 		</div>
 		
 	</article>

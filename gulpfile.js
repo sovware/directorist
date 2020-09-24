3a1b9c2083f38db28c678694e31136efd4bea498
tanjimhasan <tanjim.hasan.pro@gmail.com>
2020-09-24 12:34:48 +0600

    markup update

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,100 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
-    sourcemap = require('gulp-sourcemaps'),
-        rtlcss = require("gulp-rtlcss"),
-        rename = require("gulp-rename"),
-        gulpfilter = require('gulp-filter');
-
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemap = require("gulp-sourcemaps"),
+  autoPrefixer = require("gulp-autoprefixer"),
+  rtlcss = require("gulp-rtlcss"),
+  rename = require("gulp-rename"),
+  gulpfilter = require("gulp-filter");
 
 /* sass compiler */
-function compileSass(src, dest){
-    return function(){
-        gulp.src(src)
-            .pipe(sourcemap.init())
-            .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-            .pipe(sourcemap.write('map'))
-            .pipe(gulp.dest(dest))
-    }
+function compileSass(src, dest) {
+  return async function () {
+    gulp
+      .src(src)
+      .pipe(sourcemap.init())
+      .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+      .pipe(autoPrefixer("last 10 versions"))
+      .pipe(sourcemap.write("map"))
+      .pipe(gulp.dest(dest));
+  };
 }
 
 // compile bootstrap
-gulp.task('bs', compileSass('public/assets/bootstrap/bootstrap.scss', 'public/assets/css/'));
-gulp.task('style', compileSass('public/assets/scss/style.scss','public/assets/css/'));
-gulp.task('admin', compileSass('public/assets/scss/admin/style.scss','admin/assets/css/'));
+gulp.task(
+  "bs",
+  compileSass("public/assets/bootstrap/bootstrap.scss", "public/assets/css/")
+);
+gulp.task(
+  "style",
+  compileSass("public/assets/scss/style.scss", "public/assets/css/")
+);
+gulp.task(
+  "admin",
+  compileSass("public/assets/scss/admin/style.scss", "admin/assets/css/")
+);
 
 // default gulp task\
-gulp.task('default',['bs', 'style', 'admin'], function(){
-    gulp.watch('public/assets/bootstrap/*.scss', ['bs']);
-    gulp.watch('public/assets/scss/**/*.scss', ['style']);
-    gulp.watch('public/assets/scss/admin/**/*.scss', ['admin']);
-});
+
+gulp.task(
+  "default",
+  gulp.series("bs", "style", "admin", function (done) {
+    gulp.watch("public/assets/bootstrap/*.scss", gulp.series("bs"));
+    gulp.watch("public/assets/scss/**/*.scss", gulp.series("style"));
+    gulp.watch(["public/assets/scss/admin/**/*.scss"], gulp.series("admin"));
+    done();
+  })
+);
 
 //rtl css generator
-gulp.task('rtl', function () {
-    var bootstrap = gulpfilter('**/bootstrap.css', {restore: true}),
-        style = gulpfilter('**/style.css', {restore: true}),
-        search_style = gulpfilter('**/search-style.css', {restore: true});
+gulp.task("rtl", function (done) {
+  var bootstrap = gulpfilter("**/bootstrap.css", { restore: true }),
+    style = gulpfilter("**/style.css", { restore: true }),
+    search_style = gulpfilter("**/search-style.css", { restore: true });
 
-    gulp.src(['public/assets/css/bootstrap.css', 'public/assets/css/style.css', 'public/assets/css/search-style.css'])
-        .pipe(rtlcss({
-            'stringMap': [
-                {
-                    'name': 'left-right',
-                    'priority': 100,
-                    'search': ['left', 'Left', 'LEFT'],
-                    'replace': ['right', 'Right', 'RIGHT'],
-                    'options': {
-                        'scope': '*',
-                        'ignoreCase': false
-                    }
-                },
-                {
-                    'name': 'ltr-rtl',
-                    'priority': 100,
-                    'search': ['ltr', 'Ltr', 'LTR'],
-                    'replace': ['rtl', 'Rtl', 'RTL'],
-                    'options': {
-                        'scope': '*',
-                        'ignoreCase': false
-                    }
-                }
-            ]
-        }))
-        .pipe(bootstrap)
-        .pipe(rename({suffix: '-rtl', extname: '.css'}))
-        .pipe(gulp.dest('public/assets/css/'))
-        .pipe(bootstrap.restore)
+  gulp
+    .src([
+      "public/assets/css/bootstrap.css",
+      "public/assets/css/style.css",
+      "public/assets/css/search-style.css",
+    ])
+    .pipe(
+      rtlcss({
+        stringMap: [
+          {
+            name: "left-right",
+            priority: 100,
+            search: ["left", "Left", "LEFT"],
+            replace: ["right", "Right", "RIGHT"],
+            options: {
+              scope: "*",
+              ignoreCase: false,
+            },
+          },
+          {
+            name: "ltr-rtl",
+            priority: 100,
+            search: ["ltr", "Ltr", "LTR"],
+            replace: ["rtl", "Rtl", "RTL"],
+            options: {
+              scope: "*",
+              ignoreCase: false,
+            },
+          },
+        ],
+      })
+    )
+    .pipe(bootstrap)
+    .pipe(rename({ suffix: "-rtl", extname: ".css" }))
+    .pipe(gulp.dest("public/assets/css/"))
+    .pipe(bootstrap.restore)
 
-        .pipe(style)
-        .pipe(rename({suffix: '-rtl', extname: '.css'}))
-        .pipe(gulp.dest('public/assets/css/'))
-        .pipe(style.restore)
+    .pipe(style)
+    .pipe(rename({ suffix: "-rtl", extname: ".css" }))
+    .pipe(gulp.dest("public/assets/css/"))
+    .pipe(style.restore)
 
-        .pipe(search_style)
-        .pipe(rename({suffix: '-rtl', extname: '.css'}))
-        .pipe(gulp.dest('public/assets/css/'))
-        .pipe(search_style.restore)
+    .pipe(search_style)
+    .pipe(rename({ suffix: "-rtl", extname: ".css" }))
+    .pipe(gulp.dest("public/assets/css/"))
+    .pipe(search_style.restore);
 });
--- includes/system-status/template.php
+++ includes/system-status/template.php
@@ -1,1 +1,79 @@
-<div class='test'>hi</div>
+<section class="atbds_wrapper">
+    <div class="row">
+        <div class="col-lg-3">
+            <aside class="atbds_sidebar">
+                <ul class="nav" class="atbds_status-nav" id="atbds_status-tab" role="tablist">
+                    <li class="nav-item">
+                        <a href="#atbds_system-info" class="nav-link active" id="atbds_system-info-tab" data-toggle="tab" role="tab" aria-controls="atbds_system-info" aria-selected="true">System Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#atbds_warning" class="nav-link" id="atbds_warning-tab" data-toggle="tab" role="tab" aria-controls="atbds_warning" aria-selected="false">Warning</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#atbds_support" class="nav-link" id="atbds_support-tab" data-toggle="tab" role="tab" aria-controls="atbds_support" aria-selected="false">Support</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Remote Viewing</a>
+                    </li>
+                </ul>
+            </aside>
+        </div>
+        <div class="col-lg-9">
+            <div class="atbds_content">
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="atbds_system-info" role="tabpanel" aria-labelledby="atbds_system-info-tab">
+                        <div class="card atbds_card">
+                            <div class="card-head">
+                                <h4>System Information</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="atbds_content__tab">
+                                    <div class="atbds_c-t-menu">
+                                        <ul class="nav" id="atbds_ststus-tab" role="tablist">
+                                            <li class="nav-item">
+                                                <a href="#atbds_system-info" class="nav-link active" id="atbds_system-info-tab" data-toggle="tab" role="tab" aria-controls="atbds_system-info" aria-selected="true">Wordpress Environment</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_warning" class="nav-link" id="atbds_warning-tab" data-toggle="tab" role="tab" aria-controls="atbds_warning" aria-selected="false">PHP</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_support" class="nav-link" id="atbds_support-tab" data-toggle="tab" role="tab" aria-controls="atbds_support" aria-selected="false">Server Environment</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">User Platform</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Settings</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Database</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Post Type Counts</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Sequrity</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Active Plugins <span class="atbds__pl-count">(2)</span></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Theme</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#atbds_r-viewing" class="nav-link" id="atbds_r-viewing-tab" data-toggle="tab" role="tab" aria-controls="atbds_r-viewing" aria-selected="false">Templates</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="atbds_warning" role="tabpanel" aria-labelledby="atbds_warning-tab">...</div>
+                    <div class="tab-pane fade" id="atbds_support" role="tabpanel" aria-labelledby="atbds_support-tab">...</div>
+                    <div class="tab-pane fade" id="atbds_r-viewing" role="tabpanel" aria-labelledby="atbds_r-viewing-tab">...</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

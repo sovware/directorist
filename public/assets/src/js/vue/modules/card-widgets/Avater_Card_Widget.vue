2a69eff1740d97f05f3b11944913a082bb263c87
Syed Galib Ahmed <aazztech13@gmail.com>
2020-10-11 23:24:20 +0600

    improved listing card widget

---
+++ public/assets/src/js/vue/modules/card-widgets/Avater_Card_Widget.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="cptm-widget-card">
+        <p class="cptm-placeholder-author-thumb">
+            <img src="https://via.placeholder.com/150" alt="">
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'avater-card-widget',
+    props: {
+        label: {
+            type: String,
+        },
+
+        options: {
+            type: Object,
+        }
+    },
+
+    computed: {
+        
+    },
+
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>

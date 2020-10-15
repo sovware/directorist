b23145bef64d62a2cf6c5bb9e507276b04911d3f
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-10-15 17:07:23 +0600

    form fields updated

--- templates/public-templates/shortcodes/forms-new/add-listing-error.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author  AazzTech
- * @since   6.6
- * @version 6.6
- */
-?>
-<p class="error"><?php esc_html_e('You do not have permission to edit this listing', 'directorist'); ?></p>

b07e5aa046194d1872c6bc4e4f40b7b9305485bd
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-10-15 22:53:55 +0600

    listing text updated

--- templates/public-templates/shortcodes/forms/add-listing.php
+++ templates/public-templates/shortcodes/forms/add-listing.php
@@ -6,23 +6,38 @@
  */
 ?>
 <div id="directorist" class="directorist atbd_wrapper atbd_add_listing_wrapper">
-    <div class="<?php echo apply_filters('atbdp_add_listing_container_fluid', 'container-fluid'); ?>">
-        <form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="post" id="add-listing-form">
-            <fieldset>
-                <?php do_action('atbdb_before_add_listing_from_frontend');?>
+	<div class="<?php echo apply_filters('atbdp_add_listing_container_fluid', 'container-fluid'); ?>">
+		<form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="post" id="add-listing-form">
+			<fieldset>
+				<?php do_action('atbdb_before_add_listing_from_frontend');?>
 
-                <div class="atbdp-form-fields">
-                    <input type="hidden" name="add_listing_form" value="1">
-                    <input type="hidden" name="listing_id" value="<?php echo !empty($p_id) ? esc_attr($p_id) : ''; ?>">
-                    <?php
-                    ATBDP()->listing->add_listing->show_nonce_field();
+				<div class="atbdp-form-fields">
+					<input type="hidden" name="add_listing_form" value="1">
+					<input type="hidden" name="listing_id" value="<?php echo !empty($p_id) ? esc_attr($p_id) : ''; ?>">
+					<?php
+					ATBDP()->listing->add_listing->show_nonce_field();
 
-                    foreach ( $form_data as $section ) {
-                        Directorist_Listing_Forms::instance()->add_listing_section_template( $section );
-                    }
-                    ?>
-                </div>
-            </fieldset>
-        </form>
-    </div>
+					foreach ( $form_data as $section ) {
+						Directorist_Listing_Forms::instance()->add_listing_section_template( $section );
+					}
+
+					Directorist_Listing_Forms::instance()->add_listing_submit_template();
+
+                    /**
+                     * @since 6.6
+                     * @hooked Directorist_Listing_Forms > add_listing_general_template - 10
+                     * @hooked Directorist_Listing_Forms > add_listing_contact_template - 15
+                     * @hooked Directorist_Listing_Forms > add_listing_map_template - 20
+                     * @hooked Directorist_Listing_Forms > add_listing_image_template - 25
+                     * @hooked Directorist_Listing_Forms > add_listing_submit_template - 30
+                     */
+                    // do_action( 'directorist_add_listing_contents');
+
+
+
+					?>
+				</div>
+			</fieldset>
+		</form>
+	</div>
 </div>
--- templates/public-templates/shortcodes/forms/fields/text.php
+++ templates/public-templates/shortcodes/forms/fields/text.php
@@ -7,7 +7,7 @@
 ?>
 <div class="form-group" id="atbdp_listing_title">
 	<?php if (!empty($label)): ?>
-		<label for="<?php echo esc_attr( $id ); ?>"><?php echo esc_html( $label )?>:<?php echo !empty($required) ? Directorist_Listing_Forms::instance()->add_listing_required_html() : '';?></label>
+		<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo esc_html( $label )?>:<?php echo !empty($required) ? Directorist_Listing_Forms::instance()->add_listing_required_html() : '';?></label>
 	<?php endif; ?>
-    <input type="text" name="<?php echo esc_attr( $id ); ?>" id="<?php echo esc_attr( $id ); ?>" value="<?php echo esc_attr( $value ); ?>" class="form-control directory_field" placeholder="<?php echo esc_attr( $placeholder ); ?>">
+    <input type="text" name="<?php echo esc_attr( $field_key ); ?>" id="<?php echo esc_attr( $field_key ); ?>" value="<?php echo esc_attr( $value ); ?>" class="form-control directory_field" placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo !empty($required) ? 'required="required"' : ''; ?>>
 </div>

82a7b2fb42ebaf973f56284f479d71c650d9fa69
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-10-27 11:49:47 +0600

    exclude fields in admin

--- templates/public-templates/shortcodes/forms/fields/tagline.php
+++ templates/public-templates/shortcodes/forms/fields/tagline.php
@@ -7,7 +7,9 @@
 ?>
 
 <div class="form-group" id="directorist-tagline-field">
-	<?php $form->add_listing_label_template( $data );?>
+	<?php $form->add_listing_label_template( $data ); ?>
 
 	<input type="text" name="<?php echo esc_attr( $data['field_key'] ); ?>" id="<?php echo esc_attr( $data['field_key'] ); ?>" class="form-control" value="<?php echo esc_attr( $data['value'] ); ?>" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> >
+
+	<?php $form->add_listing_description_template( $data ); ?>
 </div>

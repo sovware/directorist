da1c7db5175256bf7b608cfb58230ffda5206412
vairafiq <vairafiq@gmail.com>
2020-11-03 07:44:36 +0600

    dashboard tab refactured

--- templates/public-templates/shortcodes/dashboard/navigation-tabs.php
+++ templates/public-templates/shortcodes/dashboard/navigation-tabs.php
@@ -5,19 +5,14 @@
  * @version 6.6
  */
 ?>
-<div class="atbdp_tab_nav_wrapper">
-	<ul class="atbdp_tab_nav--content atbd-dashboard-nav">
-		<?php foreach ($dashboard_tabs as $key => $value): ?>
-			<li class="atbdp_tab_nav--content-link"><a href="" class="atbd_tn_link" target="<?php echo esc_attr($key);?>"><?php echo wp_kses_post( $value['title'] ); ?></a>
-			</li>
-			<?php
-			if (!empty($value['after_nav_hook'])) {
-				do_action($value['after_nav_hook']);
-			}
-			?>
-		<?php endforeach; ?>
-		<li class="atbdp_tab_nav--content-link atbdp-tab-nav-last">
-			<a href="#" class="atbdp-tab-nav-link"><span class="fa fa-ellipsis-h"></span></a>
+	<a href="" class="atbd-dashboard-sidenav-toggle"><i class="fas fa-bars"></i></a>
+	<ul>
+	<?php foreach ($dashboard_tabs as $key => $value): ?>
+		<li class="atbdp_tab_nav--content-link"><a href="" class="atbd_tn_link" target="<?php echo esc_attr($key);?>"><?php echo wp_kses_post( $value['title'] ); ?></a>
 		</li>
+		<?php
+		if (!empty($value['after_nav_hook'])) {
+			do_action($value['after_nav_hook']);
+		}
+		endforeach; ?>
 	</ul>
-</div>

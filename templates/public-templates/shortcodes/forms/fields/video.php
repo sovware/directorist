55aab6f03649440ee382f544d12e580d4d5133fb
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-11-04 15:07:21 +0600

    img upload and video field

--- templates/public-templates/shortcodes/forms/fields/video.php
+++ templates/public-templates/shortcodes/forms/fields/video.php
@@ -6,10 +6,11 @@
  */
 ?>
 
-<div class="form-group" id="directorist-image_upload-field">
+<div class="form-group" class="directorist-video-field">
 	<?php $form->add_listing_label_template( $data );?>
 
 	<input type="url" name="<?php echo esc_attr( $data['field_key'] ); ?>" id="<?php echo esc_attr( $data['field_key'] ); ?>" class="form-control" value="<?php echo esc_attr( $data['value'] ); ?>" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> >
-	<?php do_action( 'atbdp_video_field', get_query_var('atbdp_listing_id', 0) ); ?>
+
+	<?php $form->add_listing_description_template( $data ); ?>
 
 </div>

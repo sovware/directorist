5ef19f7b4362898cb9c02ae3d27781feac94525d
Md Kowsar Hossain <kowsar9989@gmail.com>
2020-11-11 07:59:07 +0600

    2 bugs fixed

--- templates/public-templates/shortcodes/single-listing/items/website.php
+++ templates/public-templates/shortcodes/single-listing/items/website.php
@@ -4,10 +4,8 @@
  * @since   6.7
  * @version 6.7
  */
-
-$use_nofollow = get_directorist_option('use_nofollow');
 ?>
 <div class="directorist-single-info directorist-single-info-web">
 	<div class="directorist-single-info-label"><?php directorist_icon( $icon );?><?php echo esc_html( $data['label'] ); ?></div>
-	<div class="directorist-single-info-value"><a target="_blank" href="<?php echo esc_url($value); ?>"<?php echo !empty($use_nofollow) ? 'rel="nofollow"' : ''; ?>><?php echo esc_html($value); ?></a></div>
+	<div class="directorist-single-info-value"><a target="_blank" href="<?php echo esc_url($value); ?>"<?php echo !empty($data['use_nofollow']) ? 'rel="nofollow"' : ''; ?>><?php echo esc_html($value); ?></a></div>
 </div>

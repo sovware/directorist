15af73eeb1d2e40b725c05058321cb694fc76f0f
shamimsaj <shamimeforb@gmail.com>
2020-11-19 11:15:40 +0600

    fixed marker cluster issue

--- public/assets/js/map-view.js
+++ public/assets/js/map-view.js
@@ -142,8 +142,8 @@ MarkerLabel.prototype.draw = function() {
 
                 // update map when contact details fields are updated in the custom post type 'acadp_listings'
                 const mcOptions = {
-                        // imagePath: atbdp_map.plugin_url+'public/assets/images/m',
-                        cssClass: 'marker-cluster-shape',
+                        imagePath: `${atbdp_map.plugin_url}public/assets/images/m`,
+                        // cssClass: 'marker-cluster-shape',
                 };
                 if (map.type === 'markerclusterer') {
                         const markerCluster = new MarkerClusterer(map, map.markers, mcOptions);

8a701cd3653df90a3d738c3f05d74cdb59fcdc12
shamimsaj <shamimeforb@gmail.com>
2020-12-03 15:00:43 +0600

    commit backend extension style

--- templates/theme-extensions/theme-extension.php
+++ templates/theme-extensions/theme-extension.php
@@ -2,7 +2,178 @@
     <!-- my-themes-extensions -->
     <div id="my-themes-extensions" class="atbdp-tab-content active">
         <?php ATBDP()->load_template('theme-extensions/my-themes-extensions'); ?>
-        
+        <div class="et-wrapper">
+            <div class="et-wrapper-head">
+                <h3>All Themes and Extensions</h3>
+                <div class="et-search">
+                    <input type="text" placeholder="Search extensions and themes">
+                    <span class="la la-search"></span>
+                </div>
+            </div><!-- ends: .et-wrapper-head -->
+            <div class="et-contents">
+                <ul class="et-contents__tab-nav">
+                    <li class="active"><a href="">Extensions</a></li>
+                    <li><a href="">Themes</a></li>
+                </ul>
+                <div class="ext-wrapper">
+                    <div class="ext-installed">
+                        <h4>Installed</h4>
+                        <div class="ext-installed-table">
+                            <div class="ext-table-responsive">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th colspan="4">
+                                                <div class="ei-action-wrapper">
+                                                    <div class="ei-select-all"><input type="checkbox" name="select-all" id=""></div>
+                                                    <div class="ei-action-dropdown">
+                                                        <select name="" id="">
+                                                            <option value="">Bulk Action</option>
+                                                            <option value="">Deactivate</option>
+                                                            <option value="">Remove</option>
+                                                        </select>
+                                                    </div>
+                                                    <a href="" class="ei-action-btn">Apply</a>
+                                                </div>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-1">
+                                                    <label for="item-1"><img src="https://via.placeholder.com/44" alt=""> Directorist Coupon <span class="ext-version">v2.4.6</span></label>
+                                                </div>
+                                            </td>
+                                            <td><span class="active-badge">Active</span></td>
+                                            <td><p class="ext-update-info">Update available <span>What's new?</span></p></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-update-btn ext-action-btn">Update</a>
+                                                    <a href="" class="ext-action-btn"><i class="la la-settings"></i> Settings</a>
+                                                    <div>
+                                                        <a href="" class="ext-action-drop"><i class="la la-ellipsis-h"></i></a>
+                                                        <div class="ext-action-drop__item">
+                                                            <a href="" class="">Uninstall</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-2">
+                                                    <label for="item-2"><img src="https://via.placeholder.com/44" alt=""> Directorist Coupon <span class="ext-version">v2.4.6</span></label>
+                                                </div>
+                                            </td>
+                                            <td><span class="active-badge">Active</span></td>
+                                            <td><p class="ext-update-info">Update available <span>What's new?</span></p></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-update-btn ext-action-btn">Update</a>
+                                                    <a href="" class="ext-action-btn"><i class="la la-settings"></i> Settings</a>
+                                                    <div>
+                                                        <a href="" class="ext-action-drop"><i class="la la-ellipsis-h"></i></a>
+                                                        <div class="ext-action-drop__item">
+                                                            <a href="" class="">Uninstall</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-3">
+                                                    <label for="item-3"><img src="https://via.placeholder.com/44" alt=""> Directorist Coupon <span class="ext-version">v2.4.6</span></label>
+                                                </div>
+                                            </td>
+                                            <td><span class="active-badge">Active</span></td>
+                                            <td><p class="ext-update-info ext-updated">Up to date</p></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-action-btn"><i class="la la-settings"></i> Settings</a>
+                                                    <div>
+                                                        <a href="" class="ext-action-drop"><i class="la la-ellipsis-h"></i></a>
+                                                        <div class="ext-action-drop__item">
+                                                            <a href="" class="">Uninstall</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div><!-- ends: .ext-installed -->
+                    <div class="ext-available">
+                        <h4>Available in your subscription</h4>
+                        <div class="ext-available-table">
+                            <div class="ext-table-responsive">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th colspan="4">
+                                                <div class="ei-action-wrapper">
+                                                    <div class="ei-select-all"><input type="checkbox" name="select-all" id=""></div>
+                                                </div>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-1">
+                                                    <label for="item-1"><img src="https://via.placeholder.com/44" alt=""> Rank Featured Listings</label>
+                                                </div>
+                                            </td>
+                                            <td><span class="ext-info">It is a coupon generating tool that can increase conversions and make your directory site more fascinating.</span></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-install-btn ext-action-btn"><i class="la la-download"></i> Install</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-1">
+                                                    <label for="item-1"><img src="https://via.placeholder.com/44" alt=""> Directorist Coupon</label>
+                                                </div>
+                                            </td>
+                                            <td><span class="ext-info">It is a coupon generating tool that can increase conversions and make your directory site more fascinating.</span></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-install-btn ext-action-btn"><i class="la la-download"></i> Install</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="extension-name">
+                                                    <input type="checkbox" id="item-1">
+                                                    <label for="item-1"><img src="https://via.placeholder.com/44" alt=""> Directorist Coupon</label>
+                                                </div>
+                                            </td>
+                                            <td><span class="ext-info">It is a coupon generating tool that can increase conversions and make your directory site more fascinating.</span></td>
+                                            <td>
+                                                <div class="ext-action">
+                                                    <a href="" class="ext-install-btn ext-action-btn"><i class="la la-download"></i> Install</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div><!-- ends: .ext-available -->
+                </div><!-- ends: .ext-wrapper -->
+            </div><!-- ends: .et-contents -->
+        </div><!-- ends: .et-wrapper -->
         <?php ATBDP()->load_template('theme-extensions/all-themes-extensions'); ?>
     </div>
 </div>

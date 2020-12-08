1e5dc72c8ecd83d9414a8bf8ced57173d5b13ba1
shamimsaj <shamimeforb@gmail.com>
2020-12-08 09:15:05 +0600

    plugin extension collapse box issue fixed

--- admin/assets/js/plugins.js
+++ admin/assets/js/plugins.js
@@ -11,33 +11,14 @@ jQuery(document).ready(function($) {
                 '<div class="ext-more"><a href="" class="ext-more-link">Click to view directorist all modules</a></div>'
         );
         const moreLink = $('.directorist-extensions').find('.ext-more-link');
+        $(moreLink).hide();
 
-        $(tbody).append(
-                $(`
-        [data-slug="directorist-business-hour"],
-        [data-slug="directorist-ads-manager"],
-        [data-slug="directorist-claim-listing"],
-        [data-slug="directorist-compare-listing"],
-        [data-slug="directorist-coupon"],
-        [data-slug="directorist-listings-faqs"],
-        [data-slug="directorist-live-chat"],
-        [data-slug="directorist-mark-as-sold"],
-        [data-slug="directorist-paypal-payment-gateway"],
-        [data-slug="directorist-post-your-need"],
-        [data-slug="directorist-pricing-plans"],
-        [data-slug="directorist-social-login"],
-        [data-slug="directorist-stripe-payment-gateway"],
-        [data-slug="directorist-woocommerce-pricing-plans"],
-        [data-slug="directorist-booking"],
-        [data-slug="directorist-google-recaptcha"],
-        [data-slug="directorist-listings-slider-carousel"],
-        [data-slug="directorist-listings-with-map"],
-        [data-slug="directorists"]
-        `)
-        );
+        $(tbody).append($('#the-list tr[data-slug^="directorist-"]'));
 
         if ($(extWrapper).innerHeight() > 250) {
                 $(extWrapper).addClass('ext-height-fix');
+                $(moreLink).show();
+                $(extWrapper).css('padding-bottom', '60px');
         }
         $(moreLink).on('click', function(e) {
                 e.preventDefault();
@@ -51,4 +32,8 @@ jQuery(document).ready(function($) {
                                 .addClass('ext-height-fix');
                 }
         });
+
+        if ($(tbody).html() === '') {
+                $('.directorist-extensions').hide();
+        }
 });

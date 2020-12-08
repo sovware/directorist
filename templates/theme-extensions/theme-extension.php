666b0b5029b2c6c37f4ab458971a9b374aa87662
shamimsaj <shamimeforb@gmail.com>
2020-12-08 13:40:20 +0600

    commit updat modal changes

--- templates/theme-extensions/theme-extension.php
+++ templates/theme-extensions/theme-extension.php
@@ -3,12 +3,12 @@
     <div id="my-themes-extensions" class="atbdp-tab-content active">
 
         <?php
-
+            if ( ! $args[ 'has_purchased_products' ] ) {
                 ATBDP()->load_template('theme-extensions/auth/license-auth-section', $args );
-
+            } else {
                 ATBDP()->load_template('theme-extensions/statistics/statistics', $args);
                 ATBDP()->load_template('theme-extensions/my-themes-extensions/my-themes-extensions', $args);
-
+            }
 
             ATBDP()->load_template('theme-extensions/all-themes-extensions', $args);
          ?>

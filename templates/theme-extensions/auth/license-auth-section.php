3f59368606ab75e4f76d4ba1faa4063bce4bb37d
tanjimhasan <tanjim.hasan.pro@gmail.com>
2020-12-16 21:26:58 +0600

    login form updated

--- templates/theme-extensions/auth/license-auth-section.php
+++ templates/theme-extensions/auth/license-auth-section.php
@@ -5,17 +5,20 @@
             <div class="atbdp-form-page">
                 <h4>Connect your Directorist account</h4>
                 <p>Log in using your Directorist account email and password to manage your extensions and themes.</p>
-
-                <div class="account-connect__form-group">
-                    <input type="text" placeholder="Username" name="username" id="username">
-                    <span class="la la-user"></span>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="account-connect__form-group">
+                            <input type="text" placeholder="Username" name="username" id="username">
+                            <span class="la la-user"></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="account-connect__form-group">
+                            <input type="password" placeholder="Password" name="password" id="password">
+                            <span class="la la-lock"></span>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="account-connect__form-group">
-                    <input type="password" placeholder="Password" name="password" id="password">
-                    <span class="la la-lock"></span>
-                </div>
-
                 <div class="atbdp-form-feedback"></div>
 
                 <div class="account-connect__form-btn">

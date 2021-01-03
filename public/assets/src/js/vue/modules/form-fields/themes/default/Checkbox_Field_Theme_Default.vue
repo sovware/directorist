c6fc255719289d21e4e5555e4883ce0af5b21a26
Syed Galib Ahmed <aazztech13@gmail.com>
2021-01-03 21:36:39 +0600

    improved settings panel

---
+++ public/assets/src/js/vue/modules/form-fields/themes/default/Checkbox_Field_Theme_Default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="cptm-form-group">
+        <label>{{label}}</label>
+
+        <div class="cptm-checkbox-area">
+            <div class="cptm-checkbox-item" v-for="( option, option_index ) in theOptions" :key="option_index">
+                <input type="checkbox" class="cptm-checkbox"
+                    :id="getOptionID( option, option_index )"
+                    :value="( typeof option.value !== 'undefined' ) ? option.value : ''"
+                    v-model="local_value"
+                >
+
+                <label :for="getOptionID( option, option_index )" class="cptm-checkbox-ui"></label>
+                <label :for="getOptionID( option, option_index )" v-html="option.label"></label>
+            </div>
+        </div>
+
+        <p class="cptm-info-text" v-if="! theOptions.length">{{ infoTextForNoOption }}</p>
+    </div>
+</template>
+
+<script>
+import checkbox_feild from './../../../../mixins/form-fields/checkbox-field';
+
+export default {
+    name: 'checkbox-field-theme-default',
+    mixins: [ checkbox_feild ],
+}
+</script>

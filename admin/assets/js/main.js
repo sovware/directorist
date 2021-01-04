04ce83f083716ef6315147548d821ce332faf443
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-01-04 16:13:21 +0600

    style update

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -4134,8 +4134,6 @@ function assetsNeedToWorkInVirtualDom() {
     });
 }
 
-
-
 })(jQuery);
 
 
@@ -4190,3 +4188,27 @@ function toggle_section(show_if_value, subject_elm, terget_elm) {
   })(jQuery);
   
   pureScriptTab('.directorist_builder--tab');
+
+  // Dropdown
+  (function ($) { 
+    
+    $('body').on('click', '.directorist_dropdown .directorist_dropdown-toggle', function(){
+      $('.directorist_dropdown-option').toggle();
+    });
+
+    // Select Option after click
+    $('body').on('click','.directorist_dropdown .directorist_dropdown-option ul li a', function(){
+      console.log("works");
+      let optionText = $(this).html();
+      $('.directorist_dropdown .directorist_dropdown-toggle .directorist_dropdown-toggle__text').html(optionText);
+      $('.directorist_dropdown-option').hide();
+    });
+
+    // Hide Clicked Anywhere
+    $(document).bind('click', function(e) {
+      let clickedDom = $(e.target);
+      if(!clickedDom.parents().hasClass('directorist_dropdown'))
+      $('.directorist_dropdown-option').hide();
+    });
+    
+   })(jQuery);

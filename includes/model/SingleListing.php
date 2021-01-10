36f370a1b77cbf23f52c661155d937a01cf4df57
vairafiq <vairafiq@gmail.com>
2021-01-10 12:45:52 +0600

    fix map issue in single listing

--- includes/model/SingleListing.php
+++ includes/model/SingleListing.php
@@ -102,7 +102,6 @@ class Directorist_Single_Listing {
 			atbdp_get_shortcode_template( $template, $args );
 		}
 		else {
-			// e_var_dump($section_data);
 			atbdp_get_shortcode_template( 'single-listing/section-general', $args );
 		}
 	}
@@ -113,6 +112,13 @@ class Directorist_Single_Listing {
 		if( 'tag' === $data['widget_name'] ) {
 			$tags = get_the_terms( $this->id, ATBDP_TAGS );
 			if( $tags ) {
+				$value = true;
+			}
+		}
+		if( 'map' === $data['widget_name'] ) {
+			$lat = get_post_meta( $this->id, '_manual_lat', true );
+			$lng = get_post_meta( $this->id, '_manual_lng', true );
+			if( $lat && $lng ) {
 				$value = true;
 			}
 		}
@@ -129,7 +135,6 @@ class Directorist_Single_Listing {
 			'value'   => $value,
 			'icon'    => !empty( $data['icon'] ) ? $data['icon'] : '',
 		);
-
 		$template = 'single-listing/items/' . $data['widget_name'];
 		$template = apply_filters( 'directorist_single_item_template', $template, $data );
 		if( $load_template )

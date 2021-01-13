607cb7c1853ca1a1a6b0c85eb7ac1c5350684bd3
Syed Galib Ahmed <aazztech13@gmail.com>
2021-01-13 15:30:10 +0600

    improved subscription management

---
+++ templates/admin-templates/theme-extensions/auth/license-auth-section.php
@@ -0,0 +1,32 @@
+<div class="account-connect">
+    <div class="account-connect__form">
+        <form method="post" id="atbdp-directorist-license-login-form" class="atbdp-directorist-license-login-form">
+            <!-- form-page -->
+            <div class="atbdp-form-page">
+                <h4>Connect your Directorist account</h4>
+                <p>Log in using your Directorist account email and password to manage your extensions and themes.</p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="account-connect__form-group">
+                            <input type="text" placeholder="Username" name="username" id="username">
+                            <span class="la la-user"></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="account-connect__form-group">
+                            <input type="password" placeholder="Password" name="password" id="password">
+                            <span class="la la-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="atbdp-form-feedback"></div>
+
+                <div class="account-connect__form-btn">
+                    <button type="submit" class="account-connect__btn">Connect <span class="la la-arrow-right"></span></button>
+                </div>
+            </div>
+
+            <div class="atbdp-form-response-page"></div>
+        </form>
+    </div>
+</div>

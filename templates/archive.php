afe02b7b5b6cdae85bc944c5ba84472727bc3ba1
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-01-25 16:58:49 +0600

    archive header

---
+++ templates/archive.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying all listings page
+ *
+ * This template can be overridden by copying it to yourtheme/directorist/archive.php.
+ *
+ * @author  wpWax
+ * @since   6.6
+ * @version 6.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+?>

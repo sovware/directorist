0189161ae9b59f8075d2445ecb6c9355023cea88
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-01-27 23:24:38 +0600

    loop

--- templates/archive/listing-types.php
+++ templates/archive/listing-types.php
@@ -1,15 +1,26 @@
 <?php
 /**
+ * @template_description1
+ *
+ * This template can be overridden by copying it to yourtheme/directorist/ @template_description2
+ *
  * @author  wpWax
  * @since   6.6
  * @version 6.6
  */
+
+use \Directorist\Helper;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
+
 <div class="directorist-listing-types">
-	<div class="<?php echo apply_filters('atbdp_add_listing_container_fluid', 'container-fluid'); ?>">
-		<ul class="list-inline">
+	<div class="<?php Helper::directorist_container(); ?>">
+		<ul class="directorist-listing-type-list">
 			<?php foreach ( $listings->listing_types as $id => $value ): ?>
-				<li class="list-inline-item <?php echo $listings->current_listing_type == $value['term']->term_id ? 'current': ''; ?>"><a class="directorist-listing-types-link" href="<?php echo esc_url( add_query_arg('directory_type', $value['term']->slug ) ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a></li>
+				<li class="<?php echo $listings->current_listing_type == $value['term']->term_id ? 'current': ''; ?>">
+					<a class="directorist-listing-types-link" href="<?php echo esc_url( add_query_arg('directory_type', $value['term']->slug ) ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
+				</li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
--- templates/archive/listings-header.php
+++ templates/archive/listings-header.php
@@ -1,64 +1,65 @@
 <?php
 /**
+ * @template_description1
+ *
+ * This template can be overridden by copying it to yourtheme/directorist/ @template_description2
+ *
  * @author  wpWax
  * @since   6.6
  * @version 6.6
  */
+
+use \Directorist\Helper;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
-<div class="atbd_header_bar">
-	<div class="<?php $listings->header_container_class();?>">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="atbd_generic_header">
-					<?php if ($listings->has_listings_header()) { ?>
-						<div class="atbd_generic_header_title">
-							<?php if ($listings->has_filters_button) { ?>
-								<a href="#" class="more-filter btn btn-outline btn-outline-primary">
-									<?php if ($listings->has_filters_icon) { ?>
-										<span class="<?php atbdp_icon_type(true); ?>-filter"></span>
-									<?php } ?>
-									<?php echo $listings->filter_button_text; ?>
-								</a>
-								<?php
-							}
-							/**
-							* @since 5.4.0
-							*/
-							do_action('atbdp_after_filter_button_in_listings_header');
-							
-							if (!empty($listings->header_title)) {
-								echo $listings->item_found_title();
-							}
-							?>
-						</div>
+
+<div class="directorist-archive-header-bar">
+
+	<div class="<?php Helper::directorist_container(); ?>">
+
+		<div class="directorist-archive-header-contents">
+			
+			<?php if ($listings->has_listings_header()) { ?>
+				<div class="atbd_generic_header_title">
+					<?php if ($listings->has_filters_button) { ?>
+						<a href="#" class="more-filter btn btn-outline btn-outline-primary">
+							<?php if ($listings->has_filters_icon) { ?>
+								<span class="<?php atbdp_icon_type(true); ?>-filter"></span>
+							<?php } ?>
+							<?php echo $listings->filter_button_text; ?>
+						</a>
 						<?php
 					}
-					/**
-					 * @since 5.4.0
-					 */
-					do_action('atbdp_after_total_listing_found_in_listings_header', $listings->header_title);
 					
-					if ($listings->has_header_toolbar()) { ?>
-						<div class="atbd_listing_action_btn btn-toolbar" role="toolbar">
-							<?php
-							if ($listings->display_viewas_dropdown) {
-								$listings->viewas_dropdown_template();
-							}
-							
-							if ($listings->display_sortby_dropdown) {
-								$listings->sortby_dropdown_template();
-							}
-							?>
-						</div>
-						<?php
+					if (!empty($listings->header_title)) {
+						echo $listings->item_found_title();
 					}
 					?>
 				</div>
-
-				<div class="<?php $listings->filter_container_class(); ?>">
-					<?php $listings->advanced_search_form_template();?>
+				<?php
+			}
+			
+			if ($listings->has_header_toolbar()) { ?>
+				<div class="atbd_listing_action_btn btn-toolbar" role="toolbar">
+					<?php
+					if ($listings->display_viewas_dropdown) {
+						$listings->viewas_dropdown_template();
+					}
+					
+					if ($listings->display_sortby_dropdown) {
+						$listings->sortby_dropdown_template();
+					}
+					?>
 				</div>
-			</div>
+				<?php
+			}
+			?>
+		</div>
+
+		<div class="<?php $listings->filter_container_class(); ?>">
+			<?php $listings->advanced_search_form_template();?>
 		</div>
 	</div>
+
 </div>

4fd59c73f649203d688880c7340789bb7d6e697e
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-01-27 12:23:07 +0600

    testing

--- templates/archive.php
+++ templates/archive.php
@@ -2,12 +2,24 @@
 /**
  * The template for displaying all listings page
  *
- * This template can be overridden by copying it to yourtheme/directorist/archive.php.
+ * This template can be overridden by copying it to yourtheme/directorist/archive.php
  *
  * @author  wpWax
  * @since   6.6
  * @version 6.6
  */
 
+use Directorist\URI_Helper;
+
 if ( ! defined( 'ABSPATH' ) ) exit;
-?>
+
+get_header( 'directorist' );
+echo "string";
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		URI_Helper::get_template( 'archive/contents' );
+	}
+}
+
+get_footer( 'directorist' );

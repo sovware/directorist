528f1d2985f8bd4c50c6938bb005afbf258231ec
vairafiq <vairafiq@gmail.com>
2021-01-28 14:42:37 +0600

    fix directory type in builder

--- includes/modules/multi-directory-setup/class-multi-directory-manager.php
+++ includes/modules/multi-directory-setup/class-multi-directory-manager.php
@@ -4968,7 +4968,7 @@ if ( ! class_exists('ATBDP_Multi_Directory_Manager') ) {
             $listing_type_id = 0;
 
             if ( ! $enable_multi_directory || ( ! empty( $action ) && ( 'edit' === $action ) && ! empty( $_REQUEST['listing_type_id'] ) ) ) {
-                $listing_type_id = ( ! $enable_multi_directory ) ? $this->get_default_directory_id() : $_REQUEST['listing_type_id'];
+                $listing_type_id = ( ! $enable_multi_directory ) ? default_directory_type() : $_REQUEST['listing_type_id'];
                 $this->update_fields_with_old_data( $listing_type_id );
             }
 

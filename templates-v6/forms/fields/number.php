1e34694b0dafc829cb2b3741e2dbf5a0ab1df1c9
vairafiq <vairafiq@gmail.com>
2021-02-01 11:06:52 +0600

    improve template

--- templates-v6/forms/fields/number.php
+++ templates-v6/forms/fields/number.php
@@ -10,9 +10,7 @@
 	<?php $form->add_listing_label_template( $data );?>
 
 	<input type="number" name="<?php echo esc_attr( $data['field_key'] ); ?>" id="<?php echo esc_attr( $data['field_key'] ); ?>" class="form-control" value="<?php echo esc_attr( $data['value'] ); ?>" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" 
-	<?php 
-	echo ! empty( $data['required'] ) ? 'required="required"' : ''; 
-	echo $data['max'] ? 'max="'. $data['max'] .'"' : ''; ?> >
+	<?php  echo ! empty( $data['required'] ) ? 'required="required"' : '';  echo !empty( $data['max'] ) ? 'max="'. $data['max'] .'"' : ''; ?> >
 
 	<?php $form->add_listing_description_template( $data ); ?>
 </div>

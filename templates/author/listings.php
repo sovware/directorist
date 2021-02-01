2f09f1d74a2ac6594fc124ccde80e485166f5bf5
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-02-01 22:02:47 +0600

    author done

--- templates/author/listings.php
+++ templates/author/listings.php
@@ -5,56 +5,55 @@
  * @version 6.7
  */
 
+use \Directorist\Helper;
+
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div class="row">
-	<div class="col-md-12">
-		<div class="atbd_author_listings_area">
+<div class="atbd_author_listings_area">
 
-			<?php if ($display_title){ ?>
-			<h1><?php esc_html_e("Author Listings", 'directorist'); ?></h1>
-			<?php } ?>
+	<h2><?php esc_html_e( 'Author Listings' , 'directorist'); ?></h2>
 
-			<?php if ( $display_cat_filter ) { ?>
-				<div class="atbd_author_filter_area">
-					<?php
-                    /*
-                     * @since 6.2.3
-                     */
-                    do_action('atbpd_before_author_listings_category_dropdown', $listings->query_results);
-                    ?>
+	<?php if ( $author->cat_filter_enabled() ): ?>
 
-                    <div class="atbd_dropdown">
-                    	<a class="atbd_dropdown-toggle" href="#" id="dropdownMenuLink"><?php esc_html_e("Filter by category", 'directorist'); ?> <span class="atbd_drop-caret"></span></a>
+		<div class="atbd_dropdown">
 
-                    	<div class="atbd_dropdown-menu atbd_dropdown-menu--lg" aria-labelledby="dropdownMenuLink">
-                    		<?php foreach ($categories as $category) {
-                    			$active = (isset($_GET['category']) && ($category->slug == $_GET['category'])) ? 'active' : '';
-                    			printf('<a class="atbd_dropdown-item %s" href="%s">%s</a>', $active, add_query_arg('category', $category->slug), $category->name);
-                    		}
-                    		?>
-                    	</div>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-</div>
-<?php 
-/**
- * @since 6.6
- */
-do_action( 'atbdp_author_listings_before_loop' ); ?>
-<div class="atbd_authors_listing">
-	<?php if ($display_listings) { ?>
-		<div class="row"<?php echo esc_attr($listings->masonary_grid_attr()); ?>>
-			<?php $listings->setup_loop( ['template' => 'grid'] ); ?>
+			<a class="atbd_dropdown-toggle" href="#" id="dropdownMenuLink"><?php esc_html_e( 'Filter by category', 'directorist'); ?> <span class="atbd_drop-caret"></span></a>
+
+			<div class="atbd_dropdown-menu atbd_dropdown-menu--lg" aria-labelledby="dropdownMenuLink">
+				<?php
+				foreach ($author->get_listing_categories() as $category) {
+					$active_class = ( isset($_GET['category']) && ($category->slug == $_GET['category']) ) ? 'active' : '';
+					$link = add_query_arg( 'category', $category->slug );
+					printf('<a class="directoriet-dropdown-item %s" href="%s">%s</a>', $active_class, $link, $category->name);
+				}
+				?>
+			</div>
+
 		</div>
+
+	<?php endif; ?>
+
+</div>
+
+<div class="atbd_authors_listing <?php echo esc_attr( $listings->grid_view_class() ); ?>">
+
+		<div class="<?php Helper::directorist_row(); ?>">
+
+			<?php foreach ( $listings->post_ids() as $listing_id ): ?>
+
+				<div class="<?php Helper::directorist_column( $listings->columns ); ?>">
+					<?php $listings->loop_template( 'grid', $listing_id ); ?>
+				</div>
+				
+			<?php endforeach; ?>
+
+		</div>
+
 		<?php
-		if ( $display_pagination ) {
-			echo atbdp_pagination( $listings->query_results );
+		if ( $author->listing_pagination_enabled() ) {
+			$listings->pagination();
 		}
-	}
-	?>
+		?>
+		
 </div>

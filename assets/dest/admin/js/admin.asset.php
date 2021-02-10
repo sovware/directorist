6edffcd5d3982bd816e2085fb3bc674173824be7
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-02-10 13:57:28 +0600

    commit changes update

--- assets/dest/admin/js/admin.asset.php
+++ assets/dest/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '0762a066af3dd07f345fb2f1424159a0');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f89ecb0ee0ae225529d5738690a85aa6');

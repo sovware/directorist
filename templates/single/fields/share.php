945ee29b935f9f4647bf3caed1acfe9ef4552063
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-02-12 10:51:52 +0600

    single fields

--- templates/single/fields/share.php
+++ templates/single/fields/share.php
@@ -4,14 +4,18 @@
  * @since   6.7
  * @version 6.7
  */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
-<div class="atbd_action atbd_share atbd_tooltip" aria-label="<?php esc_html_e('Share', 'directorist'); ?>">
+
+<div class="atbd_action atbd_share atbd_tooltip" aria-label="<?php esc_html_e( 'Share', 'directorist' ); ?>">
+
 	<?php directorist_icon( $icon );?>
-	<div class="atbd_directory_social_wrap">
-		<ul>
-			<?php foreach ( $listing->social_share_data() as $social ): ?>
-				<li><a href="<?php echo esc_url( $social['link'] );?>"><span class="<?php echo esc_attr( $social['icon'] );?>"></span><?php echo esc_html( $social['title'] );?></a></li>
-			<?php endforeach; ?>							
-		</ul>
-	</div>
+
+	<ul>
+		<?php foreach ( $listing->social_share_data() as $social ): ?>
+			<li><a href="<?php echo esc_url( $social['link'] );?>"><span class="<?php echo esc_attr( $social['icon'] );?>"></span><?php echo esc_html( $social['title'] );?></a></li>
+		<?php endforeach; ?>							
+	</ul>
+	
 </div>

8cdbdacde7e5be2e274d577d83b17b6837f23e53
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-02-14 13:32:08 +0600

    beautify

--- templates/archive/loop-grid-nothumb.php
+++ templates/archive/loop-grid-nothumb.php
@@ -13,19 +13,23 @@ $loop_fields = $listings->loop['card_fields']['template_data']['grid_view_withou
 <div class="directorist-listing-single directorist-listing-card directorist-listing-no-thumb <?php echo esc_attr( $listings->loop_wrapper_class() ); ?>">
 
 	<div class="directorist-listing-single__header">
+
 		<div class="directorist-listing-single__header__left">
 			<?php $listings->render_loop_fields($loop_fields['body']['avatar']); ?>
 
 			<div class="directorist-listing-single__title"><?php $listings->render_loop_fields($loop_fields['body']['title']); ?></div>
 		</div>
+
 		<div class="directorist-listing-single__header__right">
 			<div class="directorist-listing-single__action"><?php $listings->render_loop_fields($loop_fields['body']['quick_actions']); ?></div>
 		</div>
+		
 	</div>
 
 	<div class="directorist-listing-single__info"><?php $listings->render_loop_fields($loop_fields['body']['quick_info']); ?></div>
 
 	<div class="directorist-listing-single__content">
+
 		<div class="directorist-listing-single__content__body">
 			<div class="directorist-listing-single__info--list"><ul><?php $listings->render_loop_fields($loop_fields['body']['bottom'], '<li>', '</li>'); ?></ul></div>
 			<div class="directorist-listing-single__info--excerpt"><?php $listings->render_loop_fields($loop_fields['body']['excerpt']); ?></div>

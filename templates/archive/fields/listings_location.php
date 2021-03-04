eaf6c39ec5ee68db3f4850a7e9003b53660eb293
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-03-04 23:46:42 +0600

    archive location update

--- templates/archive/fields/listings_location.php
+++ templates/archive/fields/listings_location.php
@@ -8,4 +8,9 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div class="directorist-listing-card-location"><?php directorist_icon( $icon );?><?php $listings->print_label( $label ); ?><?php echo $listings->get_the_location(); ?></div>
+<div class="directorist-listing-card-location">
+    <?php directorist_icon( $icon );?><?php $listings->print_label( $label ); ?>
+    <div class="directorist-listing-card-location-list">
+        <?php echo $listings->get_the_location(); ?>
+    </div>
+</div>

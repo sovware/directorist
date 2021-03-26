f31b1a7913144347665bf2f5dbbbb638ac18048f
Syed Galib Ahmed <55786211+sovware13@users.noreply.github.com>
2021-03-26 09:51:54 +0600

    Fixing builder (#118)
    * fixed builder widget data sync issue
    
    * fixed builder issues
    
    * fixed missing data issue in card builder

--- assets/src/js/vue/modules/form-fields/Card_Builder_Grid_View_With_Thumbnail_Field.vue
+++ assets/src/js/vue/modules/form-fields/Card_Builder_Grid_View_With_Thumbnail_Field.vue
@@ -405,9 +405,9 @@ export default {
           if ( show_if_cond_state.status ) {
             let widget_keys = [];
             for ( let matched_field of show_if_cond_state.matched_data ) {
-              
               let _main_widget = JSON.parse( JSON.stringify( main_widget ) );
               let current_key = ( widget_keys.includes( widget ) ) ? widget + '_' + (widget_keys.length + 1) : widget;
+              _main_widget.widget_key = current_key;
 
               if ( matched_field.widget_key ) {
                 _main_widget.original_widget_key = matched_field.widget_key;
@@ -424,7 +424,7 @@ export default {
           }
         }
       }
-
+      
       return available_widgets;
     },
 

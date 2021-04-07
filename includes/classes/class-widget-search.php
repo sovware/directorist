d8340e6434256129c6e95dfcbdfc130ed4385b72
Syed Galib <aazztech13@gmail.com>
2021-04-07 15:36:06 +0600

    improved

--- includes/classes/class-widget-search.php
+++ includes/classes/class-widget-search.php
@@ -57,8 +57,8 @@ if ( !class_exists('BD_Search_Widget')) {
             $atbdp_legacy_template         = get_directorist_option( 'atbdp_legacy_template', false );
             
 
-            $listing_type = '';
-            $listing_type = default_directory_type();
+            $listing_type = get_post_meta( get_the_ID(), '_directory_type', true );
+            $listing_type = ( ! empty( $listing_type ) ) ? $listing_type : default_directory_type();
             $script_args  = ['directory_type_id' => $listing_type];
 
             if ( Directorist\Helper::is_legacy_mode() ) {

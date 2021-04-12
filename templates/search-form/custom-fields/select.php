8957fcb65ce651857456c920e0b8390a83fece54
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-04-12 14:30:47 +0600

    radio and select field

--- templates/search-form/custom-fields/select.php
+++ templates/search-form/custom-fields/select.php
@@ -13,23 +13,21 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<?php if ( !empty($data['label']) ): ?>
 		<label><?php echo esc_html( $data['label'] ); ?></label>
 	<?php endif; ?>
+
 	<div class="directorist-select" id="directorist-search-select-js">
-		<select name='custom_field[<?php echo esc_html( $data['field_key'] ); ?>]' <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true">
+
+		<select name='custom_field[<?php echo esc_attr( $data['field_key'] ); ?>]' <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true">
+
+			<option value=""><?php esc_html_e( 'Select', 'directorist' )?></option>
 
 			<?php
-			if( $original_field['fields']['select']['field_key'] === $data['field_key'] ) {
-				$options = $original_field['fields']['select']['options'];
-				
-				printf( '<option value="">%s</option>', __( 'Select', 'directorist' ) );
-
-				if( $options ) {
-					foreach ( $options as $option ) {
-						printf('<option value="%s"%s>%s</option>', $option['option_value'], selected(  $value === $option[ 'option_value' ] ), $option['option_label']);
-					}
-				}
+			foreach ( $data['options'] as $option ) {
+				printf('<option value="%s"%s>%s</option>', $option['option_value'], selected(  $value === $option[ 'option_value' ] ), $option['option_label']);
 			}
 			?>
 
 		</select>
+
 	</div>
+
 </div>

8a41df0ea1dc1115b5841143f37b9413d51db174
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-05-18 10:29:19 +0600

    Single selection now works in Form Tag

--- templates/listing-form/fields/tag.php
+++ templates/listing-form/fields/tag.php
@@ -2,7 +2,7 @@
 /**
  * @author  AazzTech
  * @since   6.7
- * @version 7.0.3.2
+ * @version 7.0.4.1
  */
 
 $all_tags = get_terms( ATBDP_TAGS, array( 'hide_empty' => 0 ) );
@@ -12,8 +12,13 @@ $current_tag_ids = $listing_form->add_listing_tag_ids();
 <div class="directorist-form-group directorist-form-tag-field">
 	<?php $listing_form->field_label_template( $data, 'at_biz_dir-tags' ); ?>
 
-	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-tags" multiple="multiple" <?php echo !empty( $data['allow_new'] ) ? 'data-allow_new="'. $data['allow_new'] .'"' : '';  echo !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'"' : ''; ?>>
+	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-tags" <?php echo $data['type'] == 'multiple' ? 'multiple="multiple"' : ''; echo !empty( $data['allow_new'] ) ? 'data-allow_new="'. $data['allow_new'] .'"' : '';  echo !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'"' : ''; ?>>
+
 		<?php
+		if ( $data['type'] != 'multiple' ) {
+			printf( '<option>%s</option>', __( 'Select Tag', 'directorist' ) );
+		}
+
 		foreach ($all_tags as $tag) {
 			$current = in_array($tag->term_id, $current_tag_ids) ? true : false;
 			?>
@@ -21,6 +26,7 @@ $current_tag_ids = $listing_form->add_listing_tag_ids();
 			<?php
 		}
 		?>
+
 	</select>
 
 	<?php $listing_form->field_description_template( $data ); ?>

2052e08a6d8cf4acaad029becbef45d83fecbf6a
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-05-26 20:12:25 +0600

    re-configured webpack to build zip file on production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,16 @@
 const common    = require("./webpack.common");
 const { merge } = require('webpack-merge');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackRTLPlugin     = require("webpack-rtl-plugin");
+const MiniCssExtractPlugin   = require("mini-css-extract-plugin");
+const WebpackRTLPlugin       = require("webpack-rtl-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FileManagerPlugin      = require('filemanager-webpack-plugin');
 
 const prodConfig = {
   mode: "production", // production | development
   watch: false,
   
   plugins: [
-    new CleanWebpackPlugin({
-      dry: false,
-      cleanOnceBeforeBuildPatterns: [ '../css/*.map', '../js/*.map' ],
-      dangerouslyAllowCleanPatternsOutsideProject: true,
-    }),
     new MiniCssExtractPlugin({
       filename: "../css/[name].min.css",
       minify: true,
@@ -22,6 +18,39 @@ const prodConfig = {
     new WebpackRTLPlugin({
       minify: true,
     }),
+    new CleanWebpackPlugin({
+      dry: false,
+      cleanOnceBeforeBuildPatterns: [ '../css/*.map', '../js/*.map' ],
+      dangerouslyAllowCleanPatternsOutsideProject: true,
+    }),
+    new FileManagerPlugin({
+      events: {
+        onEnd: [
+          {
+            copy: [
+              { source: './admin', destination: './directorist/admin' },
+              { source: './assets', destination: './directorist/assets' },
+              { source: './languages', destination: './directorist/languages' },
+              { source: './includes', destination: './directorist/includes' },
+              { source: './templates', destination: './directorist/templates' },
+              { source: './views', destination: './directorist/views' },
+              { source: './*.php', destination: './directorist' },
+              { source: './readme.md', destination: './directorist/readme.md' },
+              { source: './readme.txt', destination: './directorist/readme.txt' },
+            ],
+          },
+          {
+            archive: [
+              { source: './directorist', destination: './directorist.zip' },
+            ],
+          },
+          {
+            delete: ['./directorist'],
+          },
+        ],
+      },
+    }),
+    
   ],
 
   output: {

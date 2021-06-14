1fe32aa77065723c36b4ccb301023027f435f31d
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-06-14 14:37:30 +0600

    added a helper function

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -428,4 +428,26 @@ class Helper {
 		echo "<!-- directorist-shortcode:: [{$shortcode}] -->";
 	}
 
+	public static function maybe_json( $input_data = '' ) {
+		if ( 'string' !== gettype( $input_data )  ) { return $input_data; }
+        
+        // Sanitize input data
+        $sanitized_data = $input_data;
+
+        if ( preg_match( '/\\\\+/', $sanitized_data ) ) {
+            $sanitized_data = preg_replace('/\\\\+/', '', $sanitized_data);
+        }
+
+        $output_data = json_decode( $sanitized_data, true);
+        $output_data = ( ! is_null( $output_data ) ) ? $output_data : $input_data;
+
+        // Sanitize output data
+        if ( 'string' === gettype( $output_data ) ) {
+            $output_data = preg_replace( '/\\\\"/', '"', $output_data );
+            $output_data = preg_replace( "/\\\\'/", "'", $output_data );
+            $output_data = _sanitize_text_fields( $output_data, true );
+        }
+
+        return $output_data;
+	}
 }

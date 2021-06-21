8ab09ef31c8164b232242de58604edcbc50195dd
shamimsaj <shamimeforb@gmail.com>
2021-06-21 10:59:30 +0600

    added team cards

--- templates/author/archive.php
+++ templates/author/archive.php
@@ -6,4 +6,135 @@
  */
 ?>
 
-<div>test</div>
+<div class="directorist-w-100">
+    <div class="directorist-container">
+        <div class="directorist-authors">
+            <div class="directorist-authors__nav">
+                <ul>
+                    <li><a href="">a</a></li>
+                    <li><a href="">b</a></li>
+                    <li><a href="">c</a></li>
+                    <li><a href="">d</a></li>
+                    <li><a href="">e</a></li>
+                    <li><a href="">f</a></li>
+                    <li><a href="">g</a></li>
+                    <li><a href="">h</a></li>
+                    <li><a href="">i</a></li>
+                    <li><a href="">j</a></li>
+                    <li><a href="">k</a></li>
+                    <li><a href="">l</a></li>
+                    <li><a href="">m</a></li>
+                    <li><a href="">n</a></li>
+                    <li><a href="">o</a></li>
+                    <li><a href="">p</a></li>
+                    <li><a href="">q</a></li>
+                    <li><a href="">r</a></li>
+                    <li><a href="">s</a></li>
+                    <li><a href="">t</a></li>
+                    <li><a href="">u</a></li>
+                    <li><a href="">v</a></li>
+                    <li><a href="">w</a></li>
+                    <li><a href="">x</a></li>
+                    <li><a href="">y</a></li>
+                    <li><a href="">z</a></li>
+                </ul>
+            </div><!-- ends: .directorist-authors__nav -->
+
+            <div class="directorist-authors__cards">
+                <div class="directorist-row">
+                    <div class="directorist-col-md-3">
+                        <div class="directorist-authors__card">
+                            <div class="directorist-authors__card__img">
+                                <img src="https://via.placeholder.com/260x210" alt="">
+                            </div>
+                            <div class="directorist-authors__card__details">
+                                <h2>Nazmul Hasan</h2>
+                                <h3>Developer</h3>
+                                <ul>
+                                    <li><i class="la la-phone"></i> +66 2 486 0022</li>
+                                    <li><i class="la la-envelope"></i> sample@email.com</li>
+                                </ul>
+                                <p>Proident sunt in culpa kequi officia deserunt mollit anim id est labor. Sed ut perspiciatis</p>
+
+                                <a href="" class="directorist-btn directorist-btn-light directorist-btn-block">View All listings</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="directorist-col-md-3">
+                        <div class="directorist-authors__card">
+                            <div class="directorist-authors__card__img">
+                                <img src="https://via.placeholder.com/260x210" alt="">
+                            </div>
+                            <div class="directorist-authors__card__details">
+                                <h2>Nazmul Hasan</h2>
+                                <h3>Developer</h3>
+                                <ul>
+                                    <li><i class="la la-phone"></i> +66 2 486 0022</li>
+                                    <li><i class="la la-envelope"></i> sample@email.com</li>
+                                </ul>
+                                <p>Proident sunt in culpa kequi officia deserunt mollit anim id est labor. Sed ut perspiciatis</p>
+
+                                <a href="" class="directorist-btn directorist-btn-light directorist-btn-block">View All listings</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="directorist-col-md-3">
+                        <div class="directorist-authors__card">
+                            <div class="directorist-authors__card__img">
+                                <img src="https://via.placeholder.com/260x210" alt="">
+                            </div>
+                            <div class="directorist-authors__card__details">
+                                <h2>Nazmul Hasan</h2>
+                                <h3>Developer</h3>
+                                <ul>
+                                    <li><i class="la la-phone"></i> +66 2 486 0022</li>
+                                    <li><i class="la la-envelope"></i> sample@email.com</li>
+                                </ul>
+                                <p>Proident sunt in culpa kequi officia deserunt mollit anim id est labor. Sed ut perspiciatis</p>
+
+                                <a href="" class="directorist-btn directorist-btn-light directorist-btn-block">View All listings</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="directorist-col-md-3">
+                        <div class="directorist-authors__card">
+                            <div class="directorist-authors__card__img">
+                                <img src="https://via.placeholder.com/260x210" alt="">
+                            </div>
+                            <div class="directorist-authors__card__details">
+                                <h2>Nazmul Hasan</h2>
+                                <h3>Developer</h3>
+                                <ul>
+                                    <li><i class="la la-phone"></i> +66 2 486 0022</li>
+                                    <li><i class="la la-envelope"></i> sample@email.com</li>
+                                </ul>
+                                <p>Proident sunt in culpa kequi officia deserunt mollit anim id est labor. Sed ut perspiciatis</p>
+
+                                <a href="" class="directorist-btn directorist-btn-light directorist-btn-block">View All listings</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="directorist-col-md-3">
+                        <div class="directorist-authors__card">
+                            <div class="directorist-authors__card__img">
+                                <img src="https://via.placeholder.com/260x210" alt="">
+                            </div>
+                            <div class="directorist-authors__card__details">
+                                <h2>Nazmul Hasan</h2>
+                                <h3>Developer</h3>
+                                <ul>
+                                    <li><i class="la la-phone"></i> +66 2 486 0022</li>
+                                    <li><i class="la la-envelope"></i> sample@email.com</li>
+                                </ul>
+                                <p>Proident sunt in culpa kequi officia deserunt mollit anim id est labor. Sed ut perspiciatis</p>
+
+                                <a href="" class="directorist-btn directorist-btn-light directorist-btn-block">View All listings</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- ends: .directorist-authors__cards -->
+
+        </div><!-- ends: .directorist-authors -->
+    </div>
+</div>

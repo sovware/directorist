b176f7665f2fe55bd6837b82e24ea8a335be26f9
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-06-22 15:39:26 +0600

    view file

---
+++ templates/single/fields/custom_content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author  wpWax
+ * @since   7.0.5
+ * @version 7.0.5
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+?>
+
+<div class="directorist-single-info directorist-single-info-custom">
+
+	<?php echo do_shortcode( $value ); ?>
+
+</div>

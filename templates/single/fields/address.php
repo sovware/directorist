401100f23d9985dc88f046e13d205741bdaa6c34
HeyMehedi <mehedi.2800@gmail.com>
2021-07-01 13:51:17 +0600

    HTML end tag Fix in single templates

--- templates/single/fields/address.php
+++ templates/single/fields/address.php
@@ -13,7 +13,10 @@ $address = ( is_string( $address_data ) ) ? wp_kses_post( $address_data ) : '';
 
 <div class="directorist-single-info directorist-single-info-address">
 
-	<div class="directorist-single-info__label"><span class="directorist-single-info__label-icon"><?php directorist_icon( $icon );?></span class="directorist-single-info__label--text"><span><?php echo ( isset( $data['label'] ) ) ? esc_html( $data['label'] ) : ''; ?></span></div>
+	<div class="directorist-single-info__label">
+		<span class="directorist-single-info__label-icon"><?php directorist_icon( $icon );?></span>
+		<span class="directorist-single-info__label--text"><?php echo ( isset( $data['label'] ) ) ? esc_html( $data['label'] ) : ''; ?></span>
+	</div>
 	
 	<div class="directorist-single-info__value"><?php echo $address; ?></div>
 

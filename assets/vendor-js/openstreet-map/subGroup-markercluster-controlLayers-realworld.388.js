d3610063db015358073d3b5a52813192f86027cc
Ethan Hann <ethanhann@gmail.com>
2021-07-15 13:08:34 -0700

    Use new regex to parse HTML comments

--- assets/vendor-js/openstreet-map/subGroup-markercluster-controlLayers-realworld.388.js
+++ assets/vendor-js/openstreet-map/subGroup-markercluster-controlLayers-realworld.388.js
@@ -7,13 +7,13 @@ function get_dom_data ( key ) {
 
     if ( ! dom_content.length ) { return ''; }
 
-    var pattern = new RegExp("(<!-- directorist-dom-data::" + key + "\\s)(.+)(\\s-->)");
+    var pattern = new RegExp("<!-- directorist-dom-data::" + key + "(.*?)-->");
     var terget_content = pattern.exec( dom_content );
 
     if ( ! terget_content ) { return ''; }
-    if ( typeof terget_content[2] === 'undefined' ) { return ''; }
-    
-    var dom_data = JSON.parse( terget_content[2] );
+    if ( typeof terget_content[1] === 'undefined' ) { return ''; }
+
+    var dom_data = JSON.parse( terget_content[1] );
 
     if ( ! dom_data ) { return ''; }
 

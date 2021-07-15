bbdaeec8d8aee55f4640249cef002b1b2f3fc95f
vairafiq <vairafiq@gmail.com>
2021-07-15 11:36:16 +0600

    add empty placeholder check

--- templates/listing-form/fields/category.php
+++ templates/listing-form/fields/category.php
@@ -2,15 +2,17 @@
 /**
  * @author  wpWax
  * @since   6.7
- * @version 7.0.5
+ * @version 7.0.5.1
  */
+
+$placeholder = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
 ?>
 
 <div class="directorist-form-group directorist-form-categories-field">
 
 	<?php $listing_form->field_label_template( $data );?>
 
-	<select name="admin_category_select[]" id="at_biz_dir-categories" class="directorist-form-element" data-placeholder="<?php echo esc_attr($data['placeholder']); ?>" <?php echo $data['type'] == 'multiple' ? 'multiple="multiple"' : ''; echo !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'"' : ''; echo !empty( $data['create_new_cat'] ) ? 'data-allow_new="'. $data['create_new_cat'] .'"' : ''; ?>>
+	<select name="admin_category_select[]" id="at_biz_dir-categories" class="directorist-form-element" data-placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo $data['type'] == 'multiple' ? 'multiple="multiple"' : ''; echo !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'"' : ''; echo !empty( $data['create_new_cat'] ) ? 'data-allow_new="'. $data['create_new_cat'] .'"' : ''; ?>>
 
 		<?php
 		if ($data['type'] != 'multiple') {
--- templates/listing-form/fields/location.php
+++ templates/listing-form/fields/location.php
@@ -2,15 +2,17 @@
 /**
  * @author  wpWax
  * @since   6.7
- * @version 7.0.5
+ * @version 7.0.5.1
  */
+
+$placeholder = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
 ?>
 
 <div class="directorist-form-group directorist-form-location-field">
 
 	<?php $listing_form->field_label_template( $data ); ?>
 
-	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-location" data-placeholder="<?php echo esc_attr($data['placeholder']); ?>" <?php echo $data['type'] == 'multiple' ? 'multiple="multiple"' : '';  echo !empty( $data['max_location_creation'] ) ? 'data-max="'. $data['max_location_creation'] .'"' : '';  echo !empty( $data['create_new_loc'] ) ? 'data-allow_new="'. $data['create_new_loc'] .'"' : ''; ?>>
+	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-location" data-placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo $data['type'] == 'multiple' ? 'multiple="multiple"' : '';  echo !empty( $data['max_location_creation'] ) ? 'data-max="'. $data['max_location_creation'] .'"' : '';  echo !empty( $data['create_new_loc'] ) ? 'data-allow_new="'. $data['create_new_loc'] .'"' : ''; ?>>
 
 		<?php
 		if ($data['type'] != 'multiple') {

ac2cedc30790aa5b68f890dc20c9d0eb5129a9e5
Syed Galib Ahmed <55786211+syedgalib@users.noreply.github.com>
2021-07-27 11:34:09 +0600

    fixed invalid key declaration in settings panel (#374)

--- includes/classes/class-settings-panel.php
+++ includes/classes/class-settings-panel.php
@@ -655,7 +655,7 @@ Please remember that your order may be canceled if you do not make your payment 
                 'announcement_send_to_email' => [
                     'label'   => __('Send a copy to email', 'directorist'),
                     'type'    => 'toggle',
-                    'default' => true,
+                    'value' => true,
                 ],
 
                 'button_type' => [
@@ -1372,13 +1372,13 @@ Please remember that your order may be canceled if you do not make your payment 
                 'primary_dark_back_color' => [
                     'type' => 'color',
                     'label' => __('Background Color', 'directorist'),
-                    'default' => '#444752',
+                    'value' => '#444752',
                 ],
 
                 'primary_dark_border_color' => [
                     'type' => 'color',
                     'label' => __('Border Color', 'directorist'),
-                    'default' => '#444752',
+                    'value' => '#444752',
                 ],
 
                 'font_type' => [

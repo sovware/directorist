4c371cad8abc8bf790b56ba17248de44fe6b0888
Md Kowsart Hossain <kowsar9989@gmail.com>
2021-07-29 10:44:21 +0600

    some changes

--- templates/single/fields/share.php
+++ templates/single/fields/share.php
@@ -15,10 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<ul class="directorist-social-share-links">
 		<?php foreach ( $listing->social_share_data() as $social ): ?>
 			<li class="directorist-social-links__item">
-				<a href="<?php echo esc_url( $social['link'] );?>">
-					<span class="<?php echo esc_attr( $social['icon'] );?>"></span>
-					<?php echo esc_html( $social['title'] );?>
-				</a>
+				<a href="<?php echo esc_url( $social['link'] );?>"><span class="<?php echo esc_attr( $social['icon'] );?>"></span><?php echo esc_html( $social['title'] );?></a>
 			</li>
 		<?php endforeach; ?>
 	</ul>
--- templates/single/fields/tag.php
+++ templates/single/fields/tag.php
@@ -17,10 +17,7 @@ if ( empty( $listing->get_tags() ) ) {
 	<?php foreach ( $listing->get_tags() as $tag ): ?>
 
 		<li>
-			<a href="<?php echo esc_url( get_term_link( $tag->term_id, ATBDP_TAGS ) ); ?>">
-				<span class="<?php echo apply_filters('atbdp_single_listing_tags_icon', atbdp_icon_type().'-tag'); ?>"></span>
-				<?php echo esc_html( $tag->name ); ?>
-			</a>
+			<a href="<?php echo esc_url( get_term_link( $tag->term_id, ATBDP_TAGS ) ); ?>"><span class="<?php echo apply_filters('atbdp_single_listing_tags_icon', atbdp_icon_type().'-tag'); ?>"></span> <?php echo esc_html( $tag->name ); ?></a>
 		</li>
 	
 	<?php endforeach; ?>

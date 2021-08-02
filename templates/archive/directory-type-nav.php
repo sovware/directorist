addbc9ee0049543477d851e9f45f2df1988274d5
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-08-02 14:28:29 +0600

    Permalink issue in directory type nav (#391)
    * fixed permalink issue in directory type nav
    
    * fixed indentation issue
    
    * spacing
    
    Co-authored-by: Syed Galib Ahmed <syedgalibahmed@gmail.com>

--- templates/archive/directory-type-nav.php
+++ templates/archive/directory-type-nav.php
@@ -2,28 +2,43 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 6.7
+ * @version 7.0.5.2
  */
 
 use \Directorist\Helper;
 
 if ( ! defined( 'ABSPATH' ) ) exit;
-$get_current_url_type = isset( $_GET['directory_type'] ) ? $_GET['directory_type'] : '';
-?>
 
-<?php do_action( 'directorist_before_listing_types', $listings ); ?>
+$get_current_url_type = isset( $_GET['directory_type'] ) ? $_GET['directory_type'] : '';
+
+do_action( 'directorist_before_listing_types', $listings );
+?>
 
 <div class="<?php Helper::directorist_container_fluid(); ?>">
 
 	<div class="directorist-type-nav">
 		<ul class="directorist-type-nav__list">
+
 			<?php if( ! empty( $all_types ) ) { ?>
-				<li class="list-inline-item <?php echo ( $listings->current_listing_type == 'all' || 'all' == $get_current_url_type ) ? 'current' : ''; ?>"><a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', 'all' ) ); ?>"><span class=""></span> <?php _e( 'All', 'directorist' ); ?></a></li>
+
+				<li class="list-inline-item <?php echo ( $listings->current_listing_type == 'all' || 'all' == $get_current_url_type ) ? 'current' : ''; ?>">
+
+					<a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', 'all' ) ); ?>"><span class=""></span> <?php esc_html_e( 'All', 'directorist' ); ?></a>
+
+				</li>
+
 			<?php } ?>
-			<?php foreach ( $listings->listing_types as $id => $value ): ?>
+
+			<?php
+			foreach ( $listings->listing_types as $id => $value ):
+
+				$nav_link = add_query_arg( [ 'directory_type' => $value['term']->slug ], get_permalink( get_the_ID() ) ); 
+				?>
 
 				<li class="<?php echo ( $listings->current_listing_type == $value['term']->term_id && 'all' != $get_current_url_type ) ? 'current': ''; ?>">
-					<a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', $value['term']->slug ) ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
+
+					<a class="directorist-type-nav__link" href="<?php echo esc_url( $nav_link ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
+
 				</li>
 				
 			<?php endforeach; ?>

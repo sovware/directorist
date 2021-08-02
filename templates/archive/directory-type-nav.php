afc7ad778d3667979f75ad0141254dc9e372d47c
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-08-02 10:56:19 +0600

    fixed permalink issue in directory type nav

--- templates/archive/directory-type-nav.php
+++ templates/archive/directory-type-nav.php
@@ -20,11 +20,10 @@ $get_current_url_type = isset( $_GET['directory_type'] ) ? $_GET['directory_type
 			<?php if( ! empty( $all_types ) ) { ?>
 				<li class="list-inline-item <?php echo ( $listings->current_listing_type == 'all' || 'all' == $get_current_url_type ) ? 'current' : ''; ?>"><a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', 'all' ) ); ?>"><span class=""></span> <?php _e( 'All', 'directorist' ); ?></a></li>
 			<?php } ?>
-			<?php foreach ( $listings->listing_types as $id => $value ): ?>
-
-				<li class="<?php echo ( $listings->current_listing_type == $value['term']->term_id && 'all' != $get_current_url_type ) ? 'current': ''; ?>">
-					<a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', $value['term']->slug ) ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
-				</li>
+			<?php foreach ( $listings->listing_types as $id => $value ): 
+				$nav_link = add_query_arg( [ 'directory_type' => $value['term']->slug ], get_permalink( get_the_ID() ) ); 
+				?>
+				<li class="<?php echo ( $listings->current_listing_type == $value['term']->term_id && 'all' != $get_current_url_type ) ? 'current': ''; ?>"><a class="directorist-type-nav__link" href="<?php echo esc_url( $nav_link ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a></li>
 				
 			<?php endforeach; ?>
 			

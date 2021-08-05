a1dcb5b379bb9797dabb312a8043662d6263e8e6
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-08-05 14:45:53 +0600

    check with theme & update

--- assets/js/public-main.js
+++ assets/js/public-main.js
@@ -871,6 +871,11 @@ atbdSelectData.forEach(function (el) {
 (function ($) {
   if ($('.directorist-listing-no-thumb').innerWidth() <= 220) {
     $('.directorist-listing-no-thumb').addClass('directorist-listing-no-thumb--fix');
+  } // Auhtor Profile Listing responsive fix
+
+
+  if ($('.directorist-author-listing-content').innerWidth() <= 650) {
+    $('.directorist-author-listing-content').addClass('directorist-author-listing-grid--fix');
   }
 })(jQuery);
 
--- assets/src/js/public/components/general.js
+++ assets/src/js/public/components/general.js
@@ -3,4 +3,9 @@
     if($('.directorist-listing-no-thumb').innerWidth() <= 220 ){
         $('.directorist-listing-no-thumb').addClass('directorist-listing-no-thumb--fix');
     }
+
+    // Auhtor Profile Listing responsive fix
+    if($('.directorist-author-listing-content').innerWidth() <= 650){
+        $('.directorist-author-listing-content').addClass('directorist-author-listing-grid--fix');
+    }
 })(jQuery)

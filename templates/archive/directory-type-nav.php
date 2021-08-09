4f691474473fc1c77a2c141bc83cd27a639158a5
obiPlabon <obiplabon@gmail.com>
2021-08-09 15:32:10 +0600

    Fixed multi directory nav url issue

--- templates/archive/directory-type-nav.php
+++ templates/archive/directory-type-nav.php
@@ -9,41 +9,37 @@ use \Directorist\Helper;
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-$get_current_url_type = isset( $_GET['directory_type'] ) ? $_GET['directory_type'] : '';
+$current_directory_type = ( ! empty( $_GET['directory_type'] ) ? $_GET['directory_type'] : '' );
 
 do_action( 'directorist_before_listing_types', $listings );
+
 ?>
-
 <div class="<?php Helper::directorist_container_fluid(); ?>">
-
 	<div class="directorist-type-nav">
 		<ul class="directorist-type-nav__list">
 
-			<?php if( ! empty( $all_types ) ) { ?>
+			<?php if ( ! empty( $all_types ) ) : ?>
 
-				<li class="list-inline-item <?php echo ( $listings->current_listing_type == 'all' || 'all' == $get_current_url_type ) ? 'current' : ''; ?>">
-
-					<a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg('directory_type', 'all' ) ); ?>"><span class=""></span> <?php esc_html_e( 'All', 'directorist' ); ?></a>
-
+				<li class="list-inline-item <?php echo ( $listings->current_listing_type === 'all' || 'all' === $current_directory_type ) ? 'current' : ''; ?>">
+					<a class="directorist-type-nav__link" href="<?php echo esc_url( add_query_arg( 'directory_type', 'all' ) ); ?>"><?php esc_html_e( 'All', 'directorist' ); ?></a>
 				</li>
 
-			<?php } ?>
+			<?php endif; ?>
 
 			<?php
-			foreach ( $listings->listing_types as $id => $value ):
+			foreach ( $listings->listing_types as $id => $value ) :
 
-				$nav_link = add_query_arg( [ 'directory_type' => $value['term']->slug ], get_permalink( get_the_ID() ) ); 
+				$nav_link = add_query_arg( [ 'directory_type' => $value['term']->slug ] );
 				?>
 
-				<li class="<?php echo ( $listings->current_listing_type == $value['term']->term_id && 'all' != $get_current_url_type ) ? 'current': ''; ?>">
-
-					<a class="directorist-type-nav__link" href="<?php echo esc_url( $nav_link ); ?>"><span class="<?php echo esc_html( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
-
+				<li class="<?php echo ( ( $listings->current_listing_type === $value['term']->term_id && 'all' !== $current_directory_type ) ? 'current': '' ); ?>">
+					<a class="directorist-type-nav__link" href="<?php echo esc_url( $nav_link ); ?>"><span class="<?php echo esc_attr( $value['data']['icon'] );?>"></span> <?php echo esc_html( $value['name'] );?></a>
 				</li>
-				
-			<?php endforeach; ?>
-			
+
+				<?php
+			endforeach;
+			?>
+
 		</ul>
 	</div>
-
 </div>

6bd46ef7d810dd70ad7dc65091e7c68bb54ff93f
Md Kowsart Hossain <kowsar9989@gmail.com>
2021-08-25 09:56:06 +0600

    some refractoring

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -476,9 +476,9 @@ class Helper {
 		) );
 
 		foreach ( $types as $type ) {
-			$page_id = get_directorist_type_option( $type->term_id, 'single_listing_page' );
-			$is_enable = get_directorist_type_option( $type->term_id, 'enable_single_listing_page' );
-			if ( $page_id  && $is_enable ) {
+			$page_id   = get_directorist_type_option( $type->term_id, 'single_listing_page' );
+			$single_listing_enabled = get_directorist_type_option( $type->term_id, 'enable_single_listing_page' );
+			if ( $single_listing_enabled && $page_id ) {
 				$pages[$page_id] = $type->name;
 			}
 		}

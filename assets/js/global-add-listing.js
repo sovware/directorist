ad1607ada8915188714a5ebec19633c6b148edaf
shamimsaj <shamimeforb@gmail.com>
2021-09-05 09:57:42 +0600

    Fixed: issues assigned in trello - v7.0.5.4 - 1th, Sep

--- assets/js/global-add-listing.js
+++ assets/js/global-add-listing.js
@@ -372,6 +372,7 @@ $('#at_biz_dir-categories').on('change', function () {
       }
 
       atbdp_tooltip();
+      customFieldSeeMore();
     } else {
       $('.atbdp_category_custom_fields').empty();
     }
@@ -788,18 +789,22 @@ $('body').on('submit', formID, function (e) {
   });
 }); // Custom Field Checkbox Button More
 
-$(window).on('load', function () {
+function customFieldSeeMore() {
   if ($('.directorist-custom-field-btn-more').length) {
     $('.directorist-custom-field-btn-more').each(function (index, element) {
       var fieldWrapper = $(element).closest('.directorist-custom-field-checkbox, .directorist-custom-field-radio');
       var customField = $(fieldWrapper).find('.directorist-checkbox, .directorist-radio');
       $(customField).slice(20, customField.length).slideUp();
 
-      if (customField.length < 20) {
+      if (customField.length <= 20) {
         $(element).slideUp();
       }
     });
   }
+}
+
+$(window).on('load', function () {
+  customFieldSeeMore();
 });
 $('body').on('click', '.directorist-custom-field-btn-more', function (event) {
   event.preventDefault();

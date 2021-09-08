c39d266db91e0b203b2e14c9b9670dc99864c253
vairafiq <vairafiq@gmail.com>
2021-09-08 10:25:34 +0600

    review placeholder issue

--- templates/search-form/fields/review.php
+++ templates/search-form/fields/review.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 6.7
+ * @version 7.0.5.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		<label><?php echo esc_html( $data['label'] ); ?></label>
 	<?php endif; ?>
 	<div class="directorist-select" id="directorist-review-select-js">
-		<select name='search_by_rating' <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true">
+		<select name='search_by_rating' <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-placeholder="<?php echo __('Select ', 'directorist') . esc_attr($data['label']); ?>" data-isSearch="true">
 			<?php
 				foreach ( $searchform->rating_field_data() as $option ) {
 					printf('<option value="%s"%s>%s</option>', $option['value'], $option['selected'], $option['label']);

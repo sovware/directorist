d6dfe5a28c528fa061b856d5c53edf7bc0e22040
obiPlabon <obiplabon@gmail.com>
2021-09-12 11:28:46 +0600

    Updated builder screen fields

--- includes/review/class-builder-screen.php
+++ includes/review/class-builder-screen.php
@@ -7,7 +7,9 @@
  */
 namespace Directorist\Review;
 
-defined( 'ABSPATH' ) || die();
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 class Builder_Screen {
 
@@ -18,41 +20,34 @@ class Builder_Screen {
 	}
 
 	public static function register_config( $config ) {
-		$config['fields_group']['review_config'] = array_keys( self::get_fields() );
+		$review_config = apply_filters( 'directorist/builder/config/review_form', array_keys( self::get_fields() ) );
+		$config['fields_group']['review_config'] = $review_config;
+
 		return $config;
 	}
 
 	public static function register_layout( $layouts ) {
-		$layouts['review_form'] = [
+		$layout = [
 			'label'     => __( 'Review Form', 'directorist' ),
 			'icon'      => '<span class="uil uil-star"></span>',
 			'container' => 'wide',
-			'sections'  => apply_filters( 'directorist/builder/sections/review_form', self::get_sections() ),
+			'sections'  => self::get_sections(),
 		];
+
+		$layouts['review_form'] = apply_filters( 'directorist/builder/layouts/review_form', $layout );
 
 		return $layouts;
 	}
 
 	protected static function get_sections() {
-		return [
-			// 'rating_fields' => [
-			// 	// 'title'     => __( 'Rating', 'directorist' ),
-			// 	// 'container' => 'short-width',
-			// 	'fields'    => [
-			// 		'review_rating_type',
-			// 		// 'review_rating_criteria',
-			// 	],
-			// ],
-			// 'attachment_fields' => [
-			// 	'title'     => __( 'Images', 'directorist' ),
-			// 	'container' => 'short-width',
-			// 	'fields'    => [
-			// 		'review_enable_attachments',
-			// 		'review_max_attachments',
-			// 		'review_attachments_size',
-			// 		'review_attachments_required',
-			// 	],
-			// ],
+		$sections = [
+			'rating_fields' => [
+				'title'     => __( 'Rating', 'directorist' ),
+				'container' => 'short-width',
+				'fields'    => [
+					'review_rating_type',
+				],
+			],
 			'regular_fields' => [
 				'title'     => __( 'Regular Fields', 'directorist' ),
 				'container' => 'short-width',
@@ -68,72 +63,21 @@ class Builder_Screen {
 				],
 			],
 		];
+
+		return apply_filters( 'directorist/builder/sections/review_form', $sections );
 	}
 
 	public static function get_fields() {
-		return [
+		$fields = [
 			// Rating fields
-			// 'review_rating_type' => [
-			// 	'type' => 'hidden',
-			// 	// 'label' => __( 'Rating Type', 'directorist' ),
-			// 	// 'options' => [
-			// 	// 	[ 'value' => 'single' , 'label' => __( 'Single', 'directorist' ) ],
-			// 	// 	[ 'value' => 'multiple' , 'label' => __( 'Multiple', 'directorist' ) ],
-			// 	// ],
-			// 	'value' => 'single'
-			// ],
-			// 'review_rating_criteria' => [
-			// 	'type' => 'textarea',
-			// 	'label' => __( 'Rating Criteria', 'directorist' ),
-			// 	'description' => __( 'Make sure to write criteria in <code>key | Label</code> pairs with valid keys (<code>a-z,0-9,_,-</code>) and separate each of them with a new line. Removing or changing <code>key</code> may cause data loss.', 'directorist' ),
-			// 	'show_if' => [
-			// 		'where' => 'review_rating_type',
-			// 		'conditions' => [
-			// 			['key' => 'value', 'compare' => '=', 'value' => 'multiple'],
-			// 		],
-			// 	],
-			// 	'value' => "service | Service\nlocation | Location\nprice | Price\n",
-			// ],
-
-			// Attachment fields
-			// 'review_enable_attachments' => [
-			// 	'label' => __( 'Enable', 'directorist' ),
-			// 	'type'  => 'toggle',
-			// 	'value' => true,
-			// ],
-			// 'review_max_attachments' => [
-			// 	'label' => __( 'Number of Images', 'directorist' ),
-			// 	'type'  => 'number',
-			// 	'value' => 3,
-			// 	'show_if' => [
-			// 		'where' => 'review_enable_attachments',
-			// 		'conditions' => [
-			// 			['key' => 'value', 'compare' => '=', 'value' => true],
-			// 		],
-			// 	],
-			// ],
-			// 'review_attachments_size' => [
-			// 	'label' => __( 'Total Size (MB)', 'directorist' ),
-			// 	'type'  => 'text',
-			// 	'value' => 2,
-			// 	'show_if' => [
-			// 		'where' => 'review_enable_attachments',
-			// 		'conditions' => [
-			// 			['key' => 'value', 'compare' => '=', 'value' => true],
-			// 		],
-			// 	],
-			// ],
-			// 'review_attachments_required' => [
-			// 	'label' => __( 'Required', 'directorist' ),
-			// 	'type'  => 'toggle',
-			// 	'value' => false,
-			// 	'show_if' => [
-			// 		'where' => 'review_enable_attachments',
-			// 		'conditions' => [
-			// 			['key' => 'value', 'compare' => '=', 'value' => true],
-			// 		],
-			// 	],
-			// ],
+			'review_rating_type' => [
+				'type' => 'select',
+				'label' => __( 'Rating Type', 'directorist' ),
+				'options' => [
+					[ 'value' => 'single' , 'label' => __( 'Single', 'directorist' ) ],
+				],
+				'value' => 'single'
+			],
 
 			// Regular fields
 			'review_comment_label' => [
@@ -177,6 +121,8 @@ class Builder_Screen {
 				'value' => __( 'Website url', 'directorist' ),
 			],
 		];
+
+		return apply_filters( 'directorist/builder/fields/review_form', $fields );
 	}
 
 	public static function register_fields( $fields ) {

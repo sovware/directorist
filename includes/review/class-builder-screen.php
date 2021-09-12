0d3fea8fc91436e638c8b65288adb65302c51968
obiPlabon <obiplabon@gmail.com>
2021-09-12 08:54:47 +0600

    Disabled fields

--- includes/review/class-builder-screen.php
+++ includes/review/class-builder-screen.php
@@ -35,24 +35,24 @@ class Builder_Screen {
 
 	protected static function get_sections() {
 		return [
-			'rating_fields' => [
-				'title'     => __( 'Rating', 'directorist' ),
-				'container' => 'short-width',
-				'fields'    => [
-					'review_rating_type',
-					'review_rating_criteria',
-				],
-			],
-			'attachment_fields' => [
-				'title'     => __( 'Images', 'directorist' ),
-				'container' => 'short-width',
-				'fields'    => [
-					'review_enable_attachments',
-					'review_max_attachments',
-					'review_attachments_size',
-					'review_attachments_required',
-				],
-			],
+			// 'rating_fields' => [
+			// 	// 'title'     => __( 'Rating', 'directorist' ),
+			// 	// 'container' => 'short-width',
+			// 	'fields'    => [
+			// 		'review_rating_type',
+			// 		// 'review_rating_criteria',
+			// 	],
+			// ],
+			// 'attachment_fields' => [
+			// 	'title'     => __( 'Images', 'directorist' ),
+			// 	'container' => 'short-width',
+			// 	'fields'    => [
+			// 		'review_enable_attachments',
+			// 		'review_max_attachments',
+			// 		'review_attachments_size',
+			// 		'review_attachments_required',
+			// 	],
+			// ],
 			'regular_fields' => [
 				'title'     => __( 'Regular Fields', 'directorist' ),
 				'container' => 'short-width',
@@ -73,67 +73,67 @@ class Builder_Screen {
 	public static function get_fields() {
 		return [
 			// Rating fields
-			'review_rating_type' => [
-				'type' => 'select',
-				'label' => __( 'Rating Type', 'directorist' ),
-				'options' => [
-					[ 'value' => 'single' , 'label' => __( 'Single', 'directorist' ) ],
-					[ 'value' => 'multiple' , 'label' => __( 'Multiple', 'directorist' ) ],
-				],
-				'value' => 'single'
-			],
-			'review_rating_criteria' => [
-				'type' => 'textarea',
-				'label' => __( 'Rating Criteria', 'directorist' ),
-				'description' => __( 'Make sure to write criteria in <code>key | Label</code> pairs with valid keys (<code>a-z,0-9,_,-</code>) and separate each of them with a new line. Removing or changing <code>key</code> may cause data loss.', 'directorist' ),
-				'show_if' => [
-					'where' => 'review_rating_type',
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => 'multiple'],
-					],
-				],
-				'value' => "service | Service\nlocation | Location\nprice | Price\n",
-			],
+			// 'review_rating_type' => [
+			// 	'type' => 'hidden',
+			// 	// 'label' => __( 'Rating Type', 'directorist' ),
+			// 	// 'options' => [
+			// 	// 	[ 'value' => 'single' , 'label' => __( 'Single', 'directorist' ) ],
+			// 	// 	[ 'value' => 'multiple' , 'label' => __( 'Multiple', 'directorist' ) ],
+			// 	// ],
+			// 	'value' => 'single'
+			// ],
+			// 'review_rating_criteria' => [
+			// 	'type' => 'textarea',
+			// 	'label' => __( 'Rating Criteria', 'directorist' ),
+			// 	'description' => __( 'Make sure to write criteria in <code>key | Label</code> pairs with valid keys (<code>a-z,0-9,_,-</code>) and separate each of them with a new line. Removing or changing <code>key</code> may cause data loss.', 'directorist' ),
+			// 	'show_if' => [
+			// 		'where' => 'review_rating_type',
+			// 		'conditions' => [
+			// 			['key' => 'value', 'compare' => '=', 'value' => 'multiple'],
+			// 		],
+			// 	],
+			// 	'value' => "service | Service\nlocation | Location\nprice | Price\n",
+			// ],
 
 			// Attachment fields
-			'review_enable_attachments' => [
-				'label' => __( 'Enable', 'directorist' ),
-				'type'  => 'toggle',
-				'value' => true,
-			],
-			'review_max_attachments' => [
-				'label' => __( 'Number of Images', 'directorist' ),
-				'type'  => 'number',
-				'value' => 3,
-				'show_if' => [
-					'where' => 'review_enable_attachments',
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'review_attachments_size' => [
-				'label' => __( 'Total Size (MB)', 'directorist' ),
-				'type'  => 'text',
-				'value' => 2,
-				'show_if' => [
-					'where' => 'review_enable_attachments',
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'review_attachments_required' => [
-				'label' => __( 'Required', 'directorist' ),
-				'type'  => 'toggle',
-				'value' => false,
-				'show_if' => [
-					'where' => 'review_enable_attachments',
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
+			// 'review_enable_attachments' => [
+			// 	'label' => __( 'Enable', 'directorist' ),
+			// 	'type'  => 'toggle',
+			// 	'value' => true,
+			// ],
+			// 'review_max_attachments' => [
+			// 	'label' => __( 'Number of Images', 'directorist' ),
+			// 	'type'  => 'number',
+			// 	'value' => 3,
+			// 	'show_if' => [
+			// 		'where' => 'review_enable_attachments',
+			// 		'conditions' => [
+			// 			['key' => 'value', 'compare' => '=', 'value' => true],
+			// 		],
+			// 	],
+			// ],
+			// 'review_attachments_size' => [
+			// 	'label' => __( 'Total Size (MB)', 'directorist' ),
+			// 	'type'  => 'text',
+			// 	'value' => 2,
+			// 	'show_if' => [
+			// 		'where' => 'review_enable_attachments',
+			// 		'conditions' => [
+			// 			['key' => 'value', 'compare' => '=', 'value' => true],
+			// 		],
+			// 	],
+			// ],
+			// 'review_attachments_required' => [
+			// 	'label' => __( 'Required', 'directorist' ),
+			// 	'type'  => 'toggle',
+			// 	'value' => false,
+			// 	'show_if' => [
+			// 		'where' => 'review_enable_attachments',
+			// 		'conditions' => [
+			// 			['key' => 'value', 'compare' => '=', 'value' => true],
+			// 		],
+			// 	],
+			// ],
 
 			// Regular fields
 			'review_comment_label' => [

f300da7e8c29d0c4290f6abfed06250da828f15a
Md Kowsar Hossain <kowsar9989@gmail.com>
2021-11-07 15:00:29 +0600

    Feature/all authors (#543)
    * All authors pagination (#520)
    
    * template updated
    
    * class updated
    
    * info
    
    * author fix
    
    * js fix
    
    * All authors name issue (#526)
    
    * Fixed: authors card, nav style and masonry alignment issue (#540)
    
    * typo
    
    Co-authored-by: Rafiq <36886362+vairafiq@users.noreply.github.com>
    Co-authored-by: Armanul46 <47377178+Armanul46@users.noreply.github.com>
    Co-authored-by: Shamim Ahmed <shamimeforb@gmail.com>

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -340,22 +340,64 @@ class Helper {
 		}
 	}
 
-	public static function phone_link( $args ) {
+	public static function user_info( $user_id_or_obj, $meta ) {
 
-		$defaults = array(
-			'number'    => '',
-			'whatsapp'  => false,
-		);
-
-		$args = wp_parse_args( $args, $defaults );
-
-		$num = self::formatted_tel( $args['number'], false );
-
-		if ( $args['whatsapp'] ) {
-			$result = sprintf( 'https://wa.me/%s', $num );
+		if ( is_integer( $user_id_or_obj ) ) {
+			$user_id = $user_id_or_obj;
+			$user = get_userdata( $user_id );
 		}
 		else {
-			$result = sprintf( 'tel:%s', $num );
+			$user = $user_id_or_obj;
+			$user_id = $user->data->ID;
+		}
+
+		$result = '';
+
+		switch ( $meta ) {
+			case 'name':
+			$result = $user->data->display_name;
+			break;
+
+			case 'role':
+			$result = $user->roles[0];
+			break;
+
+			case 'address':
+			$result = get_user_meta($user_id, 'address', true);
+			break;
+
+			case 'phone':
+			$result = get_user_meta($user_id, 'atbdp_phone', true);
+			break;
+
+			case 'email':
+			$result = $user->data->user_email;
+			break;
+
+			case 'website':
+			$result = $user->data->user_url;
+			break;
+
+			case 'description':
+			$result = trim( get_user_meta( $user_id, 'description', true ) );
+			//var_dump($result);
+			break;
+
+			case 'facebook':
+			$result = get_user_meta($user_id, 'atbdp_facebook', true);
+			break;
+
+			case 'twitter':
+			$result = get_user_meta($user_id, 'atbdp_twitter', true);
+			break;
+
+			case 'linkedin':
+			$result = get_user_meta($user_id, 'atbdp_linkedin', true);
+			break;
+
+			case 'youtube':
+			$result = get_user_meta($user_id, 'atbdp_youtube', true);
+			break;
 		}
 
 		return $result;

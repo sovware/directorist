84d33e05af84ce1851306f42f6e46e8434994e81
obiPlabon <obiplabon@gmail.com>
2021-11-22 15:25:43 +0600

    Change review settings location and removed unused settings

--- includes/review/class-settings-screen.php
+++ includes/review/class-settings-screen.php
@@ -14,8 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Settings_Screen {
 
 	public static function init() {
-		// add_filter( 'atbdp_listing_type_settings_layout', [ __CLASS__, 'register_layout' ] );
-		// add_filter( 'atbdp_listing_type_settings_field_list', [ __CLASS__, 'register_fields' ] );
+		add_filter( 'atbdp_listing_type_settings_layout', [ __CLASS__, 'register_layout' ] );
+		add_filter( 'atbdp_listing_type_settings_field_list', [ __CLASS__, 'register_fields' ] );
 	}
 
 	public static function register_layout( $layout ) {
@@ -37,103 +37,135 @@ class Settings_Screen {
 	public static function get_fields() {
 		return [
 			'enable_review' => [
-				'type' => 'toggle',
-				'label' => __('Enable Reviews & Rating', 'directorist'),
+				'type'  => 'toggle',
+				'label' => __( 'Enable Review', 'directorist' ),
 				'value' => true,
 			],
 			'enable_owner_review' => [
-				'type' => 'toggle',
-				'label' => __('Enable Owner Review', 'directorist'),
-				'description' => __('Allow a listing owner to post a review on his/her own listing.', 'directorist'),
-				'value' => true,
-				'show-if' => [
-					'where' => "enable_review",
+				'type'        => 'toggle',
+				'label'       => __( 'Allow Owner Review', 'directorist'),
+				'description' => __( 'You are allowing listing owners to post review on his/her own listings.', 'directorist' ),
+				'value'       => true,
+				'show-if'     => [
+					'where'      => 'enable_review',
 					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'approve_immediately' => [
-				'type' => 'toggle',
-				'label' => __('Approve Immediately?', 'directorist'),
-				'value' => true,
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'review_approval_text' => [
-				'type' => 'textarea',
-				'label' => __('Approval Notification Text', 'directorist'),
-				'description' => __('You can set the title for featured listing to show on the ORDER PAGE', 'directorist'),
-				'value' => __('We have received your review. It requires approval.', 'directorist'),
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'enable_reviewer_img' => [
-				'type' => 'toggle',
-				'label' => __('Enable Reviewer Image', 'directorist'),
-				'value' => true,
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'enable_reviewer_content' => [
-				'type' => 'toggle',
-				'label' => __('Enable Reviewer content', 'directorist'),
-				'value' => true,
-				'description' => __('Allow to display content of reviewer on single listing page.', 'directorist'),
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'required_reviewer_content' => [
-				'type' => 'toggle',
-				'label' => __('Required Reviewer content', 'directorist'),
-				'value' => true,
-				'description' => __('Allow to Require the content of reviewer on single listing page.', 'directorist'),
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
-					],
-				],
-			],
-			'review_num' => [
-				'label' => __('Number of Reviews', 'directorist'),
-				'description' => __('Enter how many reviews to show on Single listing page.', 'directorist'),
-				'type'  => 'number',
-				'value' => '5',
-				'min' => '1',
-				'max' => '20',
-				'step' => '1',
-				'show-if' => [
-					'where' => "enable_review",
-					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
+						[
+							'key'     => 'value',
+							'compare' => '=',
+							'value'   => true
+						],
 					],
 				],
 			],
 			'guest_review' => [
-				'type' => 'toggle',
-				'label' => __('Guest Review Submission', 'directorist'),
-				'value' => false,
+				'type'    => 'toggle',
+				'label'   => __( 'Allow Guest Review', 'directorist' ),
+				'value'   => false,
 				'show-if' => [
-					'where' => "enable_review",
+					'where'      => 'enable_review',
 					'conditions' => [
-						['key' => 'value', 'compare' => '=', 'value' => true],
+						[
+							'key'     => 'value',
+							'compare' => '=',
+							'value'   => true
+						],
+					],
+				],
+			],
+			'approve_immediately' => [
+				'type'    => 'toggle',
+				'label'   => __( 'Approve Immediately?', 'directorist' ),
+				'description' => __( 'Are you sure you do not need any review or comment moderation?', 'directorist' ),
+				'value'   => true,
+				'show-if' => [
+					'where'      => 'enable_review',
+					'conditions' => [
+						[
+							'key'     => 'value',
+							'compare' => '=',
+							'value'   => true
+						],
+					],
+				],
+			],
+			'review_approval_text' => [
+				'type'        => 'textarea',
+				'label'       => __( 'Approval Notification Text', 'directorist'),
+				'value'       => __( 'We have received your review. It requires approval.', 'directorist' ),
+				'show-if'     => [
+					'where'      => 'enable_review',
+					'conditions' => [
+						[
+							'key'     => 'value',
+							'compare' => '=',
+							'value'   => true
+						],
+					],
+				],
+			],
+			// 'enable_reviewer_img' => [
+			// 	'type'    => 'toggle',
+			// 	'label'   => __( 'Enable Reviewer Image', 'directorist' ),
+			// 	'value'   => true,
+			// 	'show-if' => [
+			// 		'where'      => 'enable_review',
+			// 		'conditions' => [
+			// 			[
+			// 				'key'     => 'value',
+			// 				'compare' => '=',
+			// 				'value'   => true
+			// 			],
+			// 		],
+			// 	],
+			// ],
+			// 'enable_reviewer_content' => [
+			// 	'type'        => 'toggle',
+			// 	'label'       => __('Enable Reviewer content', 'directorist'),
+			// 	'value'       => true,
+			// 	'description' => __('Allow to display content of reviewer on single listing page.', 'directorist'),
+			// 	'show-if'     => [
+			// 		'where'      => 'enable_review',
+			// 		'conditions' => [
+			// 			[
+			// 				'key'     => 'value',
+			// 				'compare' => '=',
+			// 				'value'   => true
+			// 			],
+			// 		],
+			// 	],
+			// ],
+			// 'required_reviewer_content' => [
+			// 	'type'        => 'toggle',
+			// 	'label'       => __('Required Reviewer content', 'directorist'),
+			// 	'value'       => true,
+			// 	'description' => __('Allow to Require the content of reviewer on single listing page.', 'directorist'),
+			// 	'show-if'     => [
+			// 		'where'      => 'enable_review',
+			// 		'conditions' => [
+			// 			[
+			// 				'key'     => 'value',
+			// 				'compare' => '=',
+			// 				'value'   => true
+			// 			],
+			// 		],
+			// 	],
+			// ],
+			'review_num' => [
+				'label'       => __( 'Number of Reviews', 'directorist' ),
+				'description' => __( 'Number of reviews to show per page. We do not recommend more than 10, since it will impact loading speed.', 'directorist' ),
+				'type'        => 'number',
+				'value'       => 5,
+				'min'         => 1,
+				'max'         => 20,
+				'step'        => 1,
+				'show-if'     => [
+					'where'      => 'enable_review',
+					'conditions' => [
+						[
+							'key'     => 'value',
+							'compare' => '=',
+							'value'   => true
+						],
 					],
 				],
 			],

edd84ec14413060681d48f4db11280d3fa209780
Armanul46 <arman.sovware@gmail.com>
2021-12-05 11:45:19 +0600

    fixed add listing type issue

--- includes/model/ListingForm.php
+++ includes/model/ListingForm.php
@@ -543,9 +543,11 @@ class Directorist_Listing_Form {
 	}
 
 	public function type_hidden_field() {
-		$value             = get_post_meta( $this->get_add_listing_id(), '_directory_type', true );
-		$default_directory = default_directory_type();
-		$current_type      = ! empty( $value ) ? $value : $default_directory;
+		$value             		= get_post_meta( $this->get_add_listing_id(), '_directory_type', true );
+		$current_directory_type = $this->get_current_listing_type();
+		$default_directory 		= default_directory_type();
+		$directory_type         = ! empty( $current_directory_type ) ? $current_directory_type : $default_directory;
+		$current_type      		= ! empty( $value ) ? $value : $directory_type;
 		echo '<input type="hidden" name="directory_type" value="'.$current_type.'">';
 	}
 

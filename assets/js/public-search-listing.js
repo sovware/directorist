1a6b7032b72a089f16cdfafe28d8530a4eb54273
Shamim Ahmed <shamimeforb@gmail.com>
2021-12-06 12:15:51 +0600

    Fixed: overlaped search filter issue

--- assets/js/public-search-listing.js
+++ assets/js/public-search-listing.js
@@ -229,7 +229,7 @@
   };
 
   $(document).on('click', function (e) {
-    if (!e.target.closest('.directorist-search-form-top, .directorist-listings-header') && !e.target.closest('.directorist-search-float .directorist-advanced-filter')) {
+    if (!e.target.closest('.directorist-search-form-top, .directorist-listings-header, .directorist-search-form') && !e.target.closest('.directorist-search-float .directorist-advanced-filter')) {
       count = 0;
       daf().css({
         visibility: 'hidden',

0d3661204f81edc86c53d870c2bdb2e14a20527c
Md Kowsart Hossain <kowsar9989@gmail.com>
2022-01-06 09:13:14 +0600

    remove unnecessaery hooks

--- includes/classes/class-template-hooks.php
+++ includes/classes/class-template-hooks.php
@@ -22,27 +22,6 @@ class Directorist_Template_Hooks {
 		// All Locations
 		add_action( 'atbdp_before_all_locations_loop',    array( '\wpWax\Directorist\Model\Listing_Taxonomy', 'archive_type' ) );
 
-		// Listing Archive
-		add_action( 'directorist_archive_header',    array( '\wpWax\Directorist\Model\Listings', 'archive_type' ) );
-		add_action( 'directorist_archive_header',    array( '\wpWax\Directorist\Model\Listings', 'archive_header' ), 15 );
-
-		// Listings Badges - Grid View
-		add_filter( 'atbdp_grid_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'featured_badge') );
-		add_filter( 'atbdp_grid_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'popular_badge'), 15 );
-		add_filter( 'atbdp_grid_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'new_listing_badge'), 20 );
-
-		// Listings Badges - List View
-		add_filter( 'atbdp_list_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'featured_badge_list_view') );
-		add_filter( 'atbdp_list_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'populer_badge_list_view'), 15 );
-		add_filter( 'atbdp_list_lower_badges', array( '\wpWax\Directorist\Model\Listings', 'new_badge_list_view'), 20 );
-
-		// Listings Top - List View
-		add_action( 'directorist_list_view_listing_meta_end', array( '\wpWax\Directorist\Model\Listings', 'list_view_business_hours') );
-		add_action( 'directorist_list_view_top_content_end',  array( '\wpWax\Directorist\Model\Listings', 'mark_as_favourite_button'), 15 );
-
-		// Listing Thumbnail Area
-		add_action( 'atbdp_listing_thumbnail_area', array( '\wpWax\Directorist\Model\Listings', 'mark_as_favourite_button'), 15 );
-
 		// Single Listing
 		// Set high priority to override page builders.
 		add_filter( 'template_include', [ $this, 'single_template_path' ], 999 );

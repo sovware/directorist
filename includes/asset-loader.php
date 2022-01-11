00076057ca12da49a85cb19f09c2602360c4903e
Md Kowsart Hossain <kowsar9989@gmail.com>
2022-01-11 16:09:33 +0600

    update

--- includes/asset-loader.php
+++ includes/asset-loader.php
@@ -23,7 +23,7 @@ class Asset_Loader {
 		add_action( 'wp_enqueue_scripts',    [ $this, 'enqueue_scripts' ], 12 );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_scripts' ], 12 );
 
-		add_action( 'script_loader_tag', array( $this, 'defer_js' ), 10, 2 );
+		add_action( 'script_loader_tag', array( $this, 'defer_load_js' ), 10, 2 );
 	}
 
 	public static function instance() {
@@ -37,7 +37,7 @@ class Asset_Loader {
 		return get_directorist_option( 'script_debugging', false, true );
 	}
 
-	function defer_js( $tag, $handle ) {
+	function defer_load_js( $tag, $handle ) {
 
 		$scripts = array_filter( $this->scripts, function( $script ) {
 			return $script['type'] == 'js' ? true : false;
@@ -51,19 +51,42 @@ class Asset_Loader {
 		return $tag;
 	}
 
-	public function script_file_path( $script ) {
+	public function script_file_url( $script ) {
+		if ( !empty( $script['ext'] ) ) {
+			return $script['ext'];
+		}
+
 		$min  = $this->debug_enabled() ? '' : '.min';
 		$rtl  = ( $script['type'] == 'css' && !empty( $script['rtl'] ) && is_rtl() ) ? '.rtl' : '';
 		$ext  = $script['type'] == 'css' ? '.css' : '.js';
-		$path = $script['path'] . $min . $rtl . $ext;
-		return $path;
+		$url = $script['path'] . $min . $rtl . $ext;
+		return $url;
 	}
 
+	public function gmap_url() {
+		$api = get_directorist_option( 'map_api_key', 'AIzaSyCwxELCisw4mYqSv_cBfgOahfrPFjjQLLo' );
+		$url = '//maps.googleapis.com/maps/api/js?key=' . $api . '&libraries=places';
+		return $url;
+	}
+
+	/**
+	 * Set the scripts array.
+	 *
+	 * Each item may contain following arguments:
+	 * 		$scripts['handle'] => [
+				'type' => String, // Accespts css, js
+				'path' => String, // Absolute url, without the min/rtl/js extension
+				'ext'  => String, // External url, in case the path is absent
+				'dep'  => Array [], // Dependency list eg. [jquery]
+				'rtl'  => Boolean false, // RTL exists or not
+			];
+	 */
 	public function set_scripts() {
 		$scripts = [
 			'directorist-main-style' => [
 				'type' => 'css',
 				'path' => DIRECTORIST_CSS. 'public-main',
+				'ext'  => '',
 				'dep'  => [],
 				'rtl'  => false,
 			],
@@ -145,8 +168,6 @@ class Asset_Loader {
 				'path' => DIRECTORIST_JS. 'global-markerclusterer',
 			],
 
-			/* Vendors */
-
 			'directorist-openstreet-map-leaflet' => [
 				'type' => 'css',
 				'path' => DIRECTORIST_VENDOR_CSS . 'openstreet-map/leaflet',
@@ -161,7 +182,7 @@ class Asset_Loader {
 			],
 			'directorist-unicons' => [
 				'type' => 'js',
-				'path' => '//unicons.iconscout.com/release/v3.0.3/css/line.css',
+				'ext'  => '//unicons.iconscout.com/release/v3.0.3/css/line.css',
 			],
 			'directorist-font-awesome' => [
 				'type' => 'js',
@@ -181,10 +202,99 @@ class Asset_Loader {
 			],
 			'directorist-sweetalert' => [
 				'type' => 'js',
-				'path' => DIRECTORIST_VENDOR_CSS. 'sweetalert.min',
+				'path' => DIRECTORIST_VENDOR_CSS. 'sweetalert',
 			],
 
 
+			'directorist-openstreet-layers' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/openstreetlayers',
+			],
+			'directorist-openstreet-unpkg' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/unpkg-min',
+			],
+			'directorist-openstreet-unpkg-index' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/unpkg-index',
+			],
+			'directorist-openstreet-unpkg-libs' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/unpkg-libs',
+			],
+			'directorist-openstreet-leaflet-versions' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/leaflet-versions',
+			],
+			'directorist-openstreet-leaflet-markercluster-versions' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/leaflet.markercluster-versions',
+			],
+			'directorist-openstreet-libs-setup' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/libs-setup',
+			],
+			'directorist-openstreet-open-layers' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/openlayers/OpenLayers',
+			],
+			'directorist-openstreet-crosshairs' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'openstreet-map/openlayers4jgsi/Crosshairs',
+			],
+			'directorist-openstreet-load-scripts' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'global-load-osm-map',
+			],
+			'directorist-google-map' => [
+				'type' => 'js',
+				'ext'  => $this->gmap_url(),
+			],
+			'directorist-markerclusterer' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'markerclusterer',
+			],
+			'directorist-select2' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'select2',
+			],
+			'directorist-sweetalert' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'sweetalert',
+			],
+			'directorist-tooltip' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'tooltip',
+			],
+			'directorist-popper' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'popper',
+			],
+			'directorist-range-slider' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'range-slider',
+			],
+			'directorist-ez-media-uploader' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'ez-media-uploader',
+			],
+
+			'directorist-jquery-barrating' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'jquery.barrating',
+			],
+			'directorist-plasma-slider' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'plasma-slider',
+			],
+			'directorist-uikit' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'uikit',
+			],
+			'directorist-validator' => [
+				'type' => 'js',
+				'path' => DIRECTORIST_VENDOR_JS. 'validator',
+			],
 		];
 
 		$this->scripts = apply_filters( 'directorist_register_scripts', $scripts );
@@ -194,7 +304,7 @@ class Asset_Loader {
 
 		foreach ( $this->scripts as $handle => $script ) {
 
-			$path = $this->script_file_path( $script );
+			$url = $this->script_file_url( $script );
 
 			if ( !empty( $script['dep'] ) ) {
 				$dep = $script['dep'];
@@ -204,10 +314,10 @@ class Asset_Loader {
 			}
 
 			if ( $script['type'] == 'css' ) {
-				wp_register_style( $handle, $path, [$dep], $this->version );
+				wp_register_style( $handle, $url, $dep, $this->version );
 			}
 			else {
-				wp_register_script( $handle, $path, $dep, $this->version );
+				wp_register_script( $handle, $url, $dep, $this->version );
 			}
 		}
 	}
@@ -224,3 +334,4 @@ class Asset_Loader {
 }
 
 Asset_Loader::instance();
+ray()->showWordPressErrors();

02a6d5603168312383d70818db319f9574282f18
Armanul46 <arman.sovware@gmail.com>
2022-02-07 12:32:34 +0600

    improved select file of search template

--- templates/search-form/custom-fields/select.php
+++ templates/search-form/custom-fields/select.php
@@ -2,32 +2,26 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.0.5.4
+ * @version 7.1.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+$field_id = uniqid( 'directorist-input-' );
 ?>
 
 <div class="directorist-search-field">
-
-	<?php if ( !empty($data['label']) ): ?>
-		<label><?php echo esc_html( $data['label'] ); ?></label>
+	<?php if ( ! empty( $data['label'] ) ) : ?>
+		<label for="<?php echo $field_id; ?>"><?php echo esc_html( $data['label'] ); ?></label>
 	<?php endif; ?>
 
 	<div class="directorist-select">
-
-		<select name='custom_field[<?php echo esc_attr( $data['field_key'] ); ?>]' <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true">
-
+		<select id="<?php echo $field_id; ?>" name="custom_field[<?php echo esc_attr( $data['field_key'] ); ?>]" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true">
 			<option value=""><?php esc_html_e( 'Select', 'directorist' )?></option>
 
-			<?php
-			foreach ( $data['options'] as $option ) {
-				printf('<option value="%s"%s>%s</option>', $option['option_value'], selected(  $value === $option[ 'option_value' ], true, false ), $option['option_label']);
-			}
-			?>
-
+			<?php foreach ( $data['options'] as $option ) : ?>
+				<option value="<?php echo esc_attr( $option['option_value'] ); ?>" <?php selected( $value, $option['option_value'] ); ?>><?php echo esc_html( $option['option_label'] ); ?></option>
+			<?php endforeach; ?>
 		</select>
-
 	</div>
-
 </div>

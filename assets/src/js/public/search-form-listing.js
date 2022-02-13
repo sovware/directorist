a49cb5d780d927a6fe9e4d2d226dc9e37e8d9f2e
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2022-02-13 12:08:15 +0600

    Category custom field issue in search home (#691)
    * fixed custom field issue on searching
    
    * added missing loop_get_tagline function
    
    * added show_popular_category parameter  on search form
    
    * added spacing
    
    * fixed translate issue on tab-profile
    
    * added sort by text changing setting
    
    * fixed setting naming issue
    
    * change default value of user type in registration form
    
    * fixed translate issue
    
    * fixed most used taxonomies issue with listing type in backend add listing form
    
    * fixed youtube issue on author page and removed extra language file
    
    * fixed columns validation issue on all categories & locations
    
    * fixed contact listing owner widget issue
    
    * fixed expiration date issue
    
    * removed map files
    
    * fixed translate issue
    
    * update pot file
    
    * fixed file upload issue
    
    * added templates section on system status
    
    * settings
    
    * fixed map info window issue of openstreet map
    
    * GET method replaced by REQUEST method
    
    * update npm
    
    * removed spaces
    
    * fixed directorist type slug issue
    
    * change login form settings hook name
    
    * change prefix name of login form settings section hook
    
    * removed extra div from search widget
    
    * fixed category custom field issue

--- assets/src/js/public/search-form-listing.js
+++ assets/src/js/public/search-form-listing.js
@@ -110,6 +110,42 @@
             });
         }
     });
+    
+    $('body').on('change', '.directorist-category-select', function(event) {
+        var listing_type = $('#listing_type').val();
+        var cat_id       = $(this).val();
+        var form_data = new FormData();
+        var custom_field = $('.directorist-category-select option[value='+cat_id+']').attr("data-custom-field");
+        /* if( ! custom_field ){
+          return;
+        } */
+        $('.directorist-search-form-box').addClass('atbdp-form-fade');
+        form_data.append('action', 'directorist_category_custom_field_search');
+        form_data.append('listing_type', listing_type);
+        form_data.append('cat_id', cat_id);
+        $.ajax({
+          method: 'POST',
+          processData: false,
+          contentType: false,
+          url: atbdp_search.ajax_url,
+          data: form_data,
+          success: function success(response) {
+            if (response) {
+              $('.directorist-search-form-box').empty().html(response['search_form']);
+            }
+             $('.directorist-category-select option[value='+cat_id+']').attr("selected",true);
+             $('.directorist-category-select option').attr("data-custom-field", 1);
+            var events = [new CustomEvent('directorist-search-form-nav-tab-reloaded'), new CustomEvent('directorist-reload-select2-fields'), new CustomEvent('directorist-reload-map-api-field')];
+              events.forEach(function (event) {
+                document.body.dispatchEvent(event);
+              });
+              $('.directorist-search-form-box').removeClass('atbdp-form-fade');
+          },
+          error: function error(_error) {
+            console.log(_error);
+          }
+        });
+      })
 
     // load custom fields of the selected category in the search form
     $('body').on('change', '.bdas-category-search, .directorist-category-select', function () {

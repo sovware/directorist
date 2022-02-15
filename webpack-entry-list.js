9b91f72857b89d9b2cedba2d337340ab6eb2d28b
Shamim Ahmed <shamimeforb@gmail.com>
2022-02-15 09:43:07 +0600

    Added all-lisitng.js file

--- webpack-entry-list.js
+++ webpack-entry-list.js
@@ -8,6 +8,10 @@ module.exports = {
         ['public-search-listing']: ["./assets/src/js/public/search-listing.js"],
         ['public-search-form-listing']: ["./assets/src/js/public/search-form-listing.js"],
 
+        //Pages
+        ['public-all-listings']: ["./assets/src/js/public/modules/all-listings.js"],
+        //['public-dashboard']: ["./assets/src/js/public/dashboard.js"],
+
         ['public-atmodal']: ["./assets/src/js/public/atmodal.js"],
         ['public-releated-listings-slider']: ["./assets/src/js/public/releated-listings-slider.js"],
 

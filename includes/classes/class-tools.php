16fc058874900d4865776bf5573f50314d5c4b15
Syed Galib <syedgalibahmed@gmail.com>
2022-02-28 13:06:35 +0600

    fixed listing import issue with preview image

--- includes/classes/class-tools.php
+++ includes/classes/class-tools.php
@@ -189,11 +189,12 @@
                     wp_set_object_terms( $post_id, (int)$directory_type, ATBDP_DIRECTORY_TYPE );
 
                     $preview_url = isset($post[$preview_image]) ? $post[$preview_image] : '';
-                    $preview_url = explode( ',', $preview_url );
+                    $preview_url = ( ! empty( $preview_url ) ) ? explode( ',', $preview_url ) : '';
 
-                    if ( $preview_url ) {
+                    if ( ! empty( $preview_url ) ) {
                         $attachment_ids = [];
                         foreach ( $preview_url as $_url_index => $_url ) {
+                            $_url = trim( $_url ); 
                             $attachment_id = self::atbdp_insert_attachment_from_url($_url, $post_id);
                             if ( 1 === $_url_index ) {
                                 update_post_meta($post_id, '_listing_prv_img', $attachment_id);

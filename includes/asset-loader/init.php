95ec7261cbc83133b2d00bbc43a49afa663c4b39
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-03-10 09:19:42 +0600

    Update

--- includes/asset-loader/init.php
+++ includes/asset-loader/init.php
@@ -202,13 +202,13 @@ class Init {
 	}
 
 	public function load_shortcode_scripts( $shortcode, $model = false ) {
-		wp_enqueue_script('jquery-masonry');
-		wp_enqueue_script( 'directorist-ez-media-uploader' );
-		wp_enqueue_script( 'directorist-popper' );
-		wp_enqueue_script( 'directorist-tooltip' );
-		wp_enqueue_script( 'directorist-no-script' );
-		wp_enqueue_script( 'directorist-global-script' );
-		wp_enqueue_script( 'directorist-atmodal' );
+		// wp_enqueue_script('jquery-masonry');
+		// wp_enqueue_script( 'directorist-ez-media-uploader' );
+		// wp_enqueue_script( 'directorist-popper' );
+		// wp_enqueue_script( 'directorist-tooltip' );
+		// wp_enqueue_script( 'directorist-no-script' );
+		// wp_enqueue_script( 'directorist-global-script' );
+		// wp_enqueue_script( 'directorist-atmodal' );
 	}
 
 	public function set_scripts() {

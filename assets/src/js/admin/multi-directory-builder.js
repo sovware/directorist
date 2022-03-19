2a09e66e728c30a568c239b6efec15ad0aaf6920
Shamim Ahmed <shamimeforb@gmail.com>
2022-03-19 12:45:47 +0600

    commit changes

--- assets/src/js/admin/multi-directory-builder.js
+++ assets/src/js/admin/multi-directory-builder.js
@@ -11,7 +11,6 @@ import cpt_manager_component from './vue/apps/cpt-manager/CPT_Manager.vue';
 
 window.addEventListener('DOMContentLoaded', () => {
     const cpt_manager_el = document.getElementById('atbdp-cpt-manager');
-
     if (cpt_manager_el) {
         const encodedBuilderData = cpt_manager_el.getAttribute('data-builder-data');
         let builderData = atob(encodedBuilderData);

469c3213025a3dd31f96bfd3f778fdd933430a9f
vairafiq <vairafiq@gmail.com>
2022-03-24 15:30:08 +0600

    improve

--- views/admin-templates/post-types-manager/edit-listing-type.php
+++ views/admin-templates/post-types-manager/edit-listing-type.php
@@ -6,7 +6,7 @@
         if ( $enable_multi_directory ) : ?>
             <?php
                 /**
-                 * Fires before all directory types table
+                 * Fires before single directory edited header
                  * @since 7.2.0
                  */
                 do_action( 'directorist_before_directory_type_edited' );

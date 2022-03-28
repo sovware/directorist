fdd88c6cf246adcf070078cb18fe57560094a693
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-03-28 15:24:31 +0600

    Update

--- directorist-base.php
+++ directorist-base.php
@@ -693,59 +693,6 @@ final class Directorist_Base
 
 	}
 
-	/**
-	 * It gets the related listings widget of the given listing/post
-	 * @param object|WP_Post $post The WP Post Object of whose related listing we would like to show
-	 * @return object|WP_Query It returns the related listings if found.
-	 */
-	public function get_related_listings_widget($post, $count)
-	{
-		$directory_type = get_the_terms( get_the_ID(), ATBDP_TYPE );
-		$type_id        = ! empty( $directory_type ) ? $directory_type[0]->term_id : '';
-		$same_author    = get_directorist_type_option( $type_id, 'listing_from_same_author', false );
-		$rel_listing_num = !empty($count) ? $count : 5;
-		$atbd_cats = get_the_terms($post, ATBDP_CATEGORY);
-		$atbd_tags = get_the_terms($post, ATBDP_TAGS);
-		// get the tag ids of the listing post type
-		$atbd_cats_ids = array();
-		$atbd_tags_ids = array();
-
-		if (!empty($atbd_cats)) {
-			foreach ($atbd_cats as $atbd_cat) {
-				$atbd_cats_ids[] = $atbd_cat->term_id;
-			}
-		}
-		if (!empty($atbd_tags)) {
-			foreach ($atbd_tags as $atbd_tag) {
-				$atbd_tags_ids[] = $atbd_tag->term_id;
-			}
-		}
-		$args = array(
-			'post_type' => ATBDP_POST_TYPE,
-			'tax_query' => array(
-				'relation' => 'OR',
-				array(
-					'taxonomy' => ATBDP_CATEGORY,
-					'field' => 'term_id',
-					'terms' => $atbd_cats_ids,
-				),
-				array(
-					'taxonomy' => ATBDP_TAGS,
-					'field' => 'term_id',
-					'terms' => $atbd_tags_ids,
-				),
-			),
-			'posts_per_page' => (int)$rel_listing_num,
-			'post__not_in' => array($post->ID),
-		);
-		if( ! empty( $same_author ) ){
-			$args['author']  = get_post_field( 'post_author', get_the_ID() );
-		}
-
-		return new WP_Query(apply_filters('atbdp_related_listing_args', $args));
-
-	}
-
 	public function add_custom_meta_keys_for_old_listings()
 	{
 		// get all the listings that does not have any of the following meta key missing

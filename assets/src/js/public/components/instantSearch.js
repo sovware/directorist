052a42121f2f33b6aca72f7c0ea820fef1e115ef
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2022-03-29 11:36:16 +0600

    fix location & category searching issue in search result ( instant search ) (#810)
    * fixed custom field issue on searching
    
    * added missing loop_get_tagline function
    
    * added show_popular_category parameter  on search form
    
    * added spacing
    
    * fixed translate issue on tab-profile
    
    * added sort by text changing setting
    
    * fixed setting naming issue
    
    * change default value of user type in registration form
    
    * fixed translate issue
    
    * fixed most used taxonomies issue with listing type in backend add listing form
    
    * fixed youtube issue on author page and removed extra language file
    
    * fixed columns validation issue on all categories & locations
    
    * fixed contact listing owner widget issue
    
    * fixed expiration date issue
    
    * removed map files
    
    * fixed translate issue
    
    * update pot file
    
    * fixed file upload issue
    
    * added templates section on system status
    
    * settings
    
    * fixed map info window issue of openstreet map
    
    * GET method replaced by REQUEST method
    
    * update npm
    
    * removed spaces
    
    * fixed directorist type slug issue
    
    * change login form settings hook name
    
    * change prefix name of login form settings section hook
    
    * removed extra div from search widget
    
    * fixed location and category searching issue on search result page
    
    * fixed directory type issue in search result page (instant search)

--- assets/src/js/public/components/instantSearch.js
+++ assets/src/js/public/components/instantSearch.js
@@ -68,6 +68,10 @@
         }
     }
 
+    function getURLParameter(url, name) {
+        return (RegExp(name + '=' + '(.+?)(&|$)').exec(url)||[,null])[1];
+    }
+
     /* Directorist instant search */
     $('body').on("submit", ".directorist-instant-search .directorist-advanced-filter__form", function( e ) {
         e.preventDefault();
@@ -112,6 +116,7 @@
         let view        = ( view_as && view_as.length ) ? view_as[0].replace( /view=/, '' ) : '';
         let type_href   = $('.directorist-type-nav__list .current a').attr('href');
         let type        = ( type_href && type_href.length ) ? type_href.match( /directory_type=.+/ ) : '';
+        let directory_type = getURLParameter( type_href, 'directory_type' );
 
         var data = {
             action  : 'directorist_instant_search',
@@ -123,8 +128,8 @@
         
         var fields = {
             q       : $('input[name="q"]').val(),
-            in_cat  : $('.bdas-category-search').val(),
-            in_loc  : $('.bdas-category-location').val(),
+            in_cat  : $('.bdas-category-search, .directorist-category-select').val(),
+            in_loc  : $('.bdas-category-location, .directorist-location-select').val(),
             price_range : $("input[name='price_range']:checked").val(),
             search_by_rating: $('select[name=search_by_rating]').val(),
             address : $('input[name="address"]').val(),
@@ -157,8 +162,8 @@
                 form_data.view = view
             }
 
-            if( type && type.length ) {
-                form_data.directory_type = type[0].replace( /directory_type=/, '' )
+            if( directory_type && directory_type.length ) {
+                form_data.directory_type = directory_type;
             }
 
             update_instant_search_url( form_data );
@@ -194,10 +199,12 @@
         e.preventDefault();
         let type_href   = $(this).attr('href');
         let type        = type_href.match( /directory_type=.+/ );
+        //let directory_type = ( type && type.length ) ? type[0].replace( /directory_type=/, '' ) : '';
+        let directory_type = getURLParameter( type_href, 'directory_type' );
         var form_data = {
             action  : 'directorist_instant_search',
             _nonce  : atbdp_public_data.ajax_nonce,
-            directory_type    : ( type && type.length ) ? type[0].replace( /directory_type=/, '' ) : '',
+            directory_type    : directory_type,
         };
 
         update_instant_search_url( form_data );
@@ -280,6 +287,7 @@
         let view        = view_href.match( /view=.+/ );
         let type_href   = $('.directorist-type-nav__list .current a').attr('href');
         let type        = ( type_href && type_href.length ) ? type_href.match( /directory_type=.+/ ) : '';
+        let directory_type = getURLParameter( type_href, 'directory_type' );
         let page_no     = $(".page-numbers.current").text();
 
         $(".directorist-viewas-dropdown .directorist-dropdown__links--single").removeClass('active');
@@ -289,8 +297,8 @@
             _nonce  : atbdp_public_data.ajax_nonce,
             view    : ( view && view.length ) ? view[0].replace( /view=/, '' ) : '',
             q       : $('input[name="q"]').val(),
-            in_cat  : $('.bdas-category-search').val(),
-            in_loc  : $('.bdas-category-location').val(),
+            in_cat  : $('.bdas-category-search, .directorist-category-select').val(),
+            in_loc  : $('.bdas-category-location, .directorist-location-select').val(),
             in_tag  : tag,
             price   : price,
             price_range : $("input[name='price_range']:checked").val(),
@@ -311,8 +319,8 @@
             form_data.paged = page_no;
         }
 
-        if( type && type.length ) {
-            form_data.directory_type = type[0].replace( /directory_type=/, '' )
+        if( directory_type && directory_type.length ) {
+            form_data.directory_type = directory_type;
         }
 
         if( sort && sort.length ) {
@@ -393,6 +401,7 @@
         let sort_by     = sort_href.match( /sort=.+/ );
         let type_href   = $('.directorist-type-nav__list .current a').attr('href');
         let type        = ( type_href && type_href.length ) ? type_href.match( /directory_type=.+/ ) : '';
+        let directory_type = getURLParameter( type_href, 'directory_type' );
 
         $(".directorist-sortby-dropdown .directorist-dropdown__links--single").removeClass('active');
         $(this).addClass("active");
@@ -402,8 +411,8 @@
             _nonce  : atbdp_public_data.ajax_nonce,
             sort    : ( sort_by && sort_by.length ) ? sort_by[0].replace( /sort=/, '' ) : '',
             q       : $('input[name="q"]').val(),
-            in_cat  : $('.bdas-category-search').val(),
-            in_loc  : $('.bdas-category-location').val(),
+            in_cat  : $('.bdas-category-search, .directorist-category-select').val(),
+            in_loc  : $('.bdas-category-location, .directorist-location-select').val(),
             in_tag  : tag,
             price   : price,
             price_range : $("input[name='price_range']:checked").val(),
@@ -421,8 +430,8 @@
             view : view,
         };
 
-        if( type && type.length ) {
-            form_data.directory_type = type[0].replace( /directory_type=/, '' )
+        if( directory_type && directory_type.length ) {
+            form_data.directory_type = directory_type;
         }
 
         $.ajax({
@@ -498,6 +507,7 @@
         let view        = ( view_as && view_as.length ) ? view_as[0].replace( /view=/, '' ) : '';
         let type_href   = $('.directorist-type-nav__list .current a').attr('href');
         let type        = ( type_href && type_href.length ) ? type_href.match( /directory_type=.+/ ) : '';
+        let directory_type = getURLParameter( type_href, 'directory_type' );
         
         $(".directorist-pagination .page-numbers").removeClass('current');
         $(this).addClass("current");
@@ -515,8 +525,8 @@
             _nonce  : atbdp_public_data.ajax_nonce,
             view    : ( view && view.length ) ? view[0].replace( /view=/, '' ) : '',
             q       : $('input[name="q"]').val(),
-            in_cat  : $('.bdas-category-search').val(),
-            in_loc  : $('.bdas-category-location').val(),
+            in_cat  : $('.bdas-category-search, .directorist-category-select').val(),
+            in_loc  : $('.bdas-category-location, .directorist-location-select').val(),
             in_tag  : tag,
             price   : price,
             price_range : $("input[name='price_range']:checked").val(),
@@ -537,8 +547,8 @@
 
         update_instant_search_url( form_data );
 
-        if( type && type.length ) {
-            form_data.directory_type = type[0].replace( /directory_type=/, '' )
+        if( directory_type && directory_type.length ) {
+            form_data.directory_type = directory_type;
         }
 
         if( sort && sort.length ) {

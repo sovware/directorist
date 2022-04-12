18ed32cd668c436d47bfc7df6ece0b717aef5076
Syed Galib <syedgalibahmed@gmail.com>
2022-04-12 11:12:47 +0600

    select2 category field is not working when search widget used in sidebar

--- templates/search-form/fields/category.php
+++ templates/search-form/fields/category.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 <div class="directorist-search-field">
 	<div class="directorist-select directorist-search-category">
-		<select name="in_cat" id="<?php echo !empty($searchform->category_id) ? esc_attr($searchform->category_id) : "notEmptyId".uniqid(); ?>" class="<?php echo esc_attr($searchform->category_class); ?>" data-placeholder="<?php echo esc_attr($data['placeholder']); ?>" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true" data-selected-id="<?php echo $selected_item['id'] ?>" data-selected-label="<?php echo $selected_item['label'] ?>">
+		<select name="in_cat" class="<?php echo esc_attr($searchform->category_class); ?>" data-placeholder="<?php echo esc_attr($data['placeholder']); ?>" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true" data-selected-id="<?php echo $selected_item['id'] ?>" data-selected-label="<?php echo $selected_item['label'] ?>">
 			<?php
 				echo '<option value="">' . __( 'Select Category', 'directorist' ) . '</option>';
 				echo $searchform->categories_fields;

02944cfc2618ecdc8d19f532725f18743ad9b2fc
Syed Galib <syedgalibahmed@gmail.com>
2022-04-13 11:02:38 +0600

    improved

--- views/admin-templates/import-export/header-templates/nav-item.php
+++ views/admin-templates/import-export/header-templates/nav-item.php
@@ -1,6 +1,6 @@
 <?php defined( 'ABSPATH' ) || exit; ?>
 
-<li class="<?php echo esc_html( $args['nav_item_class'] ); ?>">
+<li class="<?php echo esc_attr( $args['nav_item_class'] ); ?>">
     <span class="step">
         <span class="step-count"><?php echo esc_html( $args['step_count'] ); ?></span> 
         <span class="dashicons dashicons-yes"></span>

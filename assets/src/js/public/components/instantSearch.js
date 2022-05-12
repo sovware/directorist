4132c45e60a7fe41d45b16e94f1566cf27da3bab
Shamim Ahmed <shamimeforb@gmail.com>
2022-05-12 13:11:37 +0600

    commit changes

--- assets/src/js/public/components/instantSearch.js
+++ assets/src/js/public/components/instantSearch.js
@@ -309,7 +309,6 @@
                         $(document).scrollTop($(".directorist-archive-contents").offset().top);
                     },
                     success: function (html) {
-						console.log(html.search_result);
                         if (html.search_result) {
                             $('.directorist-archive-contents').find('.directorist-header-found-title span').text(html.count);
                             $('.directorist-archive-contents').find('.directorist-archive-items').replaceWith(html.search_result);

395e35a26b117b609c372860a6b9202a76d8e589
devShamim <shamimeforb@gmail.com>
2022-07-25 15:47:36 +0600

    Fixed single listing widget map loading issue

--- assets/js/single-listing-openstreet-map.js
+++ assets/js/single-listing-openstreet-map.js
@@ -97,7 +97,7 @@
 (function ($) {
   jQuery(document).ready(function () {
     // Localized Data
-    if ($('.directorist-details-info-wrap .directorist-single-map').length) {
+    if ($('.directorist-single-contents-area .directorist-single-map').length) {
       document.querySelectorAll('.directorist-single-map').forEach(function (mapElm) {
         var mapData = JSON.parse(mapElm.getAttribute('data-map'));
         var loc_default_latitude = parseFloat(mapData.default_latitude);

b6a80aebaa015eeafb45c6b9b887454d0cce3948
devShamim <shamimeforb@gmail.com>
2022-07-26 12:34:33 +0600

    Fixed single listing widget map loading issue

--- assets/js/single-listing-google-map.js
+++ assets/js/single-listing-google-map.js
@@ -98,7 +98,7 @@ window.addEventListener('DOMContentLoaded', function () {
   ;
 
   (function ($) {
-    if ($('.directorist-details-info-wrap .directorist-single-map').length) {
+    if ($('.directorist-single-map').length) {
       document.querySelectorAll('.directorist-single-map').forEach(function (mapElm) {
         var MAP_PIN = 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z';
 

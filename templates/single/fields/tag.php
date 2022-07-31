03d8b3c8e52a88e65ff7fdbd963ad9124b3ce49e
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-07-31 10:42:11 +0600

    Update

--- templates/single/fields/tag.php
+++ templates/single/fields/tag.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.7
- * @version 7.0.5.2
+ * @version 7.3.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -17,9 +17,9 @@ if ( empty( $listing->get_tags() ) ) {
 	<?php foreach ( $listing->get_tags() as $tag ): ?>
 
 		<li>
-			<a href="<?php echo esc_url( get_term_link( $tag->term_id, ATBDP_TAGS ) ); ?>"><span class="<?php echo apply_filters('atbdp_single_listing_tags_icon', atbdp_icon_type().'-tag'); ?>"></span> <?php echo esc_html( $tag->name ); ?></a>
+			<a href="<?php echo esc_url( get_term_link( $tag->term_id, ATBDP_TAGS ) ); ?>"><span class="<?php echo esc_attr( apply_filters('atbdp_single_listing_tags_icon', atbdp_icon_type().'-tag') ); ?>"></span> <?php echo esc_html( $tag->name ); ?></a>
 		</li>
-	
+
 	<?php endforeach; ?>
 
 </ul>

4ec8b663e28e6f89e8c4f68efd9d61cb4abd7c7d
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-08-02 15:46:44 +0600

    Improved

--- templates/listing-form/fields/location.php
+++ templates/listing-form/fields/location.php
@@ -5,12 +5,10 @@
  * @version 7.3.1
  */
 
-$placeholder       = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
-$multiple          = $data['type'] == 'multiple' ? 'multiple="multiple"' : '';
-$max               = !empty( $data['max_location_creation'] ) ? 'data-max="'. $data['max_location_creation'] .'"' : '';
-$create_new        = !empty( $data['create_new_loc'] ) ? ' data-allow_new="'. $data['create_new_loc'] .'"' : '';
-$all_terms         = $listing_form->add_listing_all_terms( ATBDP_LOCATION );
-$current_term_ids  = $listing_form->add_listing_term_ids( ATBDP_LOCATION );
+$placeholder = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
+$multiple    = $data['type'] == 'multiple' ? 'multiple="multiple"' : '';
+$max         = !empty( $data['max_location_creation'] ) ? 'data-max="'. $data['max_location_creation'] .'"' : '';
+$create_new  = !empty( $data['create_new_loc'] ) ? ' data-allow_new="'. $data['create_new_loc'] .'"' : '';
 ?>
 
 <div class="directorist-form-group directorist-form-location-field">
@@ -23,11 +21,7 @@ $current_term_ids  = $listing_form->add_listing_term_ids( ATBDP_LOCATION );
 		if ($data['type'] != 'multiple') {
 			echo '<option value="">' . esc_attr( $placeholder ) . '</option>';
 		}
-
-		foreach ( $all_terms as $term ) {
-			$selected = in_array( $term->term_id, $current_term_ids ) ? "selected" : '';
-			printf( '<option value="%s" %s>%s</option>', esc_attr( $term->term_id ), esc_attr( $selected ), esc_html( $term->name ) );
-		}
+		echo directorist_kses( $listing_form->add_listing_location_fields(), 'form_input' );
 		?>
 
 	</select>

c0a24768a7b550728b02bddd58aed63aea1a65fa
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-08-02 12:11:58 +0600

    Improved

--- includes/model/Listings.php
+++ includes/model/Listings.php
@@ -1109,26 +1109,6 @@ class Directorist_Listings {
 		endif;
 	}
 
-	/**
-	 * @deprecated 7.3.1
-	 */
-	public function search_category_location_args() {
-		return array(
-			'parent'             => 0,
-			'term_id'            => 0,
-			'hide_empty'         => 0,
-			'orderby'            => 'name',
-			'order'              => 'asc',
-			'show_count'         => 0,
-			'single_only'        => 0,
-			'pad_counts'         => true,
-			'immediate_category' => 0,
-			'active_term_id'     => 0,
-			'ancestors'          => array(),
-			'listing_type'		 => $this->listing_types
-		);
-	}
-
 	public function get_view_as_link_list() {
 		$link_list = array();
 		$view      = ! empty( $this->view ) ? $this->view : '';
@@ -1225,6 +1205,26 @@ class Directorist_Listings {
 			$current = $term->term_id;
 		}
 		return (int) $current;
+	}
+
+	/**
+	 * @deprecated 7.3.1
+	 */
+	public function search_category_location_args() {
+		return array(
+			'parent'             => 0,
+			'term_id'            => 0,
+			'hide_empty'         => 0,
+			'orderby'            => 'name',
+			'order'              => 'asc',
+			'show_count'         => 0,
+			'single_only'        => 0,
+			'pad_counts'         => true,
+			'immediate_category' => 0,
+			'active_term_id'     => 0,
+			'ancestors'          => array(),
+			'listing_type'		 => $this->listing_types
+		);
 	}
 
 	public function render_map() {

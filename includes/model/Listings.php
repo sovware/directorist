eba84b15341ff491f0e3276ee72dddca61f23c1e
Mahdi <virtualmahdi@gmail.com>
2022-08-03 15:53:38 +0600

    add filter to manipulate category icon

--- includes/model/Listings.php
+++ includes/model/Listings.php
@@ -1318,8 +1318,17 @@ class Directorist_Listings {
 		}
 
 		$cat_icon = !empty( $cat_icon ) ? $cat_icon : atbdp_icon_type() . '-map-marker';
-
-		return $cat_icon;
+		/**
+		 * Get category icon for map marker. 
+		 *
+		 * @since 7.3.1
+		 * 
+		 * @param array $cats Categories for the post.
+		 * @param string $cat_icon First category
+		 * 
+		 * @return string CATEGORY ICON NAME
+		 */
+		return apply_filters( "directorist_listings_map_view_marker_icon", $cat_icon, $cats );
 	}
 
 	public function openstreet_map_card_data() {

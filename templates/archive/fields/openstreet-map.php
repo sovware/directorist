77cbe09b46c629904c0ecaf758d8e3bd94b5cc1f
Mahdi <virtualmahdi@gmail.com>
2022-08-10 11:41:17 +0600

    bump template version

--- templates/archive/fields/openstreet-map.php
+++ templates/archive/fields/openstreet-map.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.3.1
+ * @version 7.3.1.2
  */
 ?>
 <div class='atbdp-body atbdp-map embed-responsive embed-responsive-16by9 atbdp-margin-bottom'>

818836e7e882b2be39175f18f6fa2cfbdc7143ec
vairafiq <vairafiq@gmail.com>
2022-08-23 12:03:09 +0600

    multiple attr is not printing

--- templates/listing-form/fields/category.php
+++ templates/listing-form/fields/category.php
@@ -2,21 +2,26 @@
 /**
  * @author  wpWax
  * @since   6.7
- * @version 7.3.1
+ * @version 7.3.3
  */
 
 $placeholder = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
-$multiple    = $data['type'] == 'multiple' ? 'multiple="multiple"' : '';
-$max         = !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'"' : '';
-$create_new  = !empty( $data['create_new_cat'] ) ? ' data-allow_new="'. $data['create_new_cat'] .'"' : '';
+$multiple    = $data['type'] == 'multiple' ? 'multiple="multiple" ' : '';
+$max         = !empty( $data['max'] ) ? 'data-max="'. $data['max'] .'" ' : '';
+$create_new  = !empty( $data['create_new_cat'] ) ? ' data-allow_new="'. $data['create_new_cat'] .'" ' : '';
 ?>
 
 <div class="directorist-form-group directorist-form-categories-field">
 
 	<?php $listing_form->field_label_template( $data );?>
 
-	<select name="admin_category_select[]" id="at_biz_dir-categories" class="directorist-form-element" data-placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo esc_attr( $multiple ) ; echo esc_attr( $max ); echo esc_attr( $create_new ); ?> <?php $listing_form->required( $data ); ?>>
-
+	<select name="admin_category_select[]" id="at_biz_dir-categories" class="directorist-form-element" data-placeholder="<?php echo esc_attr( $placeholder ); ?>" 
+	<?php echo $data['type'] == 'multiple' ? esc_attr( 'multiple' ) : ''; ?>
+	data-max="<?php echo ! empty( $data['max'] ) ? esc_attr( $data['max'] ) : ''; ?>"
+	data-allow_new="<?php echo ! empty( $data['create_new_cat'] ) ? esc_attr( $data['create_new_cat'] ) : ''; ?>"
+	<?php 	
+	$listing_form->required( $data ); 
+	?>>
 		<?php
 		if ($data['type'] != 'multiple') {
 			echo '<option value="">' . esc_attr( $placeholder ) . '</option>';

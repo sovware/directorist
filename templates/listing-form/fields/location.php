6a118b0d4416a63d686ad881277937c8d98cc039
vairafiq <vairafiq@gmail.com>
2022-08-23 12:16:13 +0600

    improved

--- templates/listing-form/fields/location.php
+++ templates/listing-form/fields/location.php
@@ -12,10 +12,7 @@ $placeholder = ! empty( $data['placeholder'] ) ? $data['placeholder'] : '';
 
 	<?php $listing_form->field_label_template( $data ); ?>
 
-	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-location" data-placeholder="<?php echo esc_attr( $placeholder ); ?>"
-	<?php echo $data['type'] == 'multiple' ? esc_attr( 'multiple' ) : ''; ?>
-	data-max="<?php echo ! empty( $data['max_location_creation'] ) ? esc_attr( $data['max_location_creation'] ) : ''; ?>"
-	data-allow_new="<?php echo ! empty( $data['create_new_loc'] ) ? esc_attr( $data['create_new_loc'] ) : ''; ?>"
+	<select name="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" id="at_biz_dir-location" data-placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo $data['type'] == 'multiple' ? esc_attr( 'multiple' ) : ''; ?> data-max="<?php echo ! empty( $data['max_location_creation'] ) ? esc_attr( $data['max_location_creation'] ) : ''; ?>" data-allow_new="<?php echo ! empty( $data['create_new_loc'] ) ? esc_attr( $data['create_new_loc'] ) : ''; ?>"
 	<?php 	
 	$listing_form->required( $data ); 
 	?>>

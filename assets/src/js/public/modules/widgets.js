862b51f416ebbc2d7b78529b2d08abe4ceb93d48
devShamim <shamimeforb@gmail.com>
2022-09-04 07:42:34 +0600

    fix icon style mismatch in widgets

--- assets/src/js/public/modules/widgets.js
+++ assets/src/js/public/modules/widgets.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {
     (function ($) {
         /* Category/Location expand */
         $('.atbdp_child_category').hide();
-        $('.atbdp-widget-categories .atbdp_parent_category >li >span').on('click', function () {
+        $('.atbdp-widget-categories .atbdp_parent_category >li .directorist-icon-mask').on('click', function () {
             $(this).siblings('.atbdp_child_category').slideToggle();
         });
         $('.atbdp_child_location').hide();
-        $('.atbdp-widget-categories .atbdp_parent_location >li >span').on('click', function () {
+        $('.atbdp-widget-categories .atbdp_parent_location >li .directorist-icon-mask').on('click', function () {
             $(this).siblings('.atbdp_child_location').slideToggle();
         });
 

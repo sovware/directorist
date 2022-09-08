e1c6ed1bfa13778f264cf7702991efbcede35a89
Mahdi <virtualmahdi@gmail.com>
2022-09-08 15:15:31 +0600

    fix misplaced attributes on textarea

--- templates/account/registration.php
+++ templates/account/registration.php
@@ -105,9 +105,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 								echo esc_html( $bio );
 								echo ( ! empty( $require_bio ) ? '<strong class="directorist-form-required">*</strong>' : '' );
 							?></label>
-							<textarea id="bio" class="directorist-form-element" name="bio" rows="10"><?php echo isset( $_REQUEST['bio']) ? esc_textarea( sanitize_text_field( wp_unslash( $_REQUEST['bio'] ) ) ) : ''; ?> <?php
+							<textarea id="bio" class="directorist-form-element" name="bio" rows="10" <?php
 									echo ( ! empty( $require_bio ) ? 'required' : '' );
-								?></textarea>
+								?>><?php echo isset( $_REQUEST['bio']) ? esc_textarea( sanitize_text_field( wp_unslash( $_REQUEST['bio'] ) ) ) : ''; ?></textarea>
 						</div>
 						<?php } ?>
 						<?php if ( ! empty( get_directorist_option( 'display_user_type' ) ) ) {

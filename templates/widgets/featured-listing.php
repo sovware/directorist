6070a47a98121c517fd896f6fdd9b7d0be5fee08
Mahdi <virtualmahdi@gmail.com>
2022-09-18 14:24:19 +0600

    fixed broken layout for featured  listing category

--- templates/widgets/featured-listing.php
+++ templates/widgets/featured-listing.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * @author  wpWax
  * @since   7.3.0
  * @version 7.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 $f_listing_num = !empty($instance['f_listing_num']) ? $instance['f_listing_num'] : 5;
 
 $featured_args = array(
@@ -27,9 +28,10 @@ $featured_args = array(
  *
  * @param array $featured_args  Featured Arguments.
  */
-apply_filters( "directorist_widget_featured_listings_query_arguments", $featured_args );
+apply_filters("directorist_widget_featured_listings_query_arguments", $featured_args);
 
 $featured_listings = new WP_Query($featured_args);
+$default_icon = 'las la-folder-open';
 ?>
 <div class="atbd_categorized_listings">
     <ul class="listings">
@@ -44,17 +46,17 @@ $featured_listings = new WP_Query($featured_args);
                 $price_range = get_post_meta(get_the_ID(), '_price_range', true);
                 $listing_pricing = get_post_meta(get_the_ID(), '_atbd_listing_pricing', true);
                 $cats = get_the_terms(get_the_ID(), ATBDP_CATEGORY);
-                ?>
+        ?>
                 <li>
                     <div class="atbd_left_img">
                         <?php
                         $default_image = get_directorist_option('default_preview_image', DIRECTORIST_ASSETS . 'images/grid.jpg');
                         if (!empty($listing_prv_img)) {
-                            echo '<a href="'.esc_url( get_the_permalink() ).'"><img src="' . esc_url(wp_get_attachment_image_url($listing_prv_img, array(90, 90))) . '" alt="listing image"></a>';
+                            echo '<a href="' . esc_url(get_the_permalink()) . '"><img src="' . esc_url(wp_get_attachment_image_url($listing_prv_img, array(90, 90))) . '" alt="listing image"></a>';
                         } elseif (!empty($listing_img[0]) && empty($listing_prv_img)) {
-                            echo '<a href="'.esc_url( get_the_permalink() ).'"><img src="' . esc_url(wp_get_attachment_image_url($listing_img[0], array(90, 90))) . '" alt="listing image"></a>';
+                            echo '<a href="' . esc_url(get_the_permalink()) . '"><img src="' . esc_url(wp_get_attachment_image_url($listing_img[0], array(90, 90))) . '" alt="listing image"></a>';
                         } else {
-                            echo '<a href="'.esc_url( get_the_permalink() ).'"><img src="' . esc_url( $default_image ) . '" alt="listing image"></a>';
+                            echo '<a href="' . esc_url(get_the_permalink()) . '"><img src="' . esc_url($default_image) . '" alt="listing image"></a>';
                         }
 
                         ?>
@@ -69,46 +71,56 @@ $featured_listings = new WP_Query($featured_args);
 
                             <?php } else {
                                 $output = atbdp_display_price_range($price_range);
-                                echo wp_kses_post( $output );
+                                echo wp_kses_post($output);
                             } ?>
                         </div>
 
-                        <?php if (!empty($cats)) {
-                            $totalTerm = count($cats);
+                        <div class="directorist-listing-category">
+                            <?php if (!empty($cats)) {
+                                $term_icon  = get_term_meta($cats[0]->term_id, 'category_icon', true);
+                                $term_icon  = $term_icon ? $term_icon : $default_icon;
+                                $term_link  = esc_url(get_term_link($cats[0]->term_id, ATBDP_CATEGORY));
+                                $term_label = $cats[0]->name;
                             ?>
-
-                            <p class="directory_tag">
-							<?php directorist_icon( 'las la-tags' ); ?>
-                                <span>
-                                            <a href="<?php echo esc_url( ATBDP_Permalink::atbdp_get_category_page($cats[0]) ); ?>">
-                                                                    <?php echo esc_html($cats[0]->name); ?>
-                                            </a>
-                                        <?php
-                                        if ($totalTerm > 1) {
+                                <a href="<?php echo esc_url($term_link); ?>"><?php directorist_icon($term_icon); ?><?php echo esc_html($term_label); ?></a>
+                                <?php
+                                $totalTerm = count($cats);
+                                if ($totalTerm > 1) {
+                                    $totalTerm = $totalTerm - 1; ?>
+                                    <div class="directorist-listing-category__popup">
+                                        <span class="directorist-listing-category__extran-count">+<?php echo esc_html($totalTerm); ?></span>
+                                        <div class="directorist-listing-category__popup__content">
+                                            <?php
+                                            foreach (array_slice($cats, 1) as $cat) {
+                                                $term_icon  = get_term_meta($cat->term_id, 'category_icon', true);
+                                                $term_icon  = $term_icon ? $term_icon : $default_icon;
+                                                $term_link  = esc_url(ATBDP_Permalink::atbdp_get_category_page($cat));
+                                                $term_link  = esc_url(get_term_link($cat->term_id, ATBDP_CATEGORY));
+                                                $term_label = $cat->name;
                                             ?>
-                                            <span class="atbd_cat_popup">  +<?php echo esc_html( $totalTerm - 1 ); ?>
-                                                <span class="atbd_cat_popup_wrapper">
-                                                                <?php
-                                                                $output = array();
-                                                                foreach (array_slice($cats, 1) as $cat) {
-                                                                    $link = ATBDP_Permalink::atbdp_get_category_page($cat);
-                                                                    $space = str_repeat(' ', 1);
-                                                                    $output [] = "{$space}<a href='{$link}'>{$cat->name}<span>,</span></a>";
-                                                                } ?>
-                                                    <span><?php echo wp_kses_post( join($output) ); ?></span>
-                                                            </span>
-                                                        </span>
-                                        <?php } ?>
 
-                                    </span>
-                            </p>
-                        <?php } ?>
+                                                <a href="<?php echo esc_url($term_link); ?>"><?php directorist_icon($term_icon); ?> <?php echo esc_html($term_label); ?></a>
+
+                                            <?php
+                                            }
+                                            ?>
+                                        </div>
+
+                                    </div>
+                                <?php
+                                }
+                            } else { ?>
+                                <a href="#"><?php directorist_icon($default_icon); ?><?php esc_html_e('Uncategorized', 'directorist'); ?></a>
+                            <?php
+                            }
+                            ?>
+                        </div>
                     </div>
                 </li>
-                <?php
+        <?php
             }
             wp_reset_postdata();
         }; ?>
     </ul>
-</div> <!--ends featured listing-->
-
+</div>
+<!--ends featured listing-->

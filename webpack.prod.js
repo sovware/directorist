508926e1e58b53ba67f30951c9f10990f4a4fb9f
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-09-26 08:19:11 +0600

    Update

--- webpack.prod.js
+++ webpack.prod.js
@@ -40,7 +40,6 @@ const prodConfig = {
               { source: './views', destination: './__build/directorist/directorist/views' },
               { source: './*.php', destination: './__build/directorist/directorist' },
               { source: './*.txt', destination: './__build/directorist/directorist' },
-              { source: './phpcs.xml.dist', destination: './__build/directorist/directorist/phpcs.xml.dist' },
             ],
           },
           {

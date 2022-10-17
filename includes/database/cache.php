adb3e29e2098099ddc691a25e39c13ee6ad6561a
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-10-17 11:55:56 +0600

    Update

--- includes/database/cache.php
+++ includes/database/cache.php
@@ -15,7 +15,6 @@ class Cache {
 
 	public static $transient_prefix = 'directorist_';
 	public static $cache_group      = 'directorist';
-	public static $type             = 'object'; // object, transient
 	public static $expire           = 43000;
 
 	public static function cache_enabled() {
@@ -26,31 +25,34 @@ class Cache {
 		return wp_using_ext_object_cache();
 	}
 
-	public static function set( $key, $value, $expire = '', $group = '', $type = '' ) {
-		$type   = in_array( $type, array( 'object', 'transient' ) ) ? $type : self::$type;
+	public static function set_object_cache( $key, $value, $expire = '', $group = '' ) {
 		$expire = $expire ? $expire : self::$expire;
 		$group  = $group ? $group : self::$cache_group;
-
-		if ( $type === 'object' ) {
-			return wp_cache_set( $key, $value, $group, $expire );
-		} else {
-			$transient_key = self::$transient_prefix . $key;
-			return set_transient( $transient_key, $value, $expire );
-		}
+		return wp_cache_set( $key, $value, $group, $expire );
 	}
 
-	public static function get( $key, $group = '', $type = '' ) {
-		$type   = in_array( $type, array( 'object', 'transient' ) ) ? $type : self::$type;
+	public static function get_object_cache( $key, $group = '' ) {
 		$group  = $group ? $group : self::$cache_group;
-
-		if ( $type === 'object' ) {
-			return wp_cache_get( $key, $group );
-		} else {
-			$transient_key = self::$transient_prefix . $key;
-			return get_transient( $transient_key );
-		}
+		return wp_cache_get( $key, $group );
 	}
 
+	public static function delete_object_cache( $key, $group = '' ) {
+		$group  = $group ? $group : self::$cache_group;
+		return wp_cache_delete( $key, $group );
+	}
 
+	public static function set_transient( $key, $value, $expire = '' ) {
+		$transient_key = self::$transient_prefix . $key;
+		return set_transient( $transient_key, $value, $expire );
+	}
 
+	public static function get_transient( $key ) {
+		$transient_key = self::$transient_prefix . $key;
+		return get_transient( $transient_key );
+	}
+
+	public static function delete_transient( $key ) {
+		$transient_key = self::$transient_prefix . $key;
+		return delete_transient( $transient_key );
+	}
 }

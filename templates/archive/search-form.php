b1d01a6df6c1d5311a0e2d6050b353f4879e4c75
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-12-25 09:19:43 +0600

    Update

--- templates/archive/search-form.php
+++ templates/archive/search-form.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.3.1
+ * @version 7.5.0
  */
 ?>
 
@@ -23,10 +23,17 @@ if (is_numeric($searchform->listing_type)) {
 		</div>
 
 		<div class="directorist-advanced-filter__advanced">
+
+			<div class="directorist-search-form-cat-fields" data-inner-class="directorist-form-group directorist-advanced-filter__advanced--element direcorist-search-field-WIDGETNAME"></div>
+
 			<?php foreach ($searchform->form_data[1]['fields'] as $field) : ?>
+
 				<div class="directorist-form-group directorist-advanced-filter__advanced--element direcorist-search-field-<?php echo esc_attr($field['widget_name']) ?>"><?php $searchform->field_template($field); ?></div>
+
 			<?php endforeach; ?>
+
 		</div>
+
 		<?php $searchform->buttons_template(); ?>
 	</form>
 </div>
--- templates/search-form/adv-search.php
+++ templates/search-form/adv-search.php
@@ -2,13 +2,15 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.3.0
+ * @version 7.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
 <div class="directorist-search-adv-filter directorist-advanced-filter">
+
+	<div class="directorist-search-form-cat-fields" data-inner-class="directorist-form-group directorist-advanced-filter__advanced--element direcorist-search-field-WIDGETNAME"></div>
 
 	<?php foreach ( $searchform->form_data[1]['fields'] as $field ): ?>
 

1efe882a1d3fdd97d3a5cb1c6887c4221d1adc87
vairafiq <vairafiq@gmail.com>
2023-01-02 10:20:55 +0600

    fix conflict and sync with alpha

--- assets/js/search-form.js
+++ assets/js/search-form.js
@@ -1418,13 +1418,13 @@ __webpack_require__.r(__webpack_exports__);
             var directory_type   = $container.find('.listing_type').val();
             var $search_form_box = $container.find('.directorist-search-form-box-wrap');
             var form_data        = new FormData();
-              form_data.append('action', 'directorist_category_custom_field_search');
+             form_data.append('action', 'directorist_category_custom_field_search');
             form_data.append('nonce', directorist.directorist_nonce);
             form_data.append('listing_type', directory_type);
             form_data.append('cat_id', cat_id);
             form_data.append('atts', JSON.stringify($container.data('atts')));
-              $search_form_box.addClass('atbdp-form-fade');
-              $.ajax({
+             $search_form_box.addClass('atbdp-form-fade');
+             $.ajax({
               method     : 'POST',
               processData: false,
               contentType: false,
@@ -1433,9 +1433,9 @@ __webpack_require__.r(__webpack_exports__);
               success: function success(response) {
                 if (response) {
                   $search_form_box.html(response['search_form']);
-                    $container.find('.directorist-category-select option').data('custom-field', 1);
+                   $container.find('.directorist-category-select option').data('custom-field', 1);
                   $container.find('.directorist-category-select').val(cat_id);
-                    [
+                   [
                     new CustomEvent('directorist-search-form-nav-tab-reloaded'),
                     new CustomEvent('directorist-reload-select2-fields'),
                     new CustomEvent('directorist-reload-map-api-field'),
@@ -1445,7 +1445,7 @@ __webpack_require__.r(__webpack_exports__);
                     window.dispatchEvent(event);
                   });
                 }
-                  $search_form_box.removeClass('atbdp-form-fade');
+                 $search_form_box.removeClass('atbdp-form-fade');
               },
               error: function error(_error) {
                 //console.log(_error);
@@ -1453,19 +1453,19 @@ __webpack_require__.r(__webpack_exports__);
             });
           });
     }
-      // load custom fields of the selected category in the search form
+     // load custom fields of the selected category in the search form
     $('body').on('change', '.bdas-category-search, .directorist-category-select', function () {
         const $search_elem = $(this)
             .closest('form')
             .find('.atbdp-custom-fields-search');
-          if ($search_elem.length) {
+         if ($search_elem.length) {
             $search_elem.html('<div class="atbdp-spinner"></div>');
-              const data = {
+             const data = {
                 action: 'atbdp_custom_fields_search',
                 term_id: $(this).val(),
                 security: directorist.ajaxnonce,
             };
-              $.post(directorist.ajax_url, data, function (response) {
+             $.post(directorist.ajax_url, data, function (response) {
                 $search_elem.html(response);
                 const item = $('.custom-control').closest('.bads-custom-checks');
                 item.each(function (index, el) {

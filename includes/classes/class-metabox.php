dcba4e8144f33584b2b992d703adc3098ecf79a5
Armanul46 <aazztech4@gmail.com>
2023-03-08 13:08:14 +0600

    Sub location and Categories - admin - segmentation

--- includes/classes/class-metabox.php
+++ includes/classes/class-metabox.php
@@ -83,10 +83,11 @@ class ATBDP_Metabox {
 
 	}
 
-	public function render_listing_taxonomies( $listing_id, $term_id, $taxonomy_id ) {
+	public function render_listing_taxonomies( $listing_id, $term_id, $taxonomy_id, $parent_id = 0 ) {
 		$args = array(
 			'hide_empty' => 0,
-			'hierarchical' => false
+			'hierarchical' => true,
+			'parent' => $parent_id
 		);
 		$saving_terms   = get_the_terms( $listing_id, $taxonomy_id );
 		$saving_values    = array();
@@ -96,14 +97,27 @@ class ATBDP_Metabox {
 			}
 		}
 		$terms = get_terms( $taxonomy_id, $args);
-
+	
 		if( $terms ) {
 			foreach( $terms as $term ) {
 				$directory_type = get_term_meta( $term->term_id, '_directory_type', true );
 				$directory_type = ! empty ( $directory_type ) ? $directory_type : array();
-				$checked		= in_array( $term->term_id, $saving_values ) ? 'checked' : '';
+				$checked = in_array( $term->term_id, $saving_values ) ? 'checked' : '';
 				if( in_array( $term_id, $directory_type) ) { ?>
-					<li id="<?php echo esc_attr( $taxonomy_id ); ?>-<?php echo esc_attr( $term->term_id ); ?>"><label class="selectit"><input value="<?php echo esc_attr( $term->term_id ); ?>" type="checkbox" name="tax_input[<?php echo esc_attr( $taxonomy_id ); ?>][]" id="in-<?php echo esc_attr( $taxonomy_id ); ?>-<?php echo esc_attr( $term->term_id ); ?>" <?php echo ! empty( $checked ) ? esc_attr( $checked ) : ''; ?>> <?php echo esc_html( $term->name ); ?></label></li>
+					<li id="<?php echo esc_attr( $taxonomy_id ); ?>-<?php echo esc_attr( $term->term_id ); ?>">
+						<label class="selectit">
+							<input value="<?php echo esc_attr( $term->term_id ); ?>" type="checkbox" name="tax_input[<?php echo esc_attr( $taxonomy_id ); ?>][]" id="in-<?php echo esc_attr( $taxonomy_id ); ?>-<?php echo esc_attr( $term->term_id ); ?>" <?php echo ! empty( $checked ) ? esc_attr( $checked ) : ''; ?>>
+							<?php echo esc_html( $term->name ); ?>
+						</label>
+						<?php
+						$child_terms = get_term_children( $term->term_id, $taxonomy_id );
+						if ( $child_terms ) {
+							echo '<ul>';
+								$this->render_listing_taxonomies( $listing_id, $term_id, $taxonomy_id, $term->term_id );
+							echo '</ul>';
+						}
+						?>
+					</li>
 				<?php
 				}
 			}

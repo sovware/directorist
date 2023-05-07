f8c4de220f599e556e0917a8c9e87262be947bd8
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-05-07 12:37:32 +0600

    Range Slider Updated

--- templates/search-form/fields/radius_search.php
+++ templates/search-form/fields/radius_search.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.3.1
+ * @version 7.7.0
  */
 
 if (!defined('ABSPATH')) exit;
@@ -11,7 +11,7 @@ $default_distance = $data['default_radius_distance'];
 $value = !empty($_REQUEST['miles']) ? sanitize_text_field( wp_unslash( $_REQUEST['miles'] ) ) : $default_distance;
 ?>
 
-<div class="directorist-search-field">
+<div class="directorist-search-field direcorist-search-field-radius_search">
 
 	<?php if (!empty($data['label'])) : ?>
 		<label><?php echo esc_html($data['label']); ?></label>

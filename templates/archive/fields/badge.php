c3bd44f5673bd18bbc3d91070380ecd327bd26b8
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-05-07 10:47:32 +0600

    Badge Tooltip Added

--- templates/archive/fields/badge.php
+++ templates/archive/fields/badge.php
@@ -11,9 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 <span class="directorist-badge directorist-info-item directorist-badge-<?php echo esc_attr( $class )?>">
     <?php if ( $class == 'featured' ) : ?>
         <?php directorist_icon( 'fas fa-star' ); ?>
+        <span class="directorist-badge-tooltip directorist-badge-tooltip__featured">Featured</span>
     <?php elseif ( $class == 'new' ) : ?>
         <?php directorist_icon( 'fas fa-bolt' ); ?>
+        <span class="directorist-badge-tooltip directorist-badge-tooltip__new">New</span>
     <?php elseif ( $class == 'popular' ) : ?>
         <?php directorist_icon( 'fas fa-fire' ); ?>
+        <span class="directorist-badge-tooltip directorist-badge-tooltip__popular">Popular</span>
     <?php endif;?>
 </span>

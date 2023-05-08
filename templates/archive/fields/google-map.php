d4ea4aef470f85dc514ba9830c57b6961649f9ae
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-05-08 17:16:29 +0600

    GoogleMap Updated

--- templates/archive/fields/google-map.php
+++ templates/archive/fields/google-map.php
@@ -10,102 +10,112 @@
 
 	<?php if ( ! $map_is_disabled ) { ?>
 
-		<div class="map-info-wrapper" style="display:none;">
-			
+		<div class='map-info-wrapper map-listing-card-single' style="display:none";>
+
 			<?php 
 				if( ! empty( $display_favorite_badge_map ) ) {
 					$ls_data['listings']->get_favorite_badge(); 
 				}
 			?>
-
+			
 			<?php if ( ! empty( $display_image_map ) ) { ?>
-				<div class="map-info-img">
-					<?php
-					if ( ! $disable_single_listing ) {
-						echo "<a href='" . esc_url(get_the_permalink()) . "'>";
+				<div class='map-listing-card-single__img'>
+					<?php if ( ! $disable_single_listing ) { ?>
+						<a href='<?php echo esc_url( get_the_permalink() ); ?>'>
+						<?php
 					}
 
-					if ( ! empty( $ls_data['listing_prv_img'] ) ) {
-						echo '<img src="' . esc_url( $ls_data['prv_image'] ) . '" alt="' . esc_attr( get_the_title() ) . '" width="280">';
+					if ( ! empty( $ls_data['listing_prv_img'] ) ) { ?>
+						<img src='<?php echo esc_url( $ls_data['prv_image'] ); ?>' alt='<?php echo esc_attr( get_the_title() ); ?>'>
+						<?php
 					}
 
-					if ( ! empty( $ls_data['listing_img'][0] ) && empty( $ls_data['listing_prv_img'] ) ) {
-						echo '<img src="' . esc_url( $ls_data['gallery_img'] ) . '" alt="' . esc_attr( get_the_title() ) . '" width="280">';
+					if ( ! empty( $ls_data['listing_img'][0] ) && empty( $ls_data['listing_prv_img'] ) ) { ?>
+						<img src='<?php echo esc_url( $ls_data['gallery_img'] ); ?>' alt='<?php echo esc_attr( get_the_title() ); ?>'>
+						<?php
 					}
 
-					if ( empty( $ls_data['listing_img'][0] ) && empty( $ls_data['listing_prv_img'] ) ) {
-						echo '<img src="' . esc_url( $ls_data['default_img'] ) . '" alt="' . esc_attr( get_the_title() ) . '" width="280">';
+					if ( empty( $ls_data['listing_img'][0] ) && empty( $ls_data['listing_prv_img'] ) ) {?>
+						<img src='<?php echo esc_url( $ls_data['default_image'] ); ?>' alt='<?php echo esc_attr( get_the_title() ); ?>'>
+						<?php
 					}
 
-					if ( ! $disable_single_listing ) {
-						echo '</a>';
+					if ( ! $disable_single_listing ) { ?>
+						</a>
+						<?php
 					}
 					?>
 				</div>
+			<?php } ?>
 
-				<?php 
-				if( ! empty( $display_user_avatar_map ) ) {
-					$ls_data['listings']->get_user_avatar();
-				}
+			<?php 
+				if( ! empty( $display_user_avatar_map ) ) { ?>
+				<div class='map-listing-card-single__author'>
+					<?php
+						$ls_data['listings']->get_user_avatar();
+					?>
+				</div>
+			<?php } ?>
 
-				if( ! empty( $display_review_map ) ) {
-					$ls_data['listings']->get_listing_review();
-				}
+			<div class='map-listing-card-single__content'>
 
-				if( ! empty( $display_price_map ) ) {
-					$ls_data['listings']->get_price();
-				}
+				<?php if ( ! empty( $display_title_map ) ) { ?>
+					<?php if ( ! $disable_single_listing ) { ?>
+						<h3 class='map-listing-card-single__content__title'>
+							<a href='<?php echo esc_url( get_the_permalink() ); ?>'><?php the_title(); ?></a>
+						</h3>
+						<?php
+					}
+					else { ?>
+						<h3 class='map-listing-card-single__content__title'><?php the_title();?></h3>
+						<?php
+					}
+					?>
+				<?php } ?>
 
-			}
+				<?php if ( ! empty( $display_review_map ) || ! empty( $display_price_map )  ) { ?>
+					<div class="map-listing-card-single__content__meta">
+						<?php 
+							if( ! empty( $display_review_map ) ) {
+								$ls_data['listings']->get_listing_review();
+							}
 
-			if ( ! empty( $display_title_map ) || ! empty( $display_address_map ) || ! empty( $display_direction_map ) ) { ?>
+							if( ! empty( $display_price_map ) ) {
+								$ls_data['listings']->get_price();
+							}
+						?>
+					</div>
+				<?php } ?>
 
-				<div class="map-info-details">
-
-					<?php if ( ! empty( $display_title_map ) ) { ?>
-						<div class="atbdp-listings-title-block">
-							<h3 class="atbdp-no-margin">
-								<?php if ( ! $disable_single_listing ){ ?>
-									<a href="<?php the_permalink();?>"><?php the_title();?></a>
+				<?php if ( ! empty( $ls_data['address'] ) || ! empty( $ls_data['phone'] )  ) { ?>
+					<div class="map-listing-card-single__content__info">
+						<?php
+							if ( ! empty( $ls_data['address'] ) ) {
+								if ( ! empty( $display_address_map ) ) { ?>
+									<div class='directorist-info-item map-listing-card-single__content__address'>
+										<?php directorist_icon( 'fas fa-map-marker-alt' ); ?>
+										<div class="map-listing-card-single__content__location">
+											<?php echo $ls_data['listings']->get_the_location() ?>
+										</div>
+									</div>
 									<?php
 								}
-								else {
-									the_title();
-								}
-								?>
-							</h3>
-						</div>
-						<?php
-					}
 
-					if ( ! empty( $ls_data['address'] ) ) { ?>
-						<?php if ( ! empty( $display_address_map ) ) { ?>
-							<div class="map_addr"><?php directorist_icon( 'las la-map-marker' ); ?> <a href="" class="map-info-link"><?php echo esc_html( $ls_data['address'] ); ?></a></div>
-							<?php
-						}
+							}
 
-						if ( ! empty( $display_direction_map ) ) { ?>
-							<div class="map_get_dir">
-								<a href='http://www.google.com/maps?daddr=<?php echo esc_attr( $ls_data['manual_lat'] ); ?>,<?php echo esc_attr( $ls_data['manual_lng'] ); ?>' target='_blank'><?php esc_html_e( 'Get Directions', 'directorist' ); ?></a>
-								<?php directorist_icon( 'las la-arrow-right' ); ?>
-							</div>
-							<?php
-						}
-					}
-
-					if ( ! empty( $ls_data['phone'] ) && ! empty( $display_phone_map ) ) { ?>
-						<div class="map_phone"><?php directorist_icon( 'las la-map-marker' ); ?> <a href="" class="map-info-link"><?php echo esc_html( $ls_data['phone'] ); ?></a></div>
-						<?php
-					}
-					
-					do_action( 'atbdp_after_listing_content', $ls_data['post_id'], 'map' );?>
-				</div>
-				<?php
-			}
-			?>
-
-			<span class="iw-close-btn"><?php directorist_icon( 'las la-times' ); ?></span>
+							if ( ! empty( $ls_data['phone'] ) && ! empty( $display_phone_map ) ) { ?>
+								<div class='directorist-info-item map-listing-card-single__content__phone'>
+									<?php directorist_icon( 'fas fa-phone-alt' ); ?>
+									<a href='./' class='map-info-link'><?php echo esc_html( $ls_data['phone'] ); ?></a>
+								</div>
+								<?php
+							}
+						?>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
+
 		<?php
 	}
 	?>

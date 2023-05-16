c6e68e2257a02e7e56433119e1c0bbf2b8d9c562
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-05-16 11:36:46 +0600

    Rating Updated

--- templates/search-form/fields/review.php
+++ templates/search-form/fields/review.php
@@ -17,7 +17,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<div class="directorist-checkbox directorist-checkbox-primary">
 		<?php foreach ( $searchform->rating_field_data() as $option ) { ?>
 			<input type="checkbox" name="search_by_rating[<?php echo esc_attr( $option['value'] ); ?>]" value="<?php echo esc_attr( $option['value'] ); ?>" id="search_by_rating[<?php echo esc_attr( $option['value'] ); ?>]" <?php echo esc_attr( $option['checked'] ); ?>>
-			<label for="search_by_rating[<?php echo esc_attr( $option['value'] ); ?>]" class="directorist-checkbox__label"><?php echo esc_html( $option['label'] ); ?></label>
+			<label for="search_by_rating[<?php echo esc_attr( $option['value'] ); ?>]" class="directorist-checkbox__label">
+				<?php 
+					directorist_icon( 'fas fa-star', true, 'star-empty' );
+					directorist_icon( 'fas fa-star', true, 'star-empty' );
+					directorist_icon( 'fas fa-star', true, 'star-empty' );
+					directorist_icon( 'fas fa-star', true, 'star-empty' );
+					directorist_icon( 'fas fa-star', true, 'star-empty' );
+				?>
+			</label>
 		<?php } ?>
 	</div>
 </div>

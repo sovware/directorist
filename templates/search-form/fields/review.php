4f13fcaa5d80a3adf5a768eec790c8fdae3ae4e8
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-05-30 16:23:56 +0600

    Updated

--- templates/search-form/fields/review.php
+++ templates/search-form/fields/review.php
@@ -26,28 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 					directorist_icon( 'fas fa-star', true, 'star-empty' );
 					directorist_icon( 'fas fa-star', true, 'star-empty' );
 					directorist_icon( 'fas fa-star', true, 'star-empty' );
-
-					// // Rating
-					// $rating           = $option['value'];
-
-					// // Icons
-					// $icon_empty_star = directorist_icon( 'fas fa-star', false, 'star-empty' );
-					// $icon_full_star  = directorist_icon( 'fas fa-star', false, 'star-full' );
-
-					// // Stars
-					// $star_1 = ( $rating >= 1) ? $icon_full_star : $icon_empty_star;
-
-					// $star_2 = ( $rating >= 2) ? $icon_full_star : $icon_empty_star;
-
-					// $star_3 = ( $rating >= 3) ? $icon_full_star : $icon_empty_star;
-
-					// $star_4 = ( $rating >= 4) ? $icon_full_star : $icon_empty_star;
-
-					// $star_5 = ( $rating >= 5 ) ? $icon_full_star : $icon_empty_star;
-
-					// $review_stars = "{$star_1}{$star_2}{$star_3}{$star_4}{$star_5}";
-
-					// echo $review_stars;
 				?>
 			</label>
 		<?php } ?>

0882d7c27b9ea1ea253630fe11b1dae040c18af3
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-06-25 13:17:04 +0600

    Search Form Style - Improved

--- templates/search-form/custom-fields/url.php
+++ templates/search-form/custom-fields/url.php
@@ -2,13 +2,13 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.0.5.6
+ * @version 7.7.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div class="single_search_field ">
+<div class="directorist-search-field single_search_field ">
 
 	<?php if ( !empty($data['label']) ): ?>
 		<label><?php echo esc_html( $data['label'] ); ?></label>

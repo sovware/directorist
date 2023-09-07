7ebf272c74661a969edddbef0a3658b5fdcbbba0
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-09-07 09:14:09 +0600

    Remove Commented Code

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -2332,8 +2332,6 @@ function search_category_location_filter($settings, $taxonomy_id, $prefix = '')
                     $custom_field = in_array( $term->term_id, $settings['assign_to_category']['assign_to_cat'] ) ? true : '';
                 }
 
-                // $html .= '<option data-custom-field="' . $custom_field . '" value="' . $term->term_id . '" ' . $selected . '>';
-
                 $html .= '<option data-icon = "' . esc_attr( $icon_src ). '" data-custom-field="' . $custom_field . '" value="' . $term->term_id . '" ' . $selected . '>';
 
                 $html .= $prefix . $term->name;

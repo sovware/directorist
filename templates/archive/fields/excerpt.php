caa9dd2620569fad8985062a633819d72bd5c886
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-09-21 20:43:07 +0600

    Style - Updated

--- templates/archive/fields/excerpt.php
+++ templates/archive/fields/excerpt.php
@@ -12,10 +12,12 @@ if ( !$value ) {
 }
 ?>
 
-<p>
-	<?php echo esc_html( wp_trim_words( $value, (int) $data['words_limit'] ) );
-	if ( $data['show_readmore'] ) {
-		printf( '<a href="%s"> %s</a>', esc_url( $listings->loop['permalink'] ), esc_html( $data['show_readmore_text'] ) );
-	}
-	?>
-</p>
+<div class="directorist-listing-single__info--excerpt">
+	<p>
+		<?php echo esc_html( wp_trim_words( $value, (int) $data['words_limit'] ) );
+		if ( $data['show_readmore'] ) {
+			printf( '<a href="%s"> %s</a>', esc_url( $listings->loop['permalink'] ), esc_html( $data['show_readmore_text'] ) );
+		}
+		?>
+	</p>
+</div>

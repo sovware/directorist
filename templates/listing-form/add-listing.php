185e1eac3bf4c392f8003ffb643413e60dffa44b
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-09-21 08:27:32 +0600

    Markup - Improved

--- templates/listing-form/add-listing.php
+++ templates/listing-form/add-listing.php
@@ -54,7 +54,6 @@ $action_url = isset( $_SERVER['REQUEST_URI'] ) ? esc_url_raw( wp_unslash( $_SERV
                                 $counter = 1;
                                 foreach ( $form_data as $section ) {
                                     echo '<div class="multistep-wizard__single" id="add-listing-content-'. esc_attr( $counter ) . '">';
-                                    echo '<div class="multistep-wizard__single" id="add-listing-content-'. esc_attr( $counter ) . '">';
                                         $listing_form->section_template( $section );
                                     echo '</div>';
                                     $counter++; 

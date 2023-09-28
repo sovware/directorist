40af960afc85aeda43c95656b05255124fb116ae
vairafiq <vairafiq@gmail.com>
2023-09-28 09:56:43 +0600

    fix slider issue

--- includes/model/SingleListing.php
+++ includes/model/SingleListing.php
@@ -595,6 +595,11 @@ class Directorist_Single_Listing {
 			return;
 		}
 
+		$this->slider_field_template();
+	}
+
+	public function slider_field_template() {
+
 		$args = array(
 			'listing'    => $this,
 			'data'       => $this->get_slider_data(),

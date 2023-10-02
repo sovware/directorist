cdf7fe5e5f0d809789cf23eddeae61978a0fd034
vairafiq <vairafiq@gmail.com>
2023-10-02 11:02:26 +0600

    use esc_html

--- includes/payments/class-order.php
+++ includes/payments/class-order.php
@@ -340,7 +340,7 @@ class ATBDP_Order
                 $currency        = atbdp_get_payment_currency();
                 $symbol          = atbdp_currency_symbol($currency);
                 ('after' == $c_position) ? $after = $symbol : $before = $symbol;
-                echo esc_attr( $before . $value . $after );
+                echo esc_html( $before . $value . $after );
                 break;
             case 'type' :
                 $gateway = get_post_meta($post_id, '_payment_gateway', true);

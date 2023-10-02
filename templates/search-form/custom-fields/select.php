8728b2f302fd3369c3b7df21381698885a20afa3
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-10-02 16:18:03 +0600

    All Listing Sidebar & Search Form Markup & Style - Improved

--- templates/search-form/custom-fields/select.php
+++ templates/search-form/custom-fields/select.php
@@ -8,7 +8,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div class="directorist-search-field">
+<div class="directorist-search-field directorist-form-group">
 
 	<div class="directorist-select directorist-search-field__input">
 		<label class="directorist-search-field__label"><?php echo esc_attr( $data['placeholder'] ); ?></label>
--- templates/search-form/fields/category.php
+++ templates/search-form/fields/category.php
@@ -8,7 +8,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 	$selected_item = $searchform::get_selected_category_option_data();
 ?>
-<div class="directorist-search-field">
+<div class="directorist-search-field directorist-form-group">
 	<div class="directorist-select directorist-search-category directorist-search-field__input">
 		<label class="directorist-search-field__label"><?php echo esc_attr( $data['placeholder'] ); ?></label>
 		<select name="in_cat" class="<?php echo esc_attr($searchform->category_class); ?>" data-placeholder="<?php echo esc_attr($data['placeholder']); ?>" <?php echo ! empty( $data['required'] ) ? 'required="required"' : ''; ?> data-isSearch="true" data-selected-id="<?php echo esc_attr( $selected_item['id'] ); ?>" data-selected-label="<?php echo esc_attr( $selected_item['label'] ); ?>">

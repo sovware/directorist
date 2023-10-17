9bdca5e1a72a1b22d341df66e8f4069afdacff78
Armanul46 <aazztech4@gmail.com>
2023-10-17 11:46:16 +0600

    work on min max number validation of custom number field

--- includes/fields/class-directorist-number-field.php
+++ includes/fields/class-directorist-number-field.php
@@ -14,10 +14,19 @@ class Number_Field extends Base_Field {
 	public $type = 'number';
 
 	public function validate( $posted_data ) {
-		$value = $this->get_value( $posted_data );
+		$value     = $this->get_value( $posted_data );
+		$min_value = $this->__get('min_value');
+		$max_value = $this->__get('max_value');
 		
 		if ( ! is_numeric( $value ) ) {
 			$this->add_error( __( 'Invalid number.', 'directorist' ) );
+
+			return false;
+		}
+
+		if ( isset( $min_value, $max_value ) && ( $min_value > $value || ( $max_value > 0 && $max_value < $value ) ) ) {
+			$error_message = sprintf( __( 'Value should be between %d and %d', 'directorist' ), $min_value, $max_value );
+			$this->add_error( $error_message );
 
 			return false;
 		}

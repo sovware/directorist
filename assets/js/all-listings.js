13f0e8d4c11d107abd0a593e438babe1ee6ab29f
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-10-17 12:59:44 +0600

    Add Listing Number with Prefix Style - Updated

--- assets/js/all-listings.js
+++ assets/js/all-listings.js
@@ -120,6 +120,25 @@ function init() {
       return;
     }
     selec2_add_custom_close_button($(this));
+    var selectItems = this.parentElement.querySelectorAll('.select2-selection__choice');
+    selectItems.forEach(function (item) {
+      item.childNodes && item.childNodes.forEach(function (node) {
+        if (node.nodeType && node.nodeType === Node.TEXT_NODE) {
+          var originalString = node.textContent;
+          var modifiedString = originalString.replace(/^[\s\xa0]+/, '');
+          node.textContent = modifiedString;
+          item.title = modifiedString;
+        }
+      });
+    });
+    var customSelectItem = this.parentElement.querySelector('.select2-selection__rendered');
+    customSelectItem.childNodes && customSelectItem.childNodes.forEach(function (node) {
+      if (node.nodeType && node.nodeType === Node.TEXT_NODE) {
+        var originalString = node.textContent;
+        var modifiedString = originalString.replace(/^[\s\xa0]+/, '');
+        node.textContent = modifiedString;
+      }
+    });
   });
 }
 function selec2_add_custom_dropdown_toggle_button() {
@@ -594,56 +613,43 @@ function convertToSelect2(field) {
 
 // Archive Sidebar
 window.addEventListener('DOMContentLoaded', function () {
-  /* Archive sidebar toggle */
-  var archiveSidebar = document.querySelector('.listing-with-sidebar__sidebar');
-  var archiveSidebarToggle = document.querySelector('.directorist-archive-sidebar-toggle');
-  var archiveSidebarClose = document.querySelector('.directorist-advanced-filter__close');
-  var body = document.body; // Toggle sidebar and update toggle button's active state
+  var body = document.body;
 
+  // Toggle sidebar and update toggle button's active state
   function toggleSidebar(toggleBtn, archiveSidebar) {
     archiveSidebar.classList.toggle('listing-with-sidebar__sidebar--open');
-    archiveSidebarToggle.classList.toggle('directorist-archive-sidebar-toggle--active');
-  } // Close sidebar and reset toggle button's active state
-
-
-  function closeSidebar() {
-    if (archiveSidebar) {
-      archiveSidebar.classList.remove('listing-with-sidebar__sidebar--open');
-    }
-
-    if (archiveSidebarToggle) {
-      archiveSidebarToggle.classList.remove('directorist-archive-sidebar-toggle--active');
-    }
-  } // Event delegation for sidebar toggle and close buttons
-
-
-  function handleSidebarToggleClick(e) {
-    e.preventDefault();
-    toggleSidebar();
+    toggleBtn.classList.toggle('directorist-archive-sidebar-toggle--active');
+    body.classList.toggle('modal-overlay-enabled');
   }
 
-  function handleSidebarCloseClick(e) {
-    e.preventDefault();
-    closeSidebar();
-  } // Event delegation for outside click to close sidebar
+  // Close sidebar and reset toggle button's active state
+  function closeSidebar(toggleBtn, archiveSidebar) {
+    archiveSidebar.classList.remove('listing-with-sidebar__sidebar--open');
+    toggleBtn.classList.remove('directorist-archive-sidebar-toggle--active');
+    body.classList.remove('modal-overlay-enabled');
+  }
 
-
-  function handleOutsideClick(e) {
-    if (!e.target.closest('.listing-with-sidebar__sidebar') && e.target !== archiveSidebarToggle) {
-      closeSidebar();
+  // Toggle, Close sidebar when toggle/close button is clicked
+  body.addEventListener('click', function (e) {
+    var targetElement = e.target;
+    if (targetElement.classList.contains('directorist-archive-sidebar-toggle') || targetElement.parentElement.classList.contains('directorist-archive-sidebar-toggle')) {
+      var btn = targetElement;
+      var sidebar = targetElement.closest('.listing-with-sidebar').querySelector('.listing-with-sidebar__sidebar');
+      toggleSidebar(btn, sidebar);
+    } else if (targetElement.classList.contains('directorist-advanced-filter__close') || targetElement.parentElement.classList.contains('directorist-advanced-filter__close')) {
+      var _btn = targetElement.closest('.listing-with-sidebar').querySelector('.directorist-archive-sidebar-toggle');
+      var _sidebar = targetElement.closest('.listing-with-sidebar').querySelector('.listing-with-sidebar__sidebar');
+      closeSidebar(_btn, _sidebar);
+    } else if (targetElement.classList.contains('modal-overlay-enabled')) {
+      var archiveSidebar = document.querySelectorAll('.listing-with-sidebar__sidebar');
+      archiveSidebar && archiveSidebar.forEach(function (sidebar) {
+        if (sidebar.classList.contains('listing-with-sidebar__sidebar--open')) {
+          var _btn2 = sidebar.closest('.listing-with-sidebar').querySelector('.directorist-archive-sidebar-toggle');
+          closeSidebar(_btn2, sidebar);
+        }
+      });
     }
-  } // Attach event listeners
-
-
-  if (archiveSidebarToggle) {
-    archiveSidebarToggle.addEventListener('click', handleSidebarToggleClick);
-  }
-
-  if (archiveSidebarClose) {
-    archiveSidebarClose.addEventListener('click', handleSidebarCloseClick);
-  }
-
-  body.addEventListener('click', handleOutsideClick);
+  });
 });
 
 /***/ }),
@@ -2646,6 +2652,7 @@ window.addEventListener('DOMContentLoaded', function () {
       }, {
         key: "onSubmit",
         value: function onSubmit(event) {
+          var _this2 = this;
           event.preventDefault();
           var form = $('.directorist-review-container #commentform');
           var originalButtonLabel = form.find('[type="submit"]').val();
@@ -2706,6 +2713,11 @@ window.addEventListener('DOMContentLoaded', function () {
             console.log(data);
             CommentAddReplyHandler.showError(form, body.find('.wp-die-message'));
             $(document).trigger('directorist_review_update_failed');
+            if (data.status === 403 || data.status === 401) {
+              $(document).off('submit', '.directorist-review-container #commentform', _this2.onSubmit);
+              $('#comment').prop('disabled', false);
+              form.find('[type="submit"]').prop('disabled', false).click();
+            }
           });
           do_comment.always(function () {
             $('#comment').prop('disabled', false);

ef4e883f4298a6249c765b712f04ec639600c7fc
Armanul46 <aazztech4@gmail.com>
2023-10-17 19:39:10 +0600

    fixed spacing issue

--- templates/listing-form/custom-fields/number.php
+++ templates/listing-form/custom-fields/number.php
@@ -17,13 +17,13 @@ $data_max = $data['max_value'] ?? '';
 
 	<div class="directorist-form-group__with-prefix">
 
-		<?php if( ! empty( $data['prepend'] ) ) : ?>
+		<?php if ( ! empty( $data['prepend'] ) ) : ?>
 			<span class="directorist-form-group__prefix directorist-form-group__prefix--start"><?php echo esc_html( $data['prepend'] ); ?></span>
 		<?php endif; ?>
 
 		<input type="number" name="<?php echo esc_attr( $data['field_key'] ); ?>" id="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" value="<?php echo esc_attr( $data['value'] ); ?>" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" step="any" min="<?php echo esc_attr( $data_min ); ?>" max="<?php echo esc_attr( $data_max ); ?>" <?php $listing_form->required( $data ); ?>>
 
-		<?php if( ! empty( $data['append'] ) ) : ?>
+		<?php if ( ! empty( $data['append'] ) ) : ?>
 			<span class="directorist-form-group__prefix directorist-form-group__prefix--end"><?php echo esc_html( $data['append'] ); ?></span>
 		<?php endif; ?>
 

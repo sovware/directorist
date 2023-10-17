21e65907896f2151e53918eb4e061acf12ba7a42
Armanul46 <aazztech4@gmail.com>
2023-10-17 15:12:48 +0600

    work on number custom field

--- templates/listing-form/custom-fields/number.php
+++ templates/listing-form/custom-fields/number.php
@@ -16,9 +16,17 @@ $data_max = $data['max_value'] ?? '';
 	<?php $listing_form->field_label_template( $data );?>
 
 	<div class="directorist-form-group__with-prefix">
-		<span class="directorist-form-group__prefix directorist-form-group__prefix--start">KM</span>
+
+		<?php if( ! empty( $data['prepend'] ) ) : ?>
+			<span class="directorist-form-group__prefix directorist-form-group__prefix--start"><?php echo esc_html( $data['prepend'] ); ?></span>
+		<?php endif; ?>
+
 		<input type="number" name="<?php echo esc_attr( $data['field_key'] ); ?>" id="<?php echo esc_attr( $data['field_key'] ); ?>" class="directorist-form-element" value="<?php echo esc_attr( $data['value'] ); ?>" placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" step="any" min="<?php echo esc_attr( $data_min ); ?>" max="<?php echo esc_attr( $data_max ); ?>" <?php $listing_form->required( $data ); ?>>
-		<span class="directorist-form-group__prefix directorist-form-group__prefix--end">KM</span>
+
+		<?php if( ! empty( $data['append'] ) ) : ?>
+			<span class="directorist-form-group__prefix directorist-form-group__prefix--end"><?php echo esc_html( $data['append'] ); ?></span>
+		<?php endif; ?>
+
 	</div>
 
 	<?php $listing_form->field_description_template( $data ); ?>

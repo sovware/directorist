77fe224ed962c2941a85ad8739cdc74976f63d6f
Armanul46 <aazztech4@gmail.com>
2023-11-02 11:18:46 +0600

    improved atbdp_parse_vidoes method

--- directorist-base.php
+++ directorist-base.php
@@ -671,18 +671,17 @@ final class Directorist_Base
 	/**
 	 * Deprecated: 7.8.0
 	 * 
-	 * This function is deprecated since version 7.8.0. Please use parseVideo() instead.
+	 * This function is deprecated since version 7.8.0. Please use parse_video() instead.
 	 *
 	 * @param string $url The URL to parse for videos.
 	 * @return mixed The parsed video URL.
 	 *
-	 * @deprecated Use parseVideo() for video parsing.
+	 * @deprecated Use parse_video() for video parsing.
 	 */
 	public function atbdp_parse_videos( $url ) {	
-		$helper = new \Directorist\Helper();
-		_deprecated_function( __METHOD__, '7.8.0', $helper->parse_video( $url ) );
-		
-		return $helper->parse_video( $url );
+		_deprecated_function( __METHOD__, '7.8.0', 'Directorist\Helper::parse_video()' );
+
+		return \Directorist\Helper::parse_video( $url );
 	}
 
 	public function atbdp_body_class($c_classes)

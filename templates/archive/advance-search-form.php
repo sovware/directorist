b1f4c562c88c6a7446a4480b4019dd43acc532af
Armanul46 <aazztech4@gmail.com>
2023-11-22 12:25:53 +0600

    removed esc_url from advance-search-form

--- templates/archive/advance-search-form.php
+++ templates/archive/advance-search-form.php
@@ -9,7 +9,7 @@ use \Directorist\Helper;
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
-<form action="<?php esc_url( atbdp_search_result_page_link() ); ?>" class="directorist-search-form directorist-advanced-search">
+<form action="<?php atbdp_search_result_page_link(); ?>" class="directorist-search-form directorist-advanced-search">
 	<div class="directorist-search-form-box directorist-search-form__box">
 		<div class="directorist-advanced-filter__top">
 			<h2 class="directorist-advanced-filter__title"><?php esc_html_e( 'Filters', 'directorist' ); ?></h2>

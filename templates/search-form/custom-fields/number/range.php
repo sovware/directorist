568160fa078e9d8d007eee969a928bed48f0903f
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-12-07 12:29:54 +0600

    Custom Number Range Slider - Added

---
+++ templates/search-form/custom-fields/number/range.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author  wpWax
+ * @since   8.0
+ * @version 8.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+$step = $data['step'];
+$min_val = $data['options']['min_value'];
+$max_value = $data['options']['max_value'];
+
+?>
+
+<div class="directorist-search-field directorist-custom-range-slider">
+    <div id="directorist-custom-range-slider__slide" step="<?php echo esc_attr($step) ?>"></div>
+    <div class="directorist-custom-range-slider__wrap">
+        <div class="directorist-custom-range-slider__value">
+            <label for="directorist-custom-range-slider__value__min">Min</label>
+            <input type="number" placeholder="Min" value="<?php echo esc_attr($min_val) ?>" name="directorist-custom-range-slider__value__min" id="directorist-custom-range-slider__value__min" class="directorist-custom-range-slider__value__min">
+        </div>
+        <div class="directorist-custom-range-slider__value">
+            <label for="directorist-custom-range-slider__value__max">Max</label>
+            <input type="number" placeholder="Max" value="<?php echo esc_attr($max_value) ?>" name="directorist-custom-range-slider__value__max" id="directorist-custom-range-slider__value__max" class="directorist-custom-range-slider__value__max">
+        </div>
+        <input type="hidden" name="custom_field[<?php echo esc_attr( $data['field_key'] ); ?>]" id="directorist-custom-range-slider__value__range" value="<?php echo esc_attr($min_val) ?>-<?php echo esc_attr($max_value) ?>">
+    </div>
+
+</div>

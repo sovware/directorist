206070872141a661191c27d57d797ad2918e2d33
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2024-01-30 16:02:08 +0600

    Fix "Go Back" button issue in single listing (#1695)

--- templates/single/top-actions.php
+++ templates/single/top-actions.php
@@ -2,7 +2,7 @@
 /**
  * @author  wpWax
  * @since   6.6
- * @version 7.4.0
+ * @version 7.8.4
  */
 
 use \Directorist\Directorist_Single_Listing;
@@ -16,9 +16,9 @@ $listing = Directorist_Single_Listing::instance();
 
 	<?php if ( $listing->current_user_is_author() ): ?>
 
-		<?php if ( $listing->display_back_link() && !$listing->has_redirect_link() ): ?>
+		<?php if ( $listing->display_back_link() && ! $listing->has_redirect_link() && wp_get_referer() ): ?>
 
-		<a href="javascript:history.back()" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
+		<a href="<?php echo esc_url( wp_get_referer() ) ?>" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
 
 	<?php endif; ?>
 
@@ -35,9 +35,9 @@ $listing = Directorist_Single_Listing::instance();
 
 	</div>
 
-	<?php elseif( $listing->display_back_link() ): ?>
+	<?php elseif( $listing->display_back_link() && wp_get_referer() ): ?>
 
-		<a href="javascript:history.back()" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
+		<a href="<?php echo esc_url( wp_get_referer() ) ?>" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
 
 	<?php endif; ?>
 

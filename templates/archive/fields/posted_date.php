3af33f3b8697cf2bbbe06e369f216ebab596c96e
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-01-30 09:29:02 +0600

    Archive - Posted Date Field Tag - Changed

--- templates/archive/fields/posted_date.php
+++ templates/archive/fields/posted_date.php
@@ -6,12 +6,12 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
-$container_element = $listings->view === 'list' ? 'div' : 'li';
+
 ?>
 
-<<?php echo esc_html( $container_element ); ?> class="directorist-listing-card-posted-on">
+<li class="directorist-listing-card-posted-on">
     <?php directorist_icon( $icon );?>
     <span>
         <?php echo esc_html( $listings->loop_get_published_date( $data ) );?>
     </span>
-</<?php echo esc_html( $container_element ); ?>>
+</li>

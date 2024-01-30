529445ddb1d73a3f2c3c612cb9f842369cb51a27
Armanul46 <aazztech4@gmail.com>
2024-01-30 15:58:53 +0600

    fixed go back url issue

--- templates/single/top-actions.php
+++ templates/single/top-actions.php
@@ -16,9 +16,9 @@ $listing = Directorist_Single_Listing::instance();
 
 	<?php if ( $listing->current_user_is_author() ): ?>
 
-		<?php if ( $listing->display_back_link() && !$listing->has_redirect_link() ): ?>
+		<?php if ( $listing->display_back_link() && ! $listing->has_redirect_link() && wp_get_referer() ): ?>
 
-		<a href="javascript:history.back()" onclick="history.go(-1)" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
+		<a href="<?php echo esc_url( wp_get_referer() ) ?>" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
 
 	<?php endif; ?>
 
@@ -35,9 +35,9 @@ $listing = Directorist_Single_Listing::instance();
 
 	</div>
 
-	<?php elseif( $listing->display_back_link() ): ?>
+	<?php elseif( $listing->display_back_link() && wp_get_referer() ): ?>
 
-		<a href="javascript:history.back()" onclick="history.go(-1)" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
+		<a href="<?php echo esc_url( wp_get_referer() ) ?>" class="directorist-return-back"><?php directorist_icon( 'las la-angle-left' ); ?> <?php esc_html_e( 'Go Back', 'directorist'); ?></a>
 
 	<?php endif; ?>
 

da9d03cf327e9722fedbd579e19201af0cb656f8
Armanul46 <aazztech4@gmail.com>
2024-02-27 13:00:57 +0600

    use FormData instead of selecting each field separately in registration form

--- assets/src/js/public/components/register-form.js
+++ assets/src/js/public/components/register-form.js
@@ -2,26 +2,16 @@ jQuery(($) => {
 	$('.directorist__authentication__signup').on('submit', function(e) {
 		e.preventDefault();
         
-        var data = {
-            action           : 'directorist_register_form',
-            directorist_nonce: $(this).find("input[name='directorist_nonce']").val(),
-            username         : $(this).find("input[name='username']").val(),
-            email            : $(this).find("input[name='email']").val(),
-            password         : $(this).find("input[name='password']").val(),
-            fname            : $(this).find("input[name='fname']").val(),
-            lname            : $(this).find("input[name='lname']").val(),
-            website          : $(this).find("input[name='website']").val(),
-            bio              : $(this).find("input[name='bio']").val(),
-            previous_page    : $(this).find("input[name='previous_page']").val(),
-            user_type        : $(this).find("input[name='user_type']:checked").val(),
-            privacy_policy   : $(this).find("input[name='privacy_policy']:checked").val(),
-            t_c_check        : $(this).find("input[name='t_c_check']:checked").val(),
-        };
+        var formData = new FormData(this);
+        formData.append('action', 'directorist_register_form');
 
         $.ajax({
             url: directorist.ajaxurl,
             type: "POST",
-            data: data,
+            data: formData,
+            contentType: false,
+            processData: false,
+            cache: false,
             success: function (response) {
                 if( response.success ) {
                     $('.directorist-register-error').hide();

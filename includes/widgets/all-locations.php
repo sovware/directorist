62b231d343de2ae7d19899bd52c0b1c0ca02d12a
obiPlabon <obi.plabon@gmail.com>
2024-04-01 12:22:12 +0600

    Fixed and refactored locations widget

--- includes/widgets/all-locations.php
+++ includes/widgets/all-locations.php
@@ -75,18 +75,22 @@ class All_Locations extends \WP_Widget {
             'immediate_category' => [
 				'label'   => esc_html__( 'Show all the top-level locations only', 'directorist' ),
 				'type'    => 'checkbox',
+				'value'   => 1,
 			],
             'hide_empty' => [
 				'label'   => esc_html__( 'Hide empty locations', 'directorist' ),
 				'type'    => 'checkbox',
+				'value'   => 1,
 			],
             'show_count' => [
 				'label'   => esc_html__( 'Display listing counts', 'directorist' ),
 				'type'    => 'checkbox',
+				'value'   => 1,
 			],
             'single_only' => [
 				'label'   => esc_html__( 'Display only on single listing', 'directorist' ),
 				'type'    => 'checkbox',
+				'value'   => 1,
 			],
         ];
 
@@ -100,10 +104,10 @@ class All_Locations extends \WP_Widget {
 		$instance['display_as']         = ! empty( $new_instance['display_as'] ) ? sanitize_text_field( $new_instance['display_as'] ) : 'list';
         $instance['order_by']           = ! empty( $new_instance['order_by'] ) ? sanitize_text_field( $new_instance['order_by'] ) : 'id';
         $instance['order']              = ! empty( $new_instance['order'] ) ? sanitize_text_field( $new_instance['order'] ) : 'asc';
-        $instance['immediate_category'] = isset( $new_instance['immediate_category'] ) ? 1 : 0;
-        $instance['hide_empty']         = isset( $new_instance['hide_empty'] ) ? 1 : 0;
-        $instance['show_count']         = isset( $new_instance['show_count'] ) ? 1 : 0;
-        $instance['single_only']        = isset( $new_instance['single_only'] ) ? 1 : 0;
+        $instance['immediate_category'] = ! empty( $new_instance['immediate_category'] ) ? 1 : 0;
+        $instance['hide_empty']         = ! empty( $new_instance['hide_empty'] ) ? 1 : 0;
+        $instance['show_count']         = ! empty( $new_instance['show_count'] ) ? 1 : 0;
+        $instance['single_only']        = ! empty( $new_instance['single_only'] ) ? 1 : 0;
         $instance['max_number']         = ! empty( $new_instance['max_number'] ) ? $new_instance['max_number'] : '';
 
 		return $instance;
@@ -186,104 +190,81 @@ class All_Locations extends \WP_Widget {
             'number'       => !empty($settings['max_number']) ? $settings['max_number'] : ''
         );
 
-        $terms       = get_terms( $args );
+        $terms = get_terms( $args );
+
+		if ( is_wp_error( $terms ) ) {
+			return;
+		}
+
         $parent      = $args['parent'];
-        $child_class = !empty($parent) ? 'atbdp_child_location' : 'atbdp_parent_location';
+        $child_class = ! empty( $parent ) ? 'atbdp_child_location' : 'atbdp_parent_location';
         $html        = '';
+		$html .= '<ul class="' .$child_class. '">';
 
-        if ( count( $terms ) > 0 ) {
-            $i = 1;
-            $html .= '<ul class="' .$child_class. '">';
-            foreach( $terms as $term ) {
-                $child_category = get_term_children($term->term_id,ATBDP_LOCATION);
-                $plus_icon = (!empty($child_category) && empty($parent) ) ? directorist_icon( 'las la-plus', false ) : '';
-                $settings['term_id'] = $term->term_id;
+		foreach ( $terms as $term ) {
+			$child_category = get_term_children($term->term_id,ATBDP_LOCATION);
+			$plus_icon = (!empty($child_category) && empty($parent) ) ? directorist_icon( 'las la-plus', false ) : '';
+			$settings['term_id'] = $term->term_id;
 
-                $count = 0;
-                if( ! empty( $settings['hide_empty'] ) || ! empty( $settings['show_count'] ) ) {
-                    $count = atbdp_listings_count_by_location( $term->term_id );
+			$html .= '<li>';
+			$html .= '<a href="' . \ATBDP_Permalink::atbdp_get_location_page( $term ) . '">';
+			$html .= $term->name;
 
-                    if( ! empty( $settings['hide_empty'] ) && 0 == $count ) continue;
-                }
+			if ( ! empty( $settings['show_count'] ) ) {
+				$html .= ' (' . $term->count . ')';
+			}
 
-                $html .= '<li>';
-                $html .= '<a href="' . \ATBDP_Permalink::atbdp_get_location_page( $term ) . '">';
-                $html .= $term->name;
-                if( ! empty( $settings['show_count'] ) ) {
-                    $expired_listings = atbdp_get_expired_listings(ATBDP_LOCATION, $term->term_id);
-                    $number_of_expired = $expired_listings->post_count;
-                    $number_of_expired = !empty($number_of_expired)?$number_of_expired:'0';
-                    $totat = ($count)?($count-$number_of_expired):$count;
-                    $html .= ' (' . $totat . ')';
-                }
-                $html .= '</a>'. $plus_icon . '';
-                $html .= $this->list_locations( $settings );
-                $html .= '</li>';
-                if(!empty($args['number'])) {
-                    if( $i++ == $args['number'] ) break;
-                }
-            }
-            $html .= '</ul>';
+			$html .= '</a>'. $plus_icon . '';
+			$html .= $this->list_locations( $settings );
+			$html .= '</li>';
+		}
 
-        }
+		$html .= '</ul>';
 
         return $html;
 
     }
 
     public function dropdown_locations( $settings, $prefix = '' ) {
-
-        if( $settings['immediate_category'] ) {
-
-            if( $settings['term_id'] > $settings['parent'] && ! in_array( $settings['term_id'], $settings['ancestors'] ) ) {
-                return;
-            }
-
+        if ( $settings['immediate_category'] &&
+			( $settings['term_id'] > $settings['parent'] ) &&
+			! in_array( $settings['term_id'], $settings['ancestors'] ) ) {
+			return;
         }
 
         $term_slug = get_query_var( ATBDP_LOCATION );
 
         $args = array(
             'taxonomy'     => ATBDP_LOCATION,
-            'orderby'      => $settings['orderby'],
             'order'        => $settings['order'],
+            'orderby'      => $settings['orderby'],
             'hide_empty'   => $settings['hide_empty'],
-            'parent'       => !empty($settings['term_id']) ? $settings['term_id'] : '',
             'hierarchical' => ! empty( $settings['hide_empty'] ) ? true : false,
-            'number'       => !empty($settings['max_number']) ? $settings['max_number'] : ''
+            'parent'       => ! empty( $settings['term_id'] ) ? $settings['term_id'] : '',
+            'number'       => ! empty( $settings['max_number'] ) ? $settings['max_number'] : ''
         );
 
         $terms = get_terms( $args );
 
+		if ( is_wp_error( $terms ) ) {
+			return;
+		}
+
         $html = '';
 
-        if( count( $terms ) > 0 ) {
-            $i = 1;
-            foreach( $terms as $term ) {
-                $settings['term_id'] = $term->term_id;
+		foreach ( $terms as $term ) {
+			$settings['term_id'] = $term->term_id;
 
-                $count = 0;
-                if( ! empty( $settings['hide_empty'] ) || ! empty( $settings['show_count'] ) ) {
-                    $count = atbdp_listings_count_by_category( $term->term_id );
+			$html .= sprintf( '<option value="%s" %s>', $term->term_id, selected( $term->term_id, $term_slug, false ) );
+			$html .= $prefix . $term->name;
 
-                    if( ! empty( $settings['hide_empty'] ) && 0 == $count ) continue;
-                }
-
-                $html .= sprintf( '<option value="%s" %s>', $term->term_id, selected( $term->term_id, $term_slug, false ) );
-                $html .= $prefix . $term->name;
-                if( ! empty( $settings['show_count'] ) ) {
-                    $html .= ' (' . $count . ')';
-                }
-                //$html .= $this->dropdown_locations( $settings, $prefix . '&nbsp;&nbsp;&nbsp;' );
-                $html .= '</option>';
-                if(!empty($args['number'])) {
-                    if( $i++ == $args['number'] ) break;
-                }
-            }
-
-        }
+			if ( ! empty( $settings['show_count'] ) ) {
+				$html .= ' (' . $term->count . ')';
+			}
+			//$html .= $this->dropdown_locations( $settings, $prefix . '&nbsp;&nbsp;&nbsp;' );
+			$html .= '</option>';
+		}
 
         return $html;
-
     }
 }

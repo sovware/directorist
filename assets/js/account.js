3fb7ba5cb351a3b415d897179099de4b96cf9ca2
Armanul46 <aazztech4@gmail.com>
2024-08-01 14:42:57 +0600

    fixed registration notice issue

--- assets/js/account.js
+++ assets/js/account.js
@@ -281,15 +281,16 @@ jQuery(function ($) {
       processData: false,
       cache: false,
       success: function success(response) {
+        console.log(response);
         if (response.success) {
           $('.directorist-register-error').hide();
-          if (response.data.redirect_url) {
-            $('.directorist-register-error').empty().show().append(response.data.redirect_message).css({
+          if (response.redirect_url) {
+            $('.directorist-register-error').empty().show().append(response.redirect_message).css({
               'color': '#009114',
               'background-color': '#d9efdc'
             });
             setTimeout(function () {
-              window.location.href = response.data.redirect_url;
+              window.location.href = response.redirect_url;
             }, 500);
           }
         } else {

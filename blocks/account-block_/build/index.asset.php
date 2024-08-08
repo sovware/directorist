68b7271b5cfbb2ef9fbeffcc5647f24a9013653b
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-08-08 12:06:02 +0600

    test

---
+++ blocks/account-block_/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes'), 'version' => 'ac021b2e55f54a5ce437');

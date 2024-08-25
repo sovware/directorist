0281969de021367ab162c5f2717027c4f9392ddc
Armanul46 <aazztech4@gmail.com>
2024-08-25 11:23:23 +0600

    fixed category custom field issue on add listing

--- assets/src/js/public/components/register-form.js
+++ assets/src/js/public/components/register-form.js
@@ -16,13 +16,15 @@ jQuery(($) => {
                 console.log( response );
                 if( response.success ) {
                     $('.directorist-register-error').hide();
-                    if( response.redirect_url ) {
-                        $('.directorist-register-error').empty().show().append( response.redirect_message ).css({
-                            'color'           : '#009114',
-                            'background-color': '#d9efdc'
-                        });
+                    if( response.data.redirect_url ) {
+                        if( response.data.redirect_message ) {
+                            $('.directorist-register-error').empty().show().append( response.data.redirect_message ).css({
+                                'color'           : '#009114',
+                                'background-color': '#d9efdc'
+                            });
+                        }
                         setTimeout(function () {
-                            window.location.href = response.redirect_url;
+                            window.location.href = response.data.redirect_url;
                         }, 500)
                     }
                 } else { 

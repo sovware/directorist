cc473aa4e1a415cf17ee44ce3f80f7f7bba0b52e
Rabbi Islam Rony ⚡️ <ronya4927@gmail.com>
2024-08-29 15:43:21 +0600

    Block search and account design improve

--- blocks/build/account-button/index.asset.php
+++ blocks/build/account-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes'), 'version' => '5f7894af25210d2733be');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes'), 'version' => '70aee53912651ce62498');

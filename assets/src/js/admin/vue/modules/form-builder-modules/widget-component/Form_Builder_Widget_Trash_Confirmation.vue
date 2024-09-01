bb21a679e02a6e7ddb3fbd01258e04f6561d47a3
Shamim Ahmed <shamimeforb@gmail.com>
2024-09-01 12:09:33 +0600

    Builder improvement (#1838)

---
+++ assets/src/js/admin/vue/modules/form-builder-modules/widget-component/Form_Builder_Widget_Trash_Confirmation.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    v-if="visible"
+    class="confirmation-modal-overlay"
+    @click="handleOverlayClick"
+  >
+    <div class="confirmation-modal" @click.stop>
+      <p>
+        Removing "<strong>{{
+          widgetName
+        }}</strong
+        >" field will also remove it from the single and search pages. Are you sure you want to proceed?
+      </p>
+      <button @click="confirmDelete">Yes, delete</button>
+      <button
+        class="confirmation-modal-action-btn__cancel"
+        @click="cancelDelete"
+      >
+        Cancel
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConfirmationModal",
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    widgetName: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    confirmDelete() {
+      this.$emit("confirm");
+    },
+    cancelDelete() {
+      this.$emit("cancel");
+    },
+    handleOverlayClick() {
+      this.cancelDelete();
+    },
+  },
+};
+</script>

113afac46d562aa74795c49f0961097953b9e755
obiPlabon <obi.plabon@gmail.com>
2024-09-09 13:02:44 +0600

    Fixed registration redirection

--- assets/js/account.js
+++ assets/js/account.js
@@ -276,26 +276,29 @@ jQuery(function ($) {
     formData.append('action', 'directorist_register_form');
     $.ajax({
       url: directorist.ajaxurl,
-      type: "POST",
+      type: 'POST',
       data: formData,
       contentType: false,
       processData: false,
-      cache: false,
-      success: function success(response) {
-        if (response.success) {
-          $('.directorist-register-error').hide();
-          if (response.redirect_url) {
-            $('.directorist-register-error').empty().show().append(response.redirect_message).css({
-              'color': '#009114',
-              'background-color': '#d9efdc'
-            });
-            setTimeout(function () {
-              window.location.href = response.redirect_url;
-            }, 500);
-          }
-        } else {
-          $('.directorist-register-error').empty().show().append(response.data);
-        }
+      cache: false
+    }).done(function (_ref) {
+      var data = _ref.data,
+        success = _ref.success;
+      if (!success) {
+        $('.directorist-register-error').empty().show().append(data.error);
+        return;
+      }
+      $('.directorist-register-error').hide();
+      if (data.message) {
+        $('.directorist-register-error').empty().show().append(data.message).css({
+          'color': '#009114',
+          'background-color': '#d9efdc'
+        });
+      }
+      if (data.redirect_url) {
+        setTimeout(function () {
+          return window.location.href = data.redirect_url;
+        }, 500);
       }
     });
   });

bf23068bc972020f0088d470e42f83071ea66fae
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2024-09-16 10:04:59 +0600

    fixed fatal issue in search form (#1870)

--- templates/search-form/basic-search.php
+++ templates/search-form/basic-search.php
@@ -6,17 +6,24 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
+$fields = $searchform->get_basic_fields();
 ?>
 
 <div class="directorist-search-modal__overlay"></div>
 <div class="directorist-search-adv-filter directorist-advanced-filter directorist-search-modal__contents">
 	<span class="directorist-search-modal__minimizer"></span>
 	<div class="directorist-search-modal__contents__body">
-		<?php foreach ( $searchform->form_data[0]['fields'] as $key => $field ){ ?>
-			<div class="directorist-search-modal__input <?php echo $key === 'radius_search' ? 'directorist-radius-search' : ''; ?>">
-				<?php $searchform->field_template( $field ); ?>
-			</div>
-		<?php } ?>
+
+		<?php if ( ! empty( $fields ) ) : ?>
+
+			<?php foreach ( $fields as $key => $field ){ ?>
+				<div class="directorist-search-modal__input <?php echo $key === 'radius_search' ? 'directorist-radius-search' : ''; ?>">
+					<?php $searchform->field_template( $field ); ?>
+				</div>
+			<?php } ?>
+			
+		<?php endif; ?>
+
 		<div class="directorist-search-form-action__modal">
 			<button type="submit" class="directorist-btn directorist-btn-light directorist-search-form-action__modal__btn-search">
 

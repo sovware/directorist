f4b9f10a16a18ec46694ade8850857737d878acf
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2024-09-30 14:18:30 +0600

    Builder Header Improve (#1921)

---
+++ assets/src/js/admin/vue/modules/form-builder-modules/widget-component/Form_Builder_Widget_Video_Component.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+      v-if="videoOpened"
+      class="video-popup-overlay"
+      @click.prevent="$emit('close-video')"
+  >
+      <div class="video-popup-content" @click.stop>
+          <div class="video-container">
+              <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/TLeTwLWtm9Q"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  title="Directorist Tutorial"
+              ></iframe>
+          </div>
+      </div>
+      <button 
+        class="close-btn" 
+        @click.prevent="$emit('close-video')"
+      >
+          <span class="la la-close"></span>
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "form-builder-widget-video-component",
+  props: {
+    videoOpened: {
+      default: false,
+    },
+  },
+};
+</script>

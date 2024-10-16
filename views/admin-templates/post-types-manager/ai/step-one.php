71c2bb471b3a95b7c6013c499a4a10a9cdaa0c55
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2024-10-16 17:54:54 +0600

    Create Directory Step One - Updated

--- views/admin-templates/post-types-manager/ai/step-one.php
+++ views/admin-templates/post-types-manager/ai/step-one.php
@@ -1,11 +1,105 @@
-<h1>Step 1 loading..</h1>
+<div class="directorist-create-directory">
+    <div class="directorist-create-directory__header">
+        <div class="directorist-create-directory__close">
+            <a href="<?php echo esc_attr( admin_url() ); ?>" class="directorist-create-directory__close__btn">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.24408 5.24408C5.56951 4.91864 6.09715 4.91864 6.42259 5.24408L10 8.82149L13.5774 5.24408C13.9028 4.91864 14.4305 4.91864 14.7559 5.24408C15.0814 5.56951 15.0814 6.09715 14.7559 6.42259L11.1785 10L14.7559 13.5774C15.0814 13.9028 15.0814 14.4305 14.7559 14.7559C14.4305 15.0814 13.9028 15.0814 13.5774 14.7559L10 11.1785L6.42259 14.7559C6.09715 15.0814 5.56951 15.0814 5.24408 14.7559C4.91864 14.4305 4.91864 13.9028 5.24408 13.5774L8.82149 10L5.24408 6.42259C4.91864 6.09715 4.91864 5.56951 5.24408 5.24408Z" fill="#141921"/>
+                </svg>
+                Close All
+            </a>
+        </div>
 
+        <div class="directorist-create-directory__upgrade">
+            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M32 16C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16ZM5.28 16C5.28 21.9205 10.0795 26.72 16 26.72C21.9205 26.72 26.72 21.9205 26.72 16C26.72 10.0795 21.9205 5.28 16 5.28C10.0795 5.28 5.28 10.0795 5.28 16Z" fill="#F0F3FF"/>
+                <path d="M16 29.36C16 30.818 17.1898 32.0222 18.6281 31.7827C19.8244 31.5835 20.9971 31.2484 22.1229 30.7821C24.0641 29.978 25.828 28.7994 27.3137 27.3137C28.7994 25.828 29.978 24.0641 30.7821 22.1229C31.5861 20.1817 32 18.1012 32 16C32 13.8989 31.5861 11.8183 30.7821 9.87706C29.978 7.93586 28.7994 6.17203 27.3137 4.68629C25.828 3.20055 24.0641 2.022 22.1229 1.21793C20.9971 0.751602 19.8244 0.416526 18.6281 0.217309C17.1898 -0.0221786 16 1.18197 16 2.64C16 4.09803 17.1994 5.24799 18.6134 5.60344C19.12 5.73077 19.6176 5.89523 20.1024 6.09601C21.403 6.63474 22.5847 7.42437 23.5802 8.41982C24.5756 9.41526 25.3653 10.597 25.904 11.8976C26.4427 13.1982 26.72 14.5922 26.72 16C26.72 17.4078 26.4427 18.8018 25.904 20.1024C25.3653 21.403 24.5756 22.5847 23.5802 23.5802C22.5847 24.5756 21.403 25.3653 20.1024 25.904C19.6176 26.1048 19.12 26.2692 18.6134 26.3966C17.1994 26.752 16 27.902 16 29.36Z" fill="#F6C879"/>
+                <path d="M11.3253 11.2727V20H9.74432V12.8111H9.69318L7.65199 14.1151V12.6662L9.82102 11.2727H11.3253ZM16.7713 10.8636L13.9588 21.3125H13.0384L15.8509 10.8636H16.7713ZM17.7958 20V19.3608L20.1964 16.733C20.4781 16.4252 20.7101 16.1577 20.8924 15.9304C21.0747 15.7008 21.2096 15.4853 21.2972 15.2841C21.3872 15.0805 21.4322 14.8674 21.4322 14.6449C21.4322 14.3892 21.3706 14.1679 21.2475 13.9808C21.1268 13.7938 20.9611 13.6494 20.7504 13.5476C20.5397 13.4458 20.3029 13.3949 20.0401 13.3949C19.7608 13.3949 19.5169 13.4529 19.3086 13.5689C19.1026 13.6825 18.9428 13.8423 18.8292 14.0483C18.7179 14.2543 18.6623 14.4957 18.6623 14.7727H17.8242C17.8242 14.3466 17.9225 13.9725 18.119 13.6506C18.3155 13.3286 18.583 13.0777 18.9215 12.8977C19.2624 12.7178 19.6448 12.6278 20.0685 12.6278C20.4947 12.6278 20.8723 12.7178 21.2013 12.8977C21.5304 13.0777 21.7885 13.3203 21.9755 13.6257C22.1625 13.9311 22.256 14.2708 22.256 14.6449C22.256 14.9124 22.2075 15.174 22.1104 15.4297C22.0157 15.683 21.85 15.9659 21.6133 16.2784C21.3789 16.5885 21.0534 16.9673 20.6367 17.4148L19.0032 19.1619V19.2188H22.3839V20H17.7958Z" fill="#141921"/>
+            </svg>
+            <div class="directorist-create-directory__upgrade__content">
+                <h4 class="directorist-create-directory__upgrade__title">
+                    2 Directory & limited options in Free
+                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.875 11.5C8.875 11.5995 8.83549 11.6948 8.76517 11.7652C8.69484 11.8355 8.59946 11.875 8.5 11.875C8.26794 11.875 8.04538 11.7828 7.88128 11.6187C7.71719 11.4546 7.625 11.2321 7.625 11V8.5C7.625 8.46685 7.61183 8.43505 7.58839 8.41161C7.56495 8.38817 7.53315 8.375 7.5 8.375C7.40055 8.375 7.30516 8.33549 7.23484 8.26516C7.16451 8.19484 7.125 8.09946 7.125 8C7.125 7.90054 7.16451 7.80516 7.23484 7.73484C7.30516 7.66451 7.40055 7.625 7.5 7.625C7.73207 7.625 7.95463 7.71719 8.11872 7.88128C8.28282 8.04538 8.375 8.26794 8.375 8.5V11C8.375 11.0332 8.38817 11.0649 8.41161 11.0884C8.43506 11.1118 8.46685 11.125 8.5 11.125C8.59946 11.125 8.69484 11.1645 8.76517 11.2348C8.83549 11.3052 8.875 11.4005 8.875 11.5ZM7.75 6.375C7.87362 6.375 7.99445 6.33834 8.09723 6.26967C8.20002 6.20099 8.28012 6.10338 8.32743 5.98918C8.37473 5.87497 8.38711 5.74931 8.36299 5.62807C8.33888 5.50683 8.27935 5.39547 8.19195 5.30806C8.10454 5.22065 7.99317 5.16112 7.87193 5.13701C7.7507 5.11289 7.62503 5.12527 7.51083 5.17258C7.39662 5.21988 7.29901 5.29999 7.23033 5.40277C7.16166 5.50555 7.125 5.62639 7.125 5.75C7.125 5.91576 7.19085 6.07473 7.30806 6.19194C7.42527 6.30915 7.58424 6.375 7.75 6.375ZM14.375 8.5C14.375 9.76086 14.0011 10.9934 13.3006 12.0418C12.6001 13.0901 11.6045 13.9072 10.4396 14.3897C9.27473 14.8722 7.99293 14.9985 6.7563 14.7525C5.51967 14.5065 4.38376 13.8994 3.4922 13.0078C2.60064 12.1162 1.99348 10.9803 1.7475 9.7437C1.50152 8.50707 1.62776 7.22527 2.11027 6.06039C2.59278 4.89551 3.40988 3.89988 4.45824 3.19938C5.50661 2.49889 6.73915 2.125 8 2.125C9.69015 2.12698 11.3105 2.79927 12.5056 3.99439C13.7007 5.1895 14.373 6.80985 14.375 8.5ZM13.625 8.5C13.625 7.38748 13.2951 6.29994 12.677 5.37492C12.0589 4.44989 11.1804 3.72892 10.1526 3.30318C9.12476 2.87743 7.99376 2.76604 6.90262 2.98308C5.81148 3.20012 4.8092 3.73585 4.02253 4.52252C3.23586 5.30919 2.70013 6.31147 2.48309 7.40262C2.26604 8.49376 2.37744 9.62476 2.80318 10.6526C3.22892 11.6804 3.94989 12.5589 4.87492 13.177C5.79995 13.7951 6.88748 14.125 8 14.125C9.49134 14.1233 10.9211 13.5302 11.9757 12.4756C13.0302 11.4211 13.6233 9.99133 13.625 8.5Z" fill="#747C89"/>
+                    </svg>
+                </h4>
+                <a href="https://directorist.com/pricing/" target="_blank" class="directorist-create-directory__upgrade__link">
+                    Get Unlimited access
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="directorist-create-directory__wrapper">
+        <div class="directorist-create-directory__top">
+            <div class="directorist-create-directory__step">
+                <ul class="atbdp-setup-steps <?php echo esc_attr( $hide ); ?>">
+                        <li class="active"></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                </ul>
+                <span class="step-count"><?php esc_html_e( sprintf( '%s %d of 4',  'Step', $active_number ), 'directorist' ); ?></span>
+            </div>
+            <div class="directorist-create-directory__info">
+                <h1 class="directorist-create-directory__info__title">
+                    <?php esc_html_e('Let’s set up your directory', 'directorist'); ?>
+                </h1>
+                <p class="directorist-create-directory__info__desc">
+                    <?php esc_html_e('Build your directory instantly. Customize with Directorist AI', 'directorist'); ?>
+                </p>
+            </div>
+        </div>
+        <div class="directorist-create-directory__content">
+            <div class="directorist-create-directory__content__items">
+                <div class="directorist-create-directory__content__form-group">
+                    <label for="business-name">
+                        Enter your business name <span style="color: red;">*</span>
+                    </label>
+                    <div class="directorist-create-directory__contentinput-group">
+                        <span class="directorist-create-directory__content__input-icon">📝</span>
+                        <input
+                            type="text"
+                            id="business-name"
+                            class="form-control"
+                            placeholder="Business name"
+                            required
+                            />
+                        </div>
+                    </div>
 
-<form action="" class="directorist-ai-directory-step-one">
-    <label for="directorist-ai-business-name">Business Name</label>
-    <input type="text" id="directorist-ai-business-name">
-    <label for="directorist-ai-business-location">Business Location</label>
-    <input type="text" id="directorist-ai-business-location">
-</form>
- 
-<button class="directorist-ai-directory-submit-step-one">Submit</button>
+                    <div class="directorist-create-directory__content__form-group">
+                    <label for="business-location">
+                        Your business location <span class="text-muted">(optional)</span>
+                    </label>
+                    <div class="directorist-create-directory__content__input-group">
+                        <span class="input-icon">📍</span>
+                        <select id="business-location" class="form-control">
+                            <option value="" disabled selected>Enter your business location</option>
+                            <option value="Dhaka">Dhaka</option>
+                            <option value="Chittagong">Chittagong</option>
+                            <option value="Khulna">Khulna</option>
+                            <option value="Rajshahi">Rajshahi</option>  
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="directorist-create-directory__footer">
+                <div class="directorist-create-directory__back">
+                    <a href="<?php echo esc_url( wp_get_referer() ) ?>" class="directorist-create-directory__back__btn">
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13 5L8 10L13 15" stroke="#141B34" stroke-width="1.5" stroke-miterlimit="16"/>
+                        </svg>
+                        Back
+                    </a>
+                </div>
+                <div class="directorist-create-directory__next">
+                    <button type="submit" class="directorist-create-directory__btn directorist-create-directory__btn--next">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.85753 1.4856L7.31468 0.91417C7.54325 0.685598 7.8861 0.571312 8.22896 0.799884C8.34325 0.91417 8.45753 1.14274 8.45753 1.25703V1.94274C8.45753 2.2856 8.57182 2.62846 8.91468 2.97131L11.429 5.25703C12.8004 6.62846 13.6004 8.45703 13.6004 10.3999C13.6004 13.1427 11.3147 15.4285 8.57182 15.4285H8.00039C4.91468 15.4285 2.40039 12.9142 2.40039 9.82846C2.40039 8.34274 2.97182 6.97131 4.00039 5.94274C4.22896 5.59988 4.68611 5.59988 4.91468 5.94274C5.02896 6.05703 5.14325 6.2856 5.14325 6.39988V9.02846C5.14325 10.057 6.05753 10.857 7.0861 10.857C8.11468 10.857 8.91468 10.057 8.91468 9.02846C8.91468 8.34274 8.6861 7.8856 8.34325 7.54274L7.20039 6.39988C5.94325 5.14274 5.71468 2.97131 6.85753 1.4856Z" fill="#ffffff"/>
+                        </svg>
+                        Continue to Generate
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

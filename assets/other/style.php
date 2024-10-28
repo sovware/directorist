c613a0084b867627f98ca9fe267bc25ac79da670
Armanul46 <aazztech4@gmail.com>
2024-10-28 12:04:51 +0600

    fixed rgb color issue

--- assets/other/style.php
+++ assets/other/style.php
@@ -30,7 +30,7 @@ $gray_color                       = get_directorist_option('color_gray', '#bcbcb
     :root {
         /* theme color */
         --directorist-color-primary: <?php echo $primary_color; ?>;
-        --directorist-color-primary-rgb: <?php echo $primary_color_rgb ?? 0,0,0; ?>;
+        --directorist-color-primary-rgb: <?php echo $primary_color_rgb; ?>;
         --directorist-color-secondary: <?php echo $secondary_color; ?>;
         --directorist-color-dark: <?php echo $dark_color; ?>;
         --directorist-color-dark-rgb: 0,0,0;

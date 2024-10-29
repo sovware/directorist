30fd0b0bd67df87b12e7c01fcd98946237e9314f
Md Obidullah <obiPlabon@users.noreply.github.com>
2024-10-29 10:16:08 +0600

    Added new signin signup page block (#1963)

--- blocks/build/payment-receipt/index.asset.php
+++ blocks/build/payment-receipt/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5da00f6a91b6e6b56081');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4e3ec001363d608cc6b5');

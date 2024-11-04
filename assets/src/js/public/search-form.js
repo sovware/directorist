ca28df4fb7e526d7e0d499e4ef12d63dad5e528a
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2024-11-04 12:00:27 +0600

    Updated on View Type Change

--- assets/src/js/public/search-form.js
+++ assets/src/js/public/search-form.js
@@ -708,8 +708,8 @@ import './components/directoristSelect';
             handleRadiusVisibility();
         });
 
-        // handleRadiusVisibility Trigger on directory type | page change
-        $('body').on('click', '.directorist-type-nav__link, .directorist-pagination .page-numbers', function(e) {
+        // rangeSlider, defaultTags Trigger on directory type | page change
+        $('body').on('click', '.directorist-type-nav__link, .directorist-pagination .page-numbers, .directorist-viewas .directorist-viewas__item', function(e) {
             setTimeout(() => {
                 handleRadiusVisibility();
                 directorist_custom_range_slider();

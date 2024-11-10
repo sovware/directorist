f816a895c20a6fff2de07fc9617f7a53f4ff0300
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2024-11-10 13:23:51 +0600

    AJAX Search not working on Radius Search change - Fixed

--- assets/src/js/public/components/instantSearch.js
+++ assets/src/js/public/components/instantSearch.js
@@ -1234,7 +1234,7 @@ import debounce from '../../global/components/debounce';
     }, 250));
 
     // sidebar on change searching
-    $('body').on("change", ".directorist-instant-search .listing-with-sidebar input[type='checkbox'],.directorist-instant-search .listing-with-sidebar input[type='radio'] ", debounce( function(e) {
+    $('body').on("change", ".directorist-instant-search .listing-with-sidebar input[type='checkbox'],.directorist-instant-search .listing-with-sidebar input[type='radio'], .directorist-custom-range-slider__wrap .directorist-custom-range-slider__range", debounce( function(e) {
         e.preventDefault();
         var searchElm = $(this).closest('.listing-with-sidebar');
         filterListing(searchElm);

a3bb6d6266b15608dea58b3baa4609ad0a2eba47
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-01-26 16:10:58 +0600

    add license page

--- includes/licensing/init.php
+++ includes/licensing/init.php
@@ -6,8 +6,32 @@ namespace Directorist\Licensing;
 
 defined( 'ABSPATH' ) || die();
 
-require_once trailingslashit( __DIR__ ) . 'functions.php';
-require_once trailingslashit( __DIR__ ) . 'class-controllers.php';
-require_once trailingslashit( __DIR__ ) . 'class-routes.php';
+class Init {
+	public function __construct() {
+		require_once trailingslashit( __DIR__ ) . 'functions.php';
+		require_once trailingslashit( __DIR__ ) . 'class-controllers.php';
+		require_once trailingslashit( __DIR__ ) . 'class-routes.php';
 
-new Routes();
+		add_action( 'admin_menu', [$this, 'add_menu_page'] );
+
+		new Routes();
+	}
+
+	public function add_menu_page() {
+		add_submenu_page(
+			'edit.php?post_type=at_biz_dir',
+			'Licensing',
+			'Licensing',
+			'manage_options',
+			'directorist-licensing',
+			[$this, 'directorist_licensing'],
+			12
+		);
+	}
+
+	public function directorist_licensing() {
+		include 'views.php';
+	}
+}
+
+new Init();

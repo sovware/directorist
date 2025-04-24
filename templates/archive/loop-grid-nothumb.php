384bf6727e5a070f5de908414d1d54cfbb944f64
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2025-04-24 16:45:15 +0600

    loop-grid-nothumb - updated

--- templates/archive/loop-grid-nothumb.php
+++ templates/archive/loop-grid-nothumb.php
@@ -9,6 +9,17 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 $loop_fields = $listings->loop['card_fields']['template_data']['grid_view_without_thumbnail'];
 
+
+// Capture output for quick actions
+ob_start();
+$listings->render_loop_fields($loop_fields['top']['quick_actions']);
+$quick_actions_field = ob_get_clean();
+
+// Capture output for quick_info
+ob_start();
+$listings->render_loop_fields($loop_fields['top']['quick_info']);
+$quick_info_field = ob_get_clean();
+
 // Capture output for avatar
 ob_start();
 $listings->render_loop_fields($loop_fields['body']['avatar']);
@@ -19,21 +30,34 @@ ob_start();
 $listings->render_loop_fields($loop_fields['body']['title']);
 $listing_title = ob_get_clean();
 
-// Capture output for quick actions
+// Capture output for tagline
 ob_start();
-$listings->render_loop_fields($loop_fields['body']['quick_actions']);
-$quick_actions_field = ob_get_clean();
+$listings->render_loop_fields($loop_fields['body']['tagline']);
+$listing_tagline = ob_get_clean();
 
-// Capture output for quick_info
+// Capture output for badges
 ob_start();
-$listings->render_loop_fields($loop_fields['body']['quick_info']);
-$quick_info_field = ob_get_clean();
+$listings->render_loop_fields($loop_fields['body']['badges']);
+$listing_badges = ob_get_clean();
+
 ?>
 
 <article class="directorist-listing-single directorist-listing-single--bg directorist-listing-card directorist-listing-no-thumb <?php echo esc_attr( $listings->loop_wrapper_class() ); ?>">
+	<section class="directorist-listing-single__top">
+		<?php if ( ! empty( $quick_actions_field ) ) : ?>
+			<div class="directorist-listing-single__top__left">
+				<?php echo $quick_actions_field; ?>
+			</div>
+		<?php endif; ?>
+		
+		<?php if ( ! empty( $quick_info_field ) ) : ?>
+			<div class="directorist-listing-single__top__right">
+				<?php echo $quick_info_field; ?>
+			</div>
+		<?php endif; ?>
+	</section>
 
 	<section class="directorist-listing-single__header">
-
 		<?php if ( ! empty( $listing_avatar ) ) : ?>
 			<figure class="directorist-listing-single__header__left">
 				<?php echo $listing_avatar; ?>
@@ -45,29 +69,21 @@ $quick_info_field = ob_get_clean();
 				<?php echo $listing_title; ?>
 			</header>
 		<?php endif; ?>
-		
-		<?php if ( ! empty( $quick_actions_field ) ) : ?>
-			<div class="directorist-listing-single__header__right">
-				<div class="directorist-listing-single__action">
-					<?php echo $quick_actions_field; ?>
-				</div>
+
+		<?php if ( ! empty( $listing_tagline ) ) : ?>
+			<div class="directorist-listing-single__header__tagline">
+				<?php echo $listing_tagline; ?>
 			</div>
 		<?php endif; ?>
-
 	</section>
 
-	<?php if ( ! empty( $quick_info_field ) ) : ?>
-		<div class="directorist-listing-single__info">
-			<?php echo $quick_info_field; ?>
-		</div>
-	<?php endif; ?>
-
 	<section class="directorist-listing-single__content">
-		<ul class="directorist-listing-single__info__list"><?php $listings->render_loop_fields($loop_fields['body']['bottom'], '', ''); ?></ul>
-
-		<?php if ( ! empty( $loop_fields['body']['excerpt'] ) ) : ?>
-			<?php $listings->render_loop_fields( $loop_fields['body']['excerpt'] ) ?>
+		<?php if ( ! empty( $listing_badges ) ) : ?>
+			<div class="directorist-listing-single__content__badges">
+				<?php echo $listing_badges; ?>
+			</div>
 		<?php endif; ?>
+		<ul class="directorist-listing-single__info__list"><?php $listings->render_loop_fields($loop_fields['body']['bottom'], '', ''); ?></ul>
 	</section>
 
 	<footer class="directorist-listing-single__meta">

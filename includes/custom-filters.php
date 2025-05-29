1d3c89975568407081a889a82241d07600606567
Md Obidullah <obi.plabon@gmail.com>
2025-05-29 11:35:00 +0600

    Added term meta call backward compatibility

--- includes/custom-filters.php
+++ includes/custom-filters.php
@@ -24,3 +24,28 @@ function atbdp_directorist_button_classes( $type = 'primary' ) {
 function atbdp_get_image_source( $id = null, $size = 'medium' ) {
     return wp_get_attachment_image_url( $id, $size );
 }
+
+/**
+ * Backward compatibility for get_term_meta default directory call.
+ * @param mixed $value
+ * @param mixed $term_id
+ * @param mixed $meta_key
+ */
+function directorist_bc_get_default_directory_term_meta( $value, $term_id, $meta_key ) {
+    if ( $meta_key !== '_default' || ! directorist_is_directory( $term_id ) ) {
+        return $value;
+    }
+
+    remove_filter( 'get_term_metadata',  'directorist_bc_get_default_directory_term_meta', 10 );
+    remove_filter( 'default_term_metadata', 'directorist_bc_get_default_directory_term_meta', 10 );
+
+    $is_default = directorist_get_default_directory() === $term_id;
+
+    add_filter( 'get_term_metadata',  'directorist_bc_get_default_directory_term_meta', 10, 3 );
+    add_filter( 'default_term_metadata', 'directorist_bc_get_default_directory_term_meta', 10, 3 );
+
+    return $is_default;
+}
+
+add_filter( 'get_term_metadata',  'directorist_bc_get_default_directory_term_meta', 10, 3 );
+add_filter( 'default_term_metadata', 'directorist_bc_get_default_directory_term_meta', 10, 3 );

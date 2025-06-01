5344321316d19ccf2d5d053e4afe2d416bd78292
Md Obidullah <obi.plabon@gmail.com>
2025-06-01 16:22:50 +0600

    Updated shortcode with archive support

--- includes/classes/class-shortcode.php
+++ includes/classes/class-shortcode.php
@@ -212,10 +212,9 @@ class ATBDP_Shortcode {
         return $listings->render_shortcode( $atts );
     }
 
-    public function category_archive( $atts = array() ) {
+    public function category_archive( array $atts = [] ) {
         if ( is_tax( ATBDP_CATEGORY ) ) {
-            $category = get_queried_object();
-            $atts['category'] = $category->slug;
+            $atts['category'] = get_queried_object()->slug;
         } else {
             $category_slug    = ! empty( $_GET['category'] ) ? directorist_clean( wp_unslash( $_GET['category'] ) ) : urldecode( get_query_var( 'atbdp_category' ) );
             $atts['category'] = sanitize_text_field( $category_slug );
@@ -225,20 +224,26 @@ class ATBDP_Shortcode {
         return $this->listing_archive( $atts );
     }
 
-    public function tag_archive( $atts ) {
-        $atts        = ! empty( $atts ) ? $atts : [];
-        $tag_slug    = ! empty( $_GET['tag'] ) ? directorist_clean( wp_unslash( $_GET['tag'] ) ) : get_query_var( 'atbdp_tag' );
-        $atts['tag'] = sanitize_title_for_query( $tag_slug );
+    public function tag_archive( array $atts = [] ) {
+        if ( is_tax( ATBDP_TAGS ) ) {
+            $atts['tag'] = get_queried_object()->slug;
+        } else {
+            $tag_slug    = ! empty( $_GET['tag'] ) ? directorist_clean( wp_unslash( $_GET['tag'] ) ) : get_query_var( 'atbdp_tag' );
+            $atts['tag'] = sanitize_title_for_query( $tag_slug );
+        }
 
         $atts[ 'shortcode' ] = 'directorist_tag';
 
         return $this->listing_archive( $atts );
     }
 
-    public function location_archive( $atts ) {
-        $atts             = ! empty( $atts ) ? $atts : [];
-        $location_slug    = ! empty( $_GET['location'] ) ? directorist_clean( wp_unslash( $_GET['location'] ) ) : urldecode( get_query_var( 'atbdp_location' ) );
-        $atts['location'] = sanitize_text_field( $location_slug );
+    public function location_archive( array $atts = [] ) {
+        if ( is_tax( ATBDP_LOCATION ) ) {
+            $atts['location'] = get_queried_object()->slug;
+        } else {
+            $location_slug    = ! empty( $_GET['location'] ) ? directorist_clean( wp_unslash( $_GET['location'] ) ) : urldecode( get_query_var( 'atbdp_location' ) );
+            $atts['location'] = sanitize_text_field( $location_slug );
+        }
 
         $atts[ 'shortcode' ] = 'directorist_location';
 

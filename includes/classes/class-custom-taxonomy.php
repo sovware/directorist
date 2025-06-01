b116e5d9929af5f67e8188a30ffc4dd2de013364
Md Obidullah <obi.plabon@gmail.com>
2025-06-01 14:47:21 +0600

    Fixed recursive filter call

--- includes/classes/class-custom-taxonomy.php
+++ includes/classes/class-custom-taxonomy.php
@@ -118,35 +118,39 @@ if ( ! class_exists( 'ATBDP_Custom_Taxonomy' ) ) :
         }
 
         public function taxonomy_redirect_page( $url, $term, $taxonomy ) {
-            $directory_type_id       = get_post_meta( get_the_ID(), '_directory_type', true );
-            $directory_type_slug     = '';
-            $is_directorist_taxonomy = false;
+			if ( ! in_array( $taxonomy, [ ATBDP_CATEGORY, ATBDP_LOCATION, ATBDP_TAGS ], true ) ) {
+				return $url;
+			}
 
-            if ( ! empty( $directory_type_id ) ) {
-                $directory_type_term = get_term_by( 'id', $directory_type_id, ATBDP_DIRECTORY_TYPE );
-                $directory_type_slug = ( $directory_type_term && is_object( $directory_type_term ) ) ? $directory_type_term->slug : '';
+			remove_filter( 'term_link', [ $this, 'taxonomy_redirect_page' ], 10 );
+
+            $directory_id   = directorist_get_listing_directory( get_the_ID() );
+            $directory_slug = '';
+
+            if ( ! empty( $directory_id ) ) {
+                $directory_term = get_term_by( 'id', $directory_id, ATBDP_DIRECTORY_TYPE );
+                $directory_slug = ( $directory_term && is_object( $directory_term ) ) ? $directory_term->slug : '';
             }
 
             // Categories
-            if ( ATBDP_CATEGORY == $taxonomy ) {
-                $url                     = ATBDP_Permalink::atbdp_get_category_page( $term );
-                $is_directorist_taxonomy = true;
+            if ( ATBDP_CATEGORY === $taxonomy ) {
+                $url = ATBDP_Permalink::atbdp_get_category_page( $term );
             }
 
             // Location
-            if ( ATBDP_LOCATION == $taxonomy ) {
-                $url                     = ATBDP_Permalink::atbdp_get_location_page( $term );
-                $is_directorist_taxonomy = true;
+            if ( ATBDP_LOCATION === $taxonomy ) {
+                $url = ATBDP_Permalink::atbdp_get_location_page( $term );
             }
 
             // Tag
-            if ( ATBDP_TAGS == $taxonomy ) {
-                $url                     = ATBDP_Permalink::atbdp_get_tag_page( $term );
-                $is_directorist_taxonomy = true;
+            if ( ATBDP_TAGS === $taxonomy ) {
+                $url = ATBDP_Permalink::atbdp_get_tag_page( $term );
             }
 
-            if ( $is_directorist_taxonomy && ! empty( $directory_type_slug ) ) {
-                $url = add_query_arg( 'directory_type', $directory_type_slug, $url );
+			add_filter( 'term_link', [ $this, 'taxonomy_redirect_page' ], 10, 3 );
+
+            if ( ! empty( $directory_slug ) ) {
+                $url = add_query_arg( 'directory_type', $directory_slug, $url );
             }
 
             return $url;

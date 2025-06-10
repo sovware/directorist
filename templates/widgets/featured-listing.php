fbea1798f338c713fe477ce598b4b58367767c5a
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-06-10 16:34:24 +0600

    fix: featured listings thumb issue

--- templates/widgets/featured-listing.php
+++ templates/widgets/featured-listing.php
@@ -7,6 +7,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+use Directorist\Helper;
 use Directorist\Review\Markup;
 
 $f_listing_num = ! empty( $instance['f_listing_num'] ) ? $instance['f_listing_num'] : 5;
@@ -41,22 +42,24 @@ $default_icon = 'las la-tags';
         if ( $featured_listings->have_posts() ) {
             while ( $featured_listings->have_posts() ) {
                 $featured_listings->the_post();
-                $review_rating = directorist_get_listing_rating( get_the_ID() );
-                $review_count  = directorist_get_listing_review_count( get_the_ID() );
+                $listing_id    = get_the_ID();
+                $review_rating = directorist_get_listing_rating( $listing_id);
+                $review_count  = directorist_get_listing_review_count( $listing_id);
                 $review_text   = sprintf( _n( '%s review', $review_count > 0 ?  '%s reviews' : '%s review', $review_count, 'directorist' ), number_format_i18n( $review_count ) );
                 // get only one parent or high level term object
-                $listing_img = directorist_get_listing_gallery_images( get_the_ID() );
-                $listing_prv_img = directorist_get_listing_preview_image( get_the_ID() );
-                $listing_reviews = get_post_meta( get_the_ID(), '_directorist_reviews', true );
-                $price = get_post_meta( get_the_ID(), '_price', true );
-                $price_range = get_post_meta( get_the_ID(), '_price_range', true );
-                $listing_pricing = get_post_meta( get_the_ID(), '_atbd_listing_pricing', true );
-                $cats = get_the_terms( get_the_ID(), ATBDP_CATEGORY );
+                $listing_img = directorist_get_listing_gallery_images( $listing_id);
+                $listing_prv_img = directorist_get_listing_preview_image( $listing_id);
+                $listing_reviews = get_post_meta( $listing_id, '_directorist_reviews', true );
+                $price = get_post_meta( $listing_id, '_price', true );
+                $price_range = get_post_meta( $listing_id, '_price_range', true );
+                $listing_pricing = get_post_meta( $listing_id, '_atbd_listing_pricing', true );
+                $cats = get_the_terms( $listing_id, ATBDP_CATEGORY );
                 ?>
                 <div class="directorist-widget-listing__single">
                     <div class="directorist-widget-listing__image">
                         <?php
-                        $default_image = get_directorist_option( 'default_preview_image', DIRECTORIST_ASSETS . 'images/grid.jpg' );
+                        $type          = directorist_get_listing_directory( $listing_id );
+                        $default_image = Helper::default_preview_image_src( $type );
                         if ( ! empty( $listing_prv_img ) ) {
                             echo '<a href="' . esc_url( get_the_permalink() ) . '"><img src="' . esc_url( wp_get_attachment_image_url( $listing_prv_img, [90, 90] ) ) . '" alt="listing image"></a>';
                         } elseif ( ! empty( $listing_img[0] ) && empty( $listing_prv_img ) ) {
@@ -68,7 +71,7 @@ $default_icon = 'las la-tags';
                         ?>
                     </div>
                     <div class="directorist-widget-listing__content">
-                        <h4 class="directorist-widget-listing__title"><a href="<?php echo esc_url( get_post_permalink( get_the_ID() ) ); ?>"><?php echo esc_html( stripslashes( get_the_title() ) ); ?></a></h4>
+                        <h4 class="directorist-widget-listing__title"><a href="<?php echo esc_url( get_post_permalink( $listing_id) ); ?>"><?php echo esc_html( stripslashes( get_the_title() ) ); ?></a></h4>
                         <div class="directorist-widget-listing__meta">
                             <span class="directorist-widget-listing__rating">
                                 <?php Markup::show_rating_stars( $review_rating );?>

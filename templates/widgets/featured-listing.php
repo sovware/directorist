3b3d63999df69ffc721931aeae41cb635bc4e1e7
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-06-16 15:43:39 +0600

    add: esc html

--- templates/widgets/featured-listing.php
+++ templates/widgets/featured-listing.php
@@ -43,12 +43,12 @@ $default_icon = 'las la-tags';
             while ( $featured_listings->have_posts() ) {
                 $featured_listings->the_post();
                 $listing_id    = get_the_ID();
-                $review_rating = directorist_get_listing_rating( $listing_id);
-                $review_count  = directorist_get_listing_review_count( $listing_id);
+                $review_rating = directorist_get_listing_rating( $listing_id );
+                $review_count  = directorist_get_listing_review_count( $listing_id );
                 $review_text   = sprintf( _n( '%s review', $review_count > 0 ?  '%s reviews' : '%s review', $review_count, 'directorist' ), number_format_i18n( $review_count ) );
                 // get only one parent or high level term object
-                $listing_img = directorist_get_listing_gallery_images( $listing_id);
-                $listing_prv_img = directorist_get_listing_preview_image( $listing_id);
+                $listing_img = directorist_get_listing_gallery_images( $listing_id );
+                $listing_prv_img = directorist_get_listing_preview_image( $listing_id );
                 $listing_reviews = get_post_meta( $listing_id, '_directorist_reviews', true );
                 $price = get_post_meta( $listing_id, '_price', true );
                 $price_range = get_post_meta( $listing_id, '_price_range', true );
@@ -71,7 +71,7 @@ $default_icon = 'las la-tags';
                         ?>
                     </div>
                     <div class="directorist-widget-listing__content">
-                        <h4 class="directorist-widget-listing__title"><a href="<?php echo esc_url( get_post_permalink( $listing_id) ); ?>"><?php echo esc_html( stripslashes( get_the_title() ) ); ?></a></h4>
+                        <h4 class="directorist-widget-listing__title"><a href="<?php echo esc_url( get_post_permalink( $listing_id ) ); ?>"><?php echo esc_html( stripslashes( get_the_title() ) ); ?></a></h4>
                         <div class="directorist-widget-listing__meta">
                             <span class="directorist-widget-listing__rating">
                                 <?php Markup::show_rating_stars( $review_rating );?>

d9079df77ede42d1c9f4b68abf5adc46bac82c96
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2025-06-19 14:48:30 +0600

    formgent icon - updated

--- assets/js/admin-settings-manager.js
+++ assets/js/admin-settings-manager.js
@@ -33910,7 +33910,45 @@ var render = function render() {
     _c = _vm._self._c;
   return _c('div', {
     staticClass: "cptm-form-group cptm-form-content"
-  }, [_vm.isFormGentInstalled ? [_vm.isFormGentActive ? [_vm.isLoadingForms ? [_vm._m(0)] : [_vm.forms.length > 0 ? _c('div', {
+  }, [_vm.isFormGentInstalled ? [_vm.isFormGentActive ? [_vm.isLoadingForms ? [_c('div', {
+    staticClass: "cptm-form-content-wrapper"
+  }, [_c('span', {
+    staticClass: "cptm-form-content-icon la-spin"
+  }, [_c('svg', {
+    attrs: {
+      "width": "32",
+      "height": "32",
+      "viewBox": "0 0 32 32",
+      "fill": "none",
+      "xmlns": "http://www.w3.org/2000/svg"
+    }
+  }, [_c('g', {
+    attrs: {
+      "clip-path": "url(#clip0_10032_121490)"
+    }
+  }, [_c('path', {
+    attrs: {
+      "d": "M16.0001 2.66797V8.0013M16.0001 24.0013V29.3346M6.57341 6.57464L10.3467 10.348M21.6534 21.6546L25.4267 25.428M2.66675 16.0013H8.00008M24.0001 16.0013H29.3334M6.57341 25.428L10.3467 21.6546M21.6534 10.348L25.4267 6.57464",
+      "stroke": "#1E1E1E",
+      "stroke-width": "4",
+      "stroke-linecap": "round",
+      "stroke-linejoin": "round"
+    }
+  })]), _vm._v(" "), _c('defs', [_c('clipPath', {
+    attrs: {
+      "id": "clip0_10032_121490"
+    }
+  }, [_c('rect', {
+    attrs: {
+      "width": "32",
+      "height": "32",
+      "fill": "white"
+    }
+  })])])])]), _vm._v(" "), _c('h3', {
+    staticClass: "cptm-form-content-title"
+  }, [_vm._v("Loading forms...")]), _vm._v(" "), _c('p', {
+    staticClass: "cptm-form-content-desc"
+  }, [_vm._v("FormGent forms are appearing")])])] : [_vm.forms.length > 0 ? _c('div', {
     staticClass: "cptm-form-content-wrapper cptm-form-content-select"
   }, [_c('label', {
     staticClass: "cptm-form-content-label"
@@ -33929,8 +33967,21 @@ var render = function render() {
   })], 1) : _c('div', {
     staticClass: "cptm-form-content-wrapper"
   }, [_c('span', {
-    staticClass: "cptm-form-content-icon cptm-form-content-icon-only la la-file-text-o"
-  }), _vm._v(" "), _c('h3', {
+    staticClass: "cptm-form-content-icon"
+  }, [_c('svg', {
+    attrs: {
+      "width": "40",
+      "height": "40",
+      "viewBox": "0 0 40 40",
+      "fill": "none",
+      "xmlns": "http://www.w3.org/2000/svg"
+    }
+  }, [_c('path', {
+    attrs: {
+      "d": "M7.5 3.75V36.25H32.5V11.9995L32.124 11.626L24.624 4.12598L24.2505 3.75H7.5ZM10 6.25H22.5V13.75H30V33.75H10V6.25ZM25 8.00049L28.2495 11.25H25V8.00049ZM12.5 16.25V18.75H27.5V16.25H12.5ZM12.5 22.5V25H21.25V22.5H12.5ZM23.75 22.5V25H27.5V22.5H23.75ZM12.5 27.5V30H21.25V27.5H12.5ZM23.75 27.5V30H27.5V27.5H23.75Z",
+      "fill": "#747C89"
+    }
+  })])]), _vm._v(" "), _c('h3', {
     staticClass: "cptm-form-content-title"
   }, [_vm._v("No forms available.")]), _vm._v(" "), _c('p', {
     staticClass: "cptm-form-content-desc"
@@ -33944,8 +33995,56 @@ var render = function render() {
   }), _vm._v("\n            " + _vm._s(_vm.createFormButtonData.label) + "\n          ")])])]] : [_vm.canInstallPlugins ? _c('div', {
     staticClass: "cptm-form-content-wrapper"
   }, [_c('span', {
-    staticClass: "cptm-form-content-icon las la-info-circle"
-  }), _vm._v(" "), _c('h3', {
+    staticClass: "cptm-form-content-icon"
+  }, [_c('svg', {
+    attrs: {
+      "width": "40",
+      "height": "38",
+      "viewBox": "0 0 40 38",
+      "fill": "none",
+      "xmlns": "http://www.w3.org/2000/svg"
+    }
+  }, [_c('mask', {
+    attrs: {
+      "id": "path-1-inside-1_10032_119817",
+      "fill": "white"
+    }
+  }, [_c('path', {
+    attrs: {
+      "d": "M0 0H40V37.5H0V0Z"
+    }
+  })]), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M0 0H40V37.5H0V0Z",
+      "fill": "#141921"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M40 37.5V36.875H0V37.5V38.125H40V37.5Z",
+      "fill": "#2C3239",
+      "mask": "url(#path-1-inside-1_10032_119817)"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M20.217 9.66406H13.7814C13.0737 9.66406 12.5 10.2594 12.5 10.9939V13.683C12.5 14.4174 13.0737 15.0128 13.7814 15.0128H20.217C20.9247 15.0128 21.4985 14.4174 21.4985 13.683V10.9939C21.4985 10.2594 20.9247 9.66406 20.217 9.66406Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M13.7814 28.9921C13.0695 28.9921 12.5 28.4011 12.5 27.6623V19.654C12.5 18.9152 13.0695 18.3242 13.7814 18.3242H25.9977C26.7096 18.3242 27.2791 18.9152 27.2791 19.654V22.3136C27.2791 23.0524 26.7096 23.6434 25.9977 23.6434H17.6542V27.6328C17.6542 28.3715 17.0847 28.9626 16.3728 28.9626H13.7814V28.9921Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M24.4022 10.9922L24.8863 12.4993L26.3386 13.0017L24.8863 13.504L24.4022 15.0111L23.9181 13.504L22.4658 13.0017L23.9181 12.4993L24.4022 10.9922Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M26.4809 9.66406L26.6803 10.2846L27.2783 10.4915L26.6803 10.6983L26.4809 11.3189L26.2816 10.6983L25.6836 10.4915L26.2816 10.2846L26.4809 9.66406Z",
+      "fill": "white"
+    }
+  })])]), _vm._v(" "), _c('h3', {
     staticClass: "cptm-form-content-title"
   }, [_vm._v("Activate FormGent Plugin")]), _vm._v(" "), _c('p', {
     staticClass: "cptm-form-content-desc"
@@ -33972,8 +34071,56 @@ var render = function render() {
   }, [_vm._v("\n          You need the FormGent plugin to use this feature, ask the site admin\n          to activate it.\n        ")])])]] : [_vm.canInstallPlugins ? _c('div', {
     staticClass: "cptm-form-content-wrapper"
   }, [_c('span', {
-    staticClass: "cptm-form-content-icon las la-info-circle"
-  }), _vm._v(" "), _c('h3', {
+    staticClass: "cptm-form-content-icon"
+  }, [_c('svg', {
+    attrs: {
+      "width": "40",
+      "height": "38",
+      "viewBox": "0 0 40 38",
+      "fill": "none",
+      "xmlns": "http://www.w3.org/2000/svg"
+    }
+  }, [_c('mask', {
+    attrs: {
+      "id": "path-1-inside-1_10032_119817",
+      "fill": "white"
+    }
+  }, [_c('path', {
+    attrs: {
+      "d": "M0 0H40V37.5H0V0Z"
+    }
+  })]), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M0 0H40V37.5H0V0Z",
+      "fill": "#141921"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M40 37.5V36.875H0V37.5V38.125H40V37.5Z",
+      "fill": "#2C3239",
+      "mask": "url(#path-1-inside-1_10032_119817)"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M20.217 9.66406H13.7814C13.0737 9.66406 12.5 10.2594 12.5 10.9939V13.683C12.5 14.4174 13.0737 15.0128 13.7814 15.0128H20.217C20.9247 15.0128 21.4985 14.4174 21.4985 13.683V10.9939C21.4985 10.2594 20.9247 9.66406 20.217 9.66406Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M13.7814 28.9921C13.0695 28.9921 12.5 28.4011 12.5 27.6623V19.654C12.5 18.9152 13.0695 18.3242 13.7814 18.3242H25.9977C26.7096 18.3242 27.2791 18.9152 27.2791 19.654V22.3136C27.2791 23.0524 26.7096 23.6434 25.9977 23.6434H17.6542V27.6328C17.6542 28.3715 17.0847 28.9626 16.3728 28.9626H13.7814V28.9921Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M24.4022 10.9922L24.8863 12.4993L26.3386 13.0017L24.8863 13.504L24.4022 15.0111L23.9181 13.504L22.4658 13.0017L23.9181 12.4993L24.4022 10.9922Z",
+      "fill": "white"
+    }
+  }), _vm._v(" "), _c('path', {
+    attrs: {
+      "d": "M26.4809 9.66406L26.6803 10.2846L27.2783 10.4915L26.6803 10.6983L26.4809 11.3189L26.2816 10.6983L25.6836 10.4915L26.2816 10.2846L26.4809 9.66406Z",
+      "fill": "white"
+    }
+  })])]), _vm._v(" "), _c('h3', {
     staticClass: "cptm-form-content-title"
   }, [_vm._v("\n        Install & Activate FormGent Plugin\n      ")]), _vm._v(" "), _c('p', {
     staticClass: "cptm-form-content-desc"
@@ -33995,19 +34142,7 @@ var render = function render() {
     staticClass: "las la-sync la-spin"
   })]) : _c('span', [_vm._v(" Install & Activate")])])]) : _c('div', {}, [_vm._v("\n      You need the FormGent plugin to use this feature. Ask the site admin to\n      install and activate it.\n    ")])]], 2);
 };
-var staticRenderFns = [function () {
-  var _vm = this,
-    _c = _vm._self._c;
-  return _c('div', {
-    staticClass: "cptm-form-content-wrapper"
-  }, [_c('span', {
-    staticClass: "cptm-form-content-icon cptm-form-content-icon-only la la-spinner la-spin"
-  }), _vm._v(" "), _c('h3', {
-    staticClass: "cptm-form-content-title"
-  }, [_vm._v("Loading forms...")]), _vm._v(" "), _c('p', {
-    staticClass: "cptm-form-content-desc"
-  }, [_vm._v("FormGent forms are appearing")])]);
-}];
+var staticRenderFns = [];
 render._withStripped = true;
 
 
--- assets/src/js/admin/vue/modules/form-fields/Formgent_Form_Field.vue
+++ assets/src/js/admin/vue/modules/form-fields/Formgent_Form_Field.vue
@@ -4,9 +4,30 @@
       <template v-if="isFormGentActive">
         <template v-if="isLoadingForms">
           <div class="cptm-form-content-wrapper">
-            <span
-              class="cptm-form-content-icon cptm-form-content-icon-only la la-spinner la-spin"
-            ></span>
+            <span class="cptm-form-content-icon la-spin">
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_10032_121490)">
+                  <path
+                    d="M16.0001 2.66797V8.0013M16.0001 24.0013V29.3346M6.57341 6.57464L10.3467 10.348M21.6534 21.6546L25.4267 25.428M2.66675 16.0013H8.00008M24.0001 16.0013H29.3334M6.57341 25.428L10.3467 21.6546M21.6534 10.348L25.4267 6.57464"
+                    stroke="#1E1E1E"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_10032_121490">
+                    <rect width="32" height="32" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </span>
             <h3 class="cptm-form-content-title">Loading forms...</h3>
             <p class="cptm-form-content-desc">FormGent forms are appearing</p>
           </div>
@@ -27,9 +48,20 @@
           </div>
 
           <div class="cptm-form-content-wrapper" v-else>
-            <span
-              class="cptm-form-content-icon cptm-form-content-icon-only la la-file-text-o"
-            ></span>
+            <span class="cptm-form-content-icon">
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.5 3.75V36.25H32.5V11.9995L32.124 11.626L24.624 4.12598L24.2505 3.75H7.5ZM10 6.25H22.5V13.75H30V33.75H10V6.25ZM25 8.00049L28.2495 11.25H25V8.00049ZM12.5 16.25V18.75H27.5V16.25H12.5ZM12.5 22.5V25H21.25V22.5H12.5ZM23.75 22.5V25H27.5V22.5H23.75ZM12.5 27.5V30H21.25V27.5H12.5ZM23.75 27.5V30H27.5V27.5H23.75Z"
+                  fill="#747C89"
+                />
+              </svg>
+            </span>
             <h3 class="cptm-form-content-title">No forms available.</h3>
             <p class="cptm-form-content-desc">
               You haven't created any FormGent form yet.
@@ -44,7 +76,41 @@
 
       <template v-else>
         <div class="cptm-form-content-wrapper" v-if="canInstallPlugins">
-          <span class="cptm-form-content-icon las la-info-circle"></span>
+          <span class="cptm-form-content-icon">
+            <svg
+              width="40"
+              height="38"
+              viewBox="0 0 40 38"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <mask id="path-1-inside-1_10032_119817" fill="white">
+                <path d="M0 0H40V37.5H0V0Z" />
+              </mask>
+              <path d="M0 0H40V37.5H0V0Z" fill="#141921" />
+              <path
+                d="M40 37.5V36.875H0V37.5V38.125H40V37.5Z"
+                fill="#2C3239"
+                mask="url(#path-1-inside-1_10032_119817)"
+              />
+              <path
+                d="M20.217 9.66406H13.7814C13.0737 9.66406 12.5 10.2594 12.5 10.9939V13.683C12.5 14.4174 13.0737 15.0128 13.7814 15.0128H20.217C20.9247 15.0128 21.4985 14.4174 21.4985 13.683V10.9939C21.4985 10.2594 20.9247 9.66406 20.217 9.66406Z"
+                fill="white"
+              />
+              <path
+                d="M13.7814 28.9921C13.0695 28.9921 12.5 28.4011 12.5 27.6623V19.654C12.5 18.9152 13.0695 18.3242 13.7814 18.3242H25.9977C26.7096 18.3242 27.2791 18.9152 27.2791 19.654V22.3136C27.2791 23.0524 26.7096 23.6434 25.9977 23.6434H17.6542V27.6328C17.6542 28.3715 17.0847 28.9626 16.3728 28.9626H13.7814V28.9921Z"
+                fill="white"
+              />
+              <path
+                d="M24.4022 10.9922L24.8863 12.4993L26.3386 13.0017L24.8863 13.504L24.4022 15.0111L23.9181 13.504L22.4658 13.0017L23.9181 12.4993L24.4022 10.9922Z"
+                fill="white"
+              />
+              <path
+                d="M26.4809 9.66406L26.6803 10.2846L27.2783 10.4915L26.6803 10.6983L26.4809 11.3189L26.2816 10.6983L25.6836 10.4915L26.2816 10.2846L26.4809 9.66406Z"
+                fill="white"
+              />
+            </svg>
+          </span>
           <h3 class="cptm-form-content-title">Activate FormGent Plugin</h3>
           <p class="cptm-form-content-desc">
             You need the FormGent plugin to use this feature.
@@ -77,7 +143,41 @@
 
     <template v-else>
       <div class="cptm-form-content-wrapper" v-if="canInstallPlugins">
-        <span class="cptm-form-content-icon las la-info-circle"></span>
+        <span class="cptm-form-content-icon">
+          <svg
+            width="40"
+            height="38"
+            viewBox="0 0 40 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask id="path-1-inside-1_10032_119817" fill="white">
+              <path d="M0 0H40V37.5H0V0Z" />
+            </mask>
+            <path d="M0 0H40V37.5H0V0Z" fill="#141921" />
+            <path
+              d="M40 37.5V36.875H0V37.5V38.125H40V37.5Z"
+              fill="#2C3239"
+              mask="url(#path-1-inside-1_10032_119817)"
+            />
+            <path
+              d="M20.217 9.66406H13.7814C13.0737 9.66406 12.5 10.2594 12.5 10.9939V13.683C12.5 14.4174 13.0737 15.0128 13.7814 15.0128H20.217C20.9247 15.0128 21.4985 14.4174 21.4985 13.683V10.9939C21.4985 10.2594 20.9247 9.66406 20.217 9.66406Z"
+              fill="white"
+            />
+            <path
+              d="M13.7814 28.9921C13.0695 28.9921 12.5 28.4011 12.5 27.6623V19.654C12.5 18.9152 13.0695 18.3242 13.7814 18.3242H25.9977C26.7096 18.3242 27.2791 18.9152 27.2791 19.654V22.3136C27.2791 23.0524 26.7096 23.6434 25.9977 23.6434H17.6542V27.6328C17.6542 28.3715 17.0847 28.9626 16.3728 28.9626H13.7814V28.9921Z"
+              fill="white"
+            />
+            <path
+              d="M24.4022 10.9922L24.8863 12.4993L26.3386 13.0017L24.8863 13.504L24.4022 15.0111L23.9181 13.504L22.4658 13.0017L23.9181 12.4993L24.4022 10.9922Z"
+              fill="white"
+            />
+            <path
+              d="M26.4809 9.66406L26.6803 10.2846L27.2783 10.4915L26.6803 10.6983L26.4809 11.3189L26.2816 10.6983L25.6836 10.4915L26.2816 10.2846L26.4809 9.66406Z"
+              fill="white"
+            />
+          </svg>
+        </span>
         <h3 class="cptm-form-content-title">
           Install & Activate FormGent Plugin
         </h3>

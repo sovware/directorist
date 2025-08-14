6455124e87a71e80e62a8bec21895cc6c5a658c0
mdalaminbey <75071900+mdalaminbey@users.noreply.github.com>
2025-08-14 12:26:38 +0600

    Fixed settings page loading isse

--- app/Providers/Admin/MenuServiceProvider.php
+++ app/Providers/Admin/MenuServiceProvider.php
@@ -9,7 +9,11 @@ use Directorist\WpMVC\Exceptions\Exception;
 
 class MenuServiceProvider implements Provider {
     public function boot() {
-        add_submenu_page( 'edit.php?post_type=at_biz_dir', 'Orders New', 'Orders New', 'manage_options', 'directorist-orders', [ $this, 'orders_page' ], 12);
+        add_action( 'admin_menu', [$this, 'action_admin_menu'] );
+    }
+
+    public function action_admin_menu() {
+        add_submenu_page( 'edit.php?post_type=at_biz_dir', 'Orders New', 'Orders New', 'manage_options', 'directorist-orders', [ $this, 'orders_page' ] );
     }
 
     public function orders_page() {

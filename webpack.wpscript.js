c8a6f0652f35886399a441c7ff6b3c202a85e694
Tanjim Hasan <34968601+tanjimhasan@users.noreply.github.com>
2025-09-03 10:12:59 +0600

    Update webpack.wpscript.js

--- webpack.wpscript.js
+++ webpack.wpscript.js
@@ -1,5 +1,5 @@
-const path = require( 'path' );
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require('path');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const devHost = 'directorist.local';
 
 module.exports = {
@@ -7,11 +7,23 @@ module.exports = {
 	entry: {
 		'css/admin-order': './resources/sass/app.scss',
 		'js/admin/order': './resources/js/admin/order/index.tsx',
-		'js/frontend/payment-receipt': './resources/js/frontend/payment-receipt.js',
+		'js/frontend/payment-receipt':
+			'./resources/js/frontend/payment-receipt.js',
 	},
 	output: {
 		...defaultConfig.output,
-		path: path.resolve( __dirname, './assets/build/' ),
+		path: path.resolve(__dirname, './assets/build/'),
+	},
+	resolve: {
+		...defaultConfig.resolve,
+		alias: {
+			...(defaultConfig.resolve && defaultConfig.resolve.alias
+				? defaultConfig.resolve.alias
+				: {}),
+			'@babel/runtime': path.dirname(
+				require.resolve('@babel/runtime/package.json')
+			),
+		},
 	},
 	devServer: {
 		devMiddleware: {
@@ -28,5 +40,5 @@ module.exports = {
 			},
 		},
 		headers: { 'Access-Control-Allow-Origin': '*' },
-	}
+	},
 };

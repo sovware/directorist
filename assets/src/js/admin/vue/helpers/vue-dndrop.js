6d6fdb096eb509ddcf93c576614953f672a0a162
Ibrahim Riaz <ibrahimriaz.github@gmail.com>
2025-09-15 10:45:41 +0600

    Merge branch 'development' of https://github.com/sovware/directorist into builder/improve

--- assets/src/js/admin/vue/helpers/vue-dndrop.js
+++ assets/src/js/admin/vue/helpers/vue-dndrop.js
@@ -1,21 +1,21 @@
 export function applyDrag(arr, dragResult) {
-  const { removedIndex, addedIndex } = dragResult;
+	const { removedIndex, addedIndex } = dragResult;
 
-  // If neither removedIndex nor addedIndex are valid, return the array as-is
-  if (removedIndex === null || addedIndex === null) return arr;
+	// If neither removedIndex nor addedIndex are valid, return the array as-is
+	if (removedIndex === null || addedIndex === null) return arr;
 
-  const result = [...arr];
+	const result = [...arr];
 
-  // Perform the swap betwen two items
-  // const temp = result[removedIndex];
-  // result[removedIndex] = result[addedIndex];
-  // result[addedIndex] = temp;
+	// Perform the swap betwen two items
+	// const temp = result[removedIndex];
+	// result[removedIndex] = result[addedIndex];
+	// result[addedIndex] = temp;
 
-  // Remove the item from the removedIndex
-  const [removedItem] = result.splice(removedIndex, 1);
+	// Remove the item from the removedIndex
+	const [removedItem] = result.splice(removedIndex, 1);
 
-  // Insert the removed item at the addedIndex
-  result.splice(addedIndex, 0, removedItem);
+	// Insert the removed item at the addedIndex
+	result.splice(addedIndex, 0, removedItem);
 
-  return result;
+	return result;
 }
--- assets/src/js/admin/vue/modules/form-fields/themes/default/Toggle_Field_Theme_Default.vue
+++ assets/src/js/admin/vue/modules/form-fields/themes/default/Toggle_Field_Theme_Default.vue
@@ -1,33 +1,51 @@
 <template>
-    <div class="cptm-form-group">
-        <div class="cptm-input-toggle-wrap">
-            <div class="cptm-input-toggle-content">
-                <label v-if="( label.length )">
-                    <component :is="labelType">{{ label }}</component>
-                </label>
-                <p class="cptm-form-group-info" v-if="description.length" v-html="description"></p>
-            </div>
+  <div class="cptm-form-group">
+    <div class="cptm-input-toggle-wrap">
+      <div class="cptm-input-toggle-content">
+        <label v-if="label.length">
+          <component :is="labelType">{{ label }}</component>
+        </label>
+        <p
+          class="cptm-form-group-info"
+          v-if="description.length"
+          v-html="description"
+        ></p>
+      </div>
 
-            <div class="directorist_vertical-align-m cptm-input-toggle-btn">
-                <div class="directorist_item">
-                    <span class="cptm-input-toggle" :class="toggleClass" @click="toggleValue()"></span>
-                    <input type="checkbox" :id="name" :name="name" style="display: none;" v-model="local_value">
-                </div>
-
-                <div class="directorist_item">
-                    <a 
-                        class="cptm-btn cptm-btn-outline directorist_btn-start" 
-                        v-if="compLinkIsEnable" 
-                        :href="comp.link.url"
-                        :target="comp.link.target"
-                        :class="compLinkClass" 
-                        v-html="comp.link.label">
-                    </a>
-                </div>
-            </div>
+      <div class="directorist_vertical-align-m cptm-input-toggle-btn">
+        <div class="directorist_item">
+          <span
+            class="cptm-input-toggle"
+            :class="toggleClass"
+            @click="toggleValue()"
+          ></span>
+          <input
+            type="checkbox"
+            :id="name"
+            :name="name"
+            style="display: none"
+            v-model="local_value"
+          />
         </div>
 
-        <confirmation-modal v-bind="confirmation" @cancel="confirmationOnCancel()" />
+        <div class="directorist_item">
+          <a
+            class="cptm-btn cptm-btn-outline directorist_btn-start"
+            v-if="compLinkIsEnable"
+            :href="comp.link.url"
+            :target="comp.link.target"
+            :class="compLinkClass"
+            v-html="comp.link.label"
+          >
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <confirmation-modal
+      v-bind="confirmation"
+      @cancel="confirmationOnCancel()"
+    />
 
     <form-field-validatior
       :section-id="sectionId"

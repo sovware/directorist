48e8ac5f88c4f3f1634e27514ef95df22a77219c
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2025-10-14 15:13:44 +0600

    fixed quick action label issue in single listing

--- templates/single/fields/bookmark.php
+++ templates/single/fields/bookmark.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
-$label = ! empty( $data['options']['fields']['label']['value'] ) ? $data['options']['fields']['label']['value'] : '';
+$label = ! empty( $data['label'] ) ? $data['label'] : '';
 ?>
 <button class="directorist-single-listing-action directorist-action-bookmark directorist-btn directorist-btn-sm directorist-btn-light atbdp-favourites <?php echo ! is_user_logged_in() ? 'atbdp-require-login' : ''; ?>" data-listing_id="<?php echo esc_attr( get_the_ID() ); ?>" aria-label="Add to Favorite Button" data-label="<?php echo esc_attr( $label ); ?>">
     <?php echo wp_kses_post( the_atbdp_favourites_link( get_the_ID() ) . $label ); ?>
--- templates/single/fields/report.php
+++ templates/single/fields/report.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 <?php if ( is_user_logged_in() ) : ?>
     <button class="directorist-single-listing-action directorist-btn directorist-btn-sm directorist-btn-light directorist-action-report directorist-action-report-loggedin directorist-btn-modal directorist-btn-modal-js" href="#" data-directorist_target="directorist-report-abuse-modal" aria-label="Report Open Modal">
-        <?php directorist_icon( $icon );?><span class="directorist-single-listing-action__text"><?php esc_html_e( 'Report', 'directorist' ); ?></span> 
+        <?php directorist_icon( $icon );?><span class="directorist-single-listing-action__text"><?php esc_html_e( $data['label'] ?? 'Report', 'directorist' ); ?></span> 
     </button>
 <?php else : ?>
     <button class="directorist-single-listing-action directorist-btn directorist-btn-sm directorist-btn-light directorist-action-report directorist-action-report-not-loggedin directorist-btn-modal directorist-btn-modal-js"><?php directorist_icon( $icon );?> <span class="directorist-single-listing-action__text" aria-label="Report Modal"> <?php esc_html_e( 'Report', 'directorist' ); ?></span></button>

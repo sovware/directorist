ff1868d26e4aed02ef44a96db1182168fa09eaba
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2025-10-22 11:17:27 +0600

    build

--- blocks/build/dashboard/index.asset.php
+++ blocks/build/dashboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c13fb6fccd22e890aab5');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '921d1ef8bd1a6d7ff9e4');
--- blocks/build/payment-receipt/index.asset.php
+++ blocks/build/payment-receipt/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8f638d70ff62e22d4e3a');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '08bf56635841b1ca7b37');

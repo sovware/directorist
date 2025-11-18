92b58242de2ae62f51c7038b03df9e293194c7c4
Ibrahim Riaz <ibrahimriaz.github@gmail.com>
2025-11-18 10:34:15 +0600

    Search form group icon & title based on new id (search-bar | search-filter) - updated

--- assets/src/js/admin/vue/modules/form-builder-modules/widget-group-components/Form_Builder_Widget_Group_Header_Component.vue
+++ assets/src/js/admin/vue/modules/form-builder-modules/widget-group-components/Form_Builder_Widget_Group_Header_Component.vue
@@ -31,7 +31,7 @@
             </a>
           </div>
 
-          <h3 class="cptm-form-builder-group-title">
+          <h3 class="cptm-form-builder-group-title test-it">
             <span class="cptm-form-builder-group-title-icon">
               <span
                 v-html="getSearchIconContent()"
@@ -306,8 +306,7 @@ export default {
     getSearchGroup() {
       // Check if the group is a search group
       if (
-        this.groupData.id === "basic" || this.groupData.id === "basic-search-form" ||
-        this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form"
+        this.groupData.id === "basic" || this.groupData.id === "basic-search-form" || this.groupData.id === "search-bar" || this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form" || this.groupData.id === "search-filter"
       ) {
         return true;
       }
@@ -318,11 +317,11 @@ export default {
     getSearchIconContent() {
       let groupIcon = "";
 
-      if (this.groupData.id === "basic" || this.groupData.id === "basic-search-form") {
+      if (this.groupData.id === "basic" || this.groupData.id === "basic-search-form" || this.groupData.id === "search-bar") {
         groupIcon = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.5 17.5L13.875 13.875M9.16667 5C11.4679 5 13.3333 6.86548 13.3333 9.16667M15.8333 9.16667C15.8333 12.8486 12.8486 15.8333 9.16667 15.8333C5.48477 15.8333 2.5 12.8486 2.5 9.16667C2.5 5.48477 5.48477 2.5 9.16667 2.5C12.8486 2.5 15.8333 5.48477 15.8333 9.16667Z" stroke="#141921" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
       }
 
-      if (this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form") {
+      if (this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form" || this.groupData.id === "search-filter") {
         groupIcon = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 6.66602L12.5 6.66602M12.5 6.66602C12.5 8.04673 13.6193 9.16602 15 9.16602C16.3807 9.16602 17.5 8.04673 17.5 6.66602C17.5 5.2853 16.3807 4.16602 15 4.16602C13.6193 4.16602 12.5 5.2853 12.5 6.66602ZM7.5 13.3327L17.5 13.3327M7.5 13.3327C7.5 14.7134 6.38071 15.8327 5 15.8327C3.61929 15.8327 2.5 14.7134 2.5 13.3327C2.5 11.952 3.61929 10.8327 5 10.8327C6.38071 10.8327 7.5 11.952 7.5 13.3327Z" stroke="#141921" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
       }
 
@@ -331,11 +330,11 @@ export default {
 
     getSearchLabelContent() {
       let groupLabel = "";
-      if (this.groupData.id === "basic" || this.groupData.id === "basic-search-form") {
+      if (this.groupData.id === "basic" || this.groupData.id === "basic-search-form" || this.groupData.id === "search-bar") {
         groupLabel = "Search Bar";
       }
 
-      if (this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form") {
+      if (this.groupData.id === "advanced" || this.groupData.id === "advanced-search-form" || this.groupData.id === "search-filter") {
         groupLabel = "Search Filter";
       }
 

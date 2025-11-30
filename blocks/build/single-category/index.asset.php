7af864b32b62f35d03c8e97087b5f2573b19bc3d
rabbiislamrony <ronya4927@gmail.com>
2025-11-30 13:01:10 +0600

    update block build version

--- blocks/build/single-category/index.asset.php
+++ blocks/build/single-category/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '04c067736bd7f6b9c7bf');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '2096f29dc4ede6e91a20');

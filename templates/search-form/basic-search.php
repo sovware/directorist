598c332c22043f68f7908a9e7f224458455ca873
Ibrahim Riaz <ibrahimriaz.github@gmail.com>
2026-02-18 10:50:56 +0600

    Search Home markup issue - fixed

--- templates/search-form/basic-search.php
+++ templates/search-form/basic-search.php
@@ -12,22 +12,9 @@ $fields = $searchform->get_basic_fields();
 <div class="directorist-search-adv-filter directorist-advanced-filter">
     <?php if ( ! empty( $fields ) ) : ?>
 
-            <?php foreach ( $fields as $key => $field ) { ?>
-                <div class="directorist-search-modal__input <?php echo $key === 'title' ? 'directorist-search-query' : ''; ?> <?php echo $key === 'radius_search' ? 'directorist-radius-search' : ''; ?>">
-                    <?php $searchform->field_template( $field ); ?>
-                </div>
-            <?php } ?>
-            
-        <?php endif; ?>
-
-        <div class="directorist-search-form-action__modal">
-            <button type="submit" class="directorist-btn directorist-btn-light directorist-search-form-action__modal__btn-search">
-
-                <?php directorist_icon( 'las la-search' ); ?>
-
-                <?php echo esc_html( $searchform->search_button_text );?>
-
-            </button>
-        </div>
-    </div>
+        <?php foreach ( $fields as $key => $field ) { ?>
+            <?php $searchform->field_template( $field ); ?>
+        <?php } ?>
+        
+    <?php endif; ?>
 </div>

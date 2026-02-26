61097a15b61e294835b390b1f33120c84689b71a
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2026-02-26 15:09:49 +0600

    refactor: removed old order history from admin menu

--- app/Providers/Admin/MenuServiceProvider.php
+++ app/Providers/Admin/MenuServiceProvider.php
@@ -5,7 +5,6 @@ namespace Directorist\App\Providers\Admin;
 defined( "ABSPATH" ) || exit;
 
 use Directorist\WpMVC\Contracts\Provider;
-use Directorist\WpMVC\Exceptions\Exception;
 
 class MenuServiceProvider implements Provider {
     public function boot() {
@@ -13,7 +12,7 @@ class MenuServiceProvider implements Provider {
     }
 
     public function action_admin_menu() {
-        add_submenu_page( 'edit.php?post_type=at_biz_dir', 'Orders New', 'Orders New', 'manage_options', 'directorist-orders', [ $this, 'orders_page' ] );
+        add_submenu_page( 'edit.php?post_type=at_biz_dir', 'Orders', 'Orders', 'manage_options', 'directorist-orders', [ $this, 'orders_page' ] );
     }
 
     public function orders_page() {

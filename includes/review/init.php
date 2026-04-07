22d9f070b54a67c3a0a7815dfbf09329ad0d7591
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2026-04-07 10:44:02 +0600

    fixed security issue

--- includes/review/init.php
+++ includes/review/init.php
@@ -99,7 +99,7 @@ class Bootstrap {
                 $scope = '#respond';
             }
 
-            return wp_validate_redirect( wp_unslash( $_GET['redirect'] ), home_url( '/' ) ) . $scope;
+            return wp_validate_redirect( esc_url_raw( wp_unslash( $_GET['redirect'] ) ), home_url( '/' ) ) . $scope;
         }
 
         return $redirect;

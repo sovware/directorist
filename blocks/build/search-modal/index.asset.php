2f3065b4fc0c3c8bd0305b92fa7c9666584fe859
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2026-04-16 11:09:53 +0600

    build: update compiled assets with new bundles and formgene-integration support

--- blocks/build/search-modal/index.asset.php
+++ blocks/build/search-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes'), 'version' => '72ecb4552c3580c21bae');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes'), 'version' => '2cdaa393b66fe0e1e318');

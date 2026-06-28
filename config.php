37c381685170eb56a2fbfe35198827743c3a80ff
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2026-06-28 14:57:44 +0600

    work on readme

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Plugin version.
 if ( ! defined( 'ATBDP_VERSION' ) ) {
-    define( 'ATBDP_VERSION', '8.8.0' );
+    define( 'ATBDP_VERSION', '8.8.1' );
 }
 // Plugin Folder Path.
 if ( ! defined( 'ATBDP_DIR' ) ) {

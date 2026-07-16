5ef9c51891cb9059cfe757d752d4f05b81d10c3a
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2026-07-16 11:12:23 +0600

    build: restore generated assets from development

---
+++ assets/build/css/admin/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8e46a88d485bcd3c4fa4');
---
+++ assets/build/css/public/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2036ae4425f72101576a');

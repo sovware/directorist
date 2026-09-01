74212849c72babbe3aa274da386b915190703713
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2026-09-01 16:32:48 +0600

    readme update & build

--- blocks/build/single-location/index.asset.php
+++ blocks/build/single-location/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ad28f0d74dd098cb36f4');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '9c5ece60eca877df76c1');

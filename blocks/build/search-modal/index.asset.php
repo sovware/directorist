aa82bfb24c173975c361bf369fee88540b24efbc
Armanul46 <47377178+Armanul46@users.noreply.github.com>
2026-09-14 12:18:32 +0600

    build

--- blocks/build/search-modal/index.asset.php
+++ blocks/build/search-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'ac62213cf5643d647e05');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'd80277d728e160a83ccb');
